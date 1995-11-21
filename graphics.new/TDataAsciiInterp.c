42e57b3811362c9230865e3d5e44e3833f02a3d5
jussi <>
1995-11-21 23:20:49 +0000

    Added copyright notice and cleaned up code.

--- graphics.new/TDataAsciiInterp.c
+++ graphics.new/TDataAsciiInterp.c
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:51  jussi
+  Added CVS header.
+*/
 
 #include <string.h>
 #include "TDataAsciiInterp.h"
@@ -15,255 +32,277 @@
 TDataAsciiInterpClassInfo::TDataAsciiInterpClassInfo(
 	char *className, AttrList *attrList, int recSize,
 	char *separators, int numSeparators,
-	Boolean isSeparator, char *commentString){
-	_className = className;
-	_attrList = attrList;
-	_separators = separators;
-	_numSeparators = numSeparators;
-	_commentString = commentString;
-	_recSize = recSize;
-	_isSeparator = isSeparator;
-	_tdata = NULL;
-
+	Boolean isSeparator, char *commentString)
+{
+  _className = className;
+  _attrList = attrList;
+  _separators = separators;
+  _numSeparators = numSeparators;
+  _commentString = commentString;
+  _recSize = recSize;
+  _isSeparator = isSeparator;
+  _tdata = NULL;
 }
 
 TDataAsciiInterpClassInfo::TDataAsciiInterpClassInfo(
-	char *className, char *name, char *alias, TData *tdata){
-	_className = className;
-	_name = name;
-	_alias = alias;
-	_tdata = tdata;
+	char *className, char *name, char *alias, TData *tdata)
+{
+  _className = className;
+  _name = name;
+  _alias = alias;
+  _tdata = tdata;
 }
 
-TDataAsciiInterpClassInfo::~TDataAsciiInterpClassInfo(){
-/*
-printf("TDataAsciiInterpClassInfo: destructor\n");
-*/
-	if (_tdata != NULL)
-		delete _tdata;
+TDataAsciiInterpClassInfo::~TDataAsciiInterpClassInfo()
+{
+  /*
+     printf("TDataAsciiInterpClassInfo: destructor\n");
+  */
+  if (_tdata != NULL)
+    delete _tdata;
 }
 
-char *TDataAsciiInterpClassInfo::ClassName() { return _className; }
+char *TDataAsciiInterpClassInfo::ClassName()
+{
+  return _className;
+}
 
 static char buf1[256], buf2[256];
 static char *args[2];
-void TDataAsciiInterpClassInfo::ParamNames(int &argc, char **&argv){
-	argc = 2;
-	argv = args;
-	args[0] = buf1;
-	args[1] = buf2;
 
-	sprintf(buf1, "File %s", ControlPanel::Instance()->FileName());
-	sprintf(buf2, "Alias %s", ControlPanel::Instance()->FileAlias());
+void TDataAsciiInterpClassInfo::ParamNames(int &argc, char **&argv)
+{
+  argc = 2;
+  argv = args;
+  args[0] = buf1;
+  args[1] = buf2;
+  
+  sprintf(buf1, "File %s", ControlPanel::Instance()->FileName());
+  sprintf(buf2, "Alias %s", ControlPanel::Instance()->FileAlias());
 }
 
-ClassInfo *TDataAsciiInterpClassInfo::CreateWithParams(int argc, char **argv){
-    if (argc != 2){
-		fprintf(stderr,"TDataAsciiInterp::CreateWithParams: wrong args\n");
-		for (int i=0; i < argc; i++)
-			printf("%s\n", argv[i]);
-			return (ClassInfo *)NULL;
-	}
-	char *name = CopyString(argv[0]);
-	char *alias = CopyString(argv[1]);
-	TDataAsciiInterp *tdata = new TDataAsciiInterp(name,_recSize, _attrList,
-		_separators, _numSeparators, _isSeparator, _commentString);
-	return new TDataAsciiInterpClassInfo(_className, name, alias, tdata);
+ClassInfo *TDataAsciiInterpClassInfo::CreateWithParams(int argc, char **argv)
+{
+  if (argc != 2){
+    fprintf(stderr,"TDataAsciiInterp::CreateWithParams: wrong args\n");
+    for (int i=0; i < argc; i++)
+      printf("%s\n", argv[i]);
+    return (ClassInfo *)NULL;
+  }
+  char *name = CopyString(argv[0]);
+  char *alias = CopyString(argv[1]);
+  TDataAsciiInterp *tdata = new TDataAsciiInterp(name,_recSize, _attrList,
+						 _separators, _numSeparators,
+						 _isSeparator, _commentString);
+  return new TDataAsciiInterpClassInfo(_className, name, alias, tdata);
 }
 
-char *TDataAsciiInterpClassInfo::InstanceName(){
-	return _alias;
+char *TDataAsciiInterpClassInfo::InstanceName()
+{
+  return _alias;
 }
 
-void *TDataAsciiInterpClassInfo::GetInstance(){
-	return _tdata;
+void *TDataAsciiInterpClassInfo::GetInstance()
+{
+  return _tdata;
 }
 
 /* Get parameters that can be used to re-create this instance */
-void TDataAsciiInterpClassInfo::CreateParams(int &argc, char **&argv) {
-	argc = 2;
-	argv = args;
-	args[0] = _name;
-	args[1] = _alias;
+void TDataAsciiInterpClassInfo::CreateParams(int &argc, char **&argv)
+{
+  argc = 2;
+  argv = args;
+  args[0] = _name;
+  args[1] = _alias;
 }
-
-
 
 TDataAsciiInterp::TDataAsciiInterp(char *name, int recSize, AttrList *attrs,
-	char *separators,int numSeparators, Boolean isSeparator,
-	char *commentString):
-	TDataAscii(name, recSize){
-/*
-printf("TDataAsciiInterp %s recSize %d\n", name, recSize);
-*/
-	_recInterp = new RecInterp();
-	_recInterp->SetAttrs(attrs);
-
-	_name = name;
-	_recSize = recSize;
-	_attrList = attrs;
-	_separators = separators;
-	_numSeparators = numSeparators;
-	_commentString = commentString;
-	_isSeparator = isSeparator;
-	if (_commentString != NULL){
-		_commentStringLength = strlen(_commentString);
-	}
-	_numAttrs = _attrList->NumAttrs();
-
-	_numMatchingAttrs = 0;
-	int i;
-	for (i=0; i < _numAttrs; i++){
-		AttrInfo *info = _attrList->Get(i);
-		if (info->hasMatchVal){
-			if (_numMatchingAttrs >= MAX_MATCHING_ATTRS){
-				fprintf(stderr,"TDataASciiInterp: too many matching attrs\n");
-				Exit::DoExit(2);
-			}
-			_matchingAttrs[_numMatchingAttrs++] = info;
-		}
-	}
-
-	Initialize();
+				   char *separators, int numSeparators,
+				   Boolean isSeparator, char *commentString) :
+	TDataAscii(name, recSize)
+{
+  /*
+     printf("TDataAsciiInterp %s recSize %d\n", name, recSize);
+  */
+  _recInterp = new RecInterp();
+  _recInterp->SetAttrs(attrs);
+  
+  _name = name;
+  _recSize = recSize;
+  _attrList = attrs;
+  _separators = separators;
+  _numSeparators = numSeparators;
+  _commentString = commentString;
+  _isSeparator = isSeparator;
+  if (_commentString != NULL){
+    _commentStringLength = strlen(_commentString);
+  }
+  _numAttrs = _attrList->NumAttrs();
+  
+  _numMatchingAttrs = 0;
+  for(int i = 0; i < _numAttrs; i++) {
+    AttrInfo *info = _attrList->Get(i);
+    if (info->hasMatchVal){
+      if (_numMatchingAttrs >= MAX_MATCHING_ATTRS){
+	fprintf(stderr,"TDataASciiInterp: too many matching attrs\n");
+	Exit::DoExit(2);
+      }
+      _matchingAttrs[_numMatchingAttrs++] = info;
+    }
+  }
+  
+  Initialize();
 }
 
-TDataAsciiInterp::~TDataAsciiInterp(){
-/*
-printf("TDataAsciiInterp: destructor\n");
-*/
+TDataAsciiInterp::~TDataAsciiInterp()
+{
+  /*
+     printf("TDataAsciiInterp: destructor\n");
+  */
 }
 
-Boolean TDataAsciiInterp::Decode(RecId id, void *recordBuf, char *line){
+Boolean TDataAsciiInterp::Decode(RecId id, void *recordBuf, char *line)
+{
+  /* set buffer for interpreted record */
+  _recInterp->SetBuf(recordBuf);
+  
+  int numArgs;
+  char **args;
+  /*
+     printf("1 parsing line %s\n", line);
+  */
+  Parse(line,numArgs, args, _separators, _numSeparators, _isSeparator);
+  
+  /*
+     printf("got %d args\n", numArgs);
+  */
+  if (numArgs == 0 || 
+      (_commentString != NULL &&
+       strncmp(args[0],_commentString,_commentStringLength)== 0)) {
+    /*
+       printf("comment\n");
+    */
+    return false;
+  }
 
-	/* set buffer for interpreted record */
-	_recInterp->SetBuf(recordBuf);
+  /* See if this record matches if the record specifies a match*/
+  if (_numMatchingAttrs > 0) {
+    /*
+       printf("test for match\n");
+    */
 
-	int numArgs;
-	char **args;
-/*
-	printf("1 parsing line %s\n", line);
-*/
-	Parse(line,numArgs, args, _separators, _numSeparators, _isSeparator);
+    for(int i = 0; i < _numMatchingAttrs; i++) {
+      AttrInfo *info = _matchingAttrs[i];
+      if (info->attrNum >= numArgs)
+	/* does not match */
+	return false;
 
-/*
-printf("got %d args\n", numArgs);
-*/
-	if (numArgs == 0 || 
-		(_commentString != NULL &&
-			strncmp(args[0],_commentString,_commentStringLength)== 0)) {
-		/*
-		printf("comment\n");
-		*/
-		return false;
-	}
+      switch(info->type){
+      case IntAttr:
+	int intVal = atoi(args[i]);
+	if (intVal != info->matchVal.intVal)
+	  /* does not match */
+	  return false;
+	break;
 
-	
-	int i;
+      case FloatAttr:
+	float floatVal = UtilAtof(args[i]);
+	if (floatVal != info->matchVal.floatVal)
+	  return false;
+	break;
 
-	/* See if this record matches if the record specifies a match*/
-	if (_numMatchingAttrs > 0){
-		/*
-		printf("test for match\n");
-		*/
+      case DoubleAttr:
+	double doubleVal = UtilAtof(args[i]);
+	if (doubleVal != info->matchVal.doubleVal)
+	  return false;
+	break;
 
-		for (i=0; i < _numMatchingAttrs; i++){
-			AttrInfo *info = _matchingAttrs[i];
-			if (info->attrNum >= numArgs)
-				/* does not match */
-				return false;
-			switch(info->type){
-				case IntAttr:
-					int intVal = atoi(args[i]);
-					if (intVal != info->matchVal.intVal)
-						/* does not match */
-						return false;
-					break;
-				case FloatAttr:
-					float floatVal = UtilAtof(args[i]);
-					if (floatVal != info->matchVal.floatVal)
-						return false;
-					break;
-				case DoubleAttr:
-					double doubleVal = UtilAtof(args[i]);
-					if (doubleVal != info->matchVal.doubleVal)
-						return false;
-					break;
-				case StringAttr:
-					if (strcmp(args[i],info->matchVal.strVal) != 0)
-						return false;
-					break;
-				case DateAttr:
-					fprintf(stderr,"TDataAsciiInterp: date attr not implemented\n");
-					Exit::DoExit(2);
-					break;
-				default:
-					fprintf(stderr,"TDataAsciiInterp: unknown attrType\n");
-					Exit::DoExit(2);
-					break;
-			}
-		}
-	}
+      case StringAttr:
+	if (strcmp(args[i],info->matchVal.strVal) != 0)
+	  return false;
+	break;
 
-/*
-printf("match\n");
-*/
-	int maxArgs = MinMax::min(numArgs, _numAttrs);
-	Boolean hasComposite = false;
-	for (i=0; i < _numAttrs ; i++){
-		AttrInfo *attrInfo = _attrList->Get(i);
-		char *ptr = (char *)recordBuf + attrInfo->offset;
-		if (attrInfo->isComposite){
-			/* ignore composites for now */
-			hasComposite = true;
-			continue;
-		}
-		else {
-			if (i >= maxArgs)
-				/* nothing in buffer to convert */
-				continue;
-			switch(attrInfo->type){
-				case IntAttr:
-					int intVal = atoi(args[i]);
-					*(int *)ptr = intVal;
-					/*
-					printf("%d\n",intVal);
-					*/
-					break;
-				case FloatAttr:
-					float floatVal = UtilAtof(args[i]);
-					*(float *)ptr = floatVal;
-					/*
-					if (i == 0 )
-						printf("attr 0 %f\n",floatVal);
-					*/
-					break;
-				case DoubleAttr:
-					double doubleVal = UtilAtof(args[i]);
-					*(double *)ptr = doubleVal;
-					/*
-					if (i == 0 )
-						printf("attr 0 %f\n",doubleVal);
-					*/
-					break;
-				case StringAttr:
-					strncpy(ptr, args[i], attrInfo->length);
-					/*
-					printf("'%s'\n",args[i]);
-					*/
-					break;
-				case DateAttr:
-					time_t tm = atoi(args[i]);
-					*(time_t *)ptr = tm;
-					break;
-				default:
-					fprintf(stderr,"TDataAsciiInterp: unknown attribute type\n");
-					Exit::DoExit(2);
-			}
-		}
-	}
+      case DateAttr:
+	fprintf(stderr,"TDataAsciiInterp: date attr not implemented\n");
+	Exit::DoExit(2);
+	break;
 
-	/* decode composite attributes */
-	if (hasComposite)
-		CompositeParser::Decode(_attrList->GetName(), _recInterp);
-	return true;
+      default:
+	fprintf(stderr,"TDataAsciiInterp: unknown attrType\n");
+	Exit::DoExit(2);
+	break;
+      }
+    }
+  }
+
+  /*
+     printf("match\n");
+  */
+
+  int maxArgs = MinMax::min(numArgs, _numAttrs);
+  Boolean hasComposite = false;
+
+  for(int i = 0; i < _numAttrs; i++) {
+    AttrInfo *attrInfo = _attrList->Get(i);
+    char *ptr = (char *)recordBuf + attrInfo->offset;
+    if (attrInfo->isComposite) {
+      /* ignore composites for now */
+      hasComposite = true;
+      continue;
+    }
+
+    if (i >= maxArgs)
+      /* nothing in buffer to convert */
+      continue;
+
+    switch(attrInfo->type){
+    case IntAttr:
+      int intVal = atoi(args[i]);
+      *(int *)ptr = intVal;
+      /*
+	 printf("%d\n",intVal);
+      */
+      break;
+
+    case FloatAttr:
+      float floatVal = UtilAtof(args[i]);
+      *(float *)ptr = floatVal;
+      /*
+	 if (i == 0 )
+	 printf("attr 0 %f\n",floatVal);
+      */
+      break;
+
+    case DoubleAttr:
+      double doubleVal = UtilAtof(args[i]);
+      *(double *)ptr = doubleVal;
+      /*
+	 if (i == 0 )
+	 printf("attr 0 %f\n",doubleVal);
+      */
+      break;
+
+    case StringAttr:
+      strncpy(ptr, args[i], attrInfo->length);
+      /*
+	 printf("'%s'\n",args[i]);
+      */
+      break;
+      
+    case DateAttr:
+      time_t tm = atoi(args[i]);
+      *(time_t *)ptr = tm;
+      break;
+      
+    default:
+      fprintf(stderr,"TDataAsciiInterp: unknown attribute type\n");
+      Exit::DoExit(2);
+    }
+  }
+  
+  /* decode composite attributes */
+  if (hasComposite)
+    CompositeParser::Decode(_attrList->GetName(), _recInterp);
+
+  return true;
 }
--- graphics.new/TDataMap.c
+++ graphics.new/TDataMap.c
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:54  jussi
+  Added CVS header.
+*/
 
 #include <stdio.h>
 #if defined(SUN)||defined(PENTIUM)
@@ -19,276 +36,285 @@
 #include "QueryProc.h"
 
 /* counts how many TDataMaps have been created.
-Also used in creating the name of GData */
+   Also used in creating the name of GData */
 int TDataMap::_incarnation= 0; 
 
 /* constructor */
 TDataMap::TDataMap(char *name, TData *tdata, char *gdataName, 
-	int gdataRecSize,
-	VisualFlag dynamicArgs, int dynamicAttrs, int maxGDataPages,
-	VisualFlag *dimensionInfo, int numDimensions, Boolean createGData){
-	_incarnation++;
+		   int gdataRecSize,
+		   VisualFlag dynamicArgs, int dynamicAttrs, int maxGDataPages,
+		   VisualFlag *dimensionInfo, int numDimensions,
+		   Boolean createGData)
+{
+  _incarnation++;
 
-	_userData = NULL;
+  _userData = NULL;
 
-	_gdataAttrList = NULL;
-	_createGData = createGData;
-
-	_focusId = 0;
-	_maxGDataPages = maxGDataPages;
-
-
+  _gdataAttrList = NULL;
+  _createGData = createGData;
+  
+  _focusId = 0;
+  _maxGDataPages = maxGDataPages;
+  
 #ifdef OLD_CODE
-	/* should make it work even if NumPages in tdata > MAX_MAPPING_PAGES
-	by checking for bounding box f those pages not in the bitmap */
-	if (tdata->NumPages() > MAX_MAPPING_PAGES){
-		fprintf(stderr,"TDataMap can't handle tdata with %d pages, max = %d\n",
-				tdata->NumPages(), MAX_MAPPING_PAGES);
-		Exit::DoExit(2);
-	}
-
-	_boundingBoxBitmap = new Bitmap(MAX_MAPPING_PAGES);
+  /* should make it work even if NumPages in tdata > MAX_MAPPING_PAGES
+     by checking for bounding box f those pages not in the bitmap */
+  if (tdata->NumPages() > MAX_MAPPING_PAGES) {
+    fprintf(stderr,"TDataMap can't handle tdata with %d pages, max = %d\n",
+	    tdata->NumPages(), MAX_MAPPING_PAGES);
+    Exit::DoExit(2);
+  }
+  
+  _boundingBoxBitmap = new Bitmap(MAX_MAPPING_PAGES);
 #endif
-
-	/* set defaults */
-	_dimensionInfo = dimensionInfo;
-	_numDimensions = numDimensions;
-	_mappingName = name;
-	_pixelWidth = 1;
-	_dynamicAttrs = dynamicAttrs;
-	_dynamicArgs = dynamicArgs;
-	_tdata = tdata;
-	_gRecSize = gdataRecSize;
-
-	_gdataName = gdataName;
-	if (_gdataName == NULL)
-		_gdataName = CreateGDataName(_tdata->GetName(), name);
-	
-	_gdataPath = CreateGDataPath(_gdataName);
-
-	if (_createGData && _gRecSize < tdata->RecSize()){
-		_gdata = new GData(_tdata, _gdataPath, _gRecSize, 
-			maxGDataPages*DEVISE_PAGESIZE);
-	}
-	else {
-		_gdata = NULL;
-	}
-
-
-	_x = 0.0; _y = 0.0; _color = BlackColor;
-	_size = 1.0; _pattern = Pattern0; _orientation = 0.0;
-	_shapeId = RectShapeID; 
-	_numShapeAttr = MAX_MAPPING_SHAPE_ATTRS;
-	_shapeAttrs = new Coord[MAX_MAPPING_SHAPE_ATTRS];
-	for (int i=0; i < MAX_MAPPING_SHAPE_ATTRS; i++){
-		_shapeAttrs[i] = 0.1;
-	}
-
-	_boundWidth = 9.0; _boundHeight = 9.0;
-
-	_hintInitialized = false;
+  
+  /* set defaults */
+  _dimensionInfo = dimensionInfo;
+  _numDimensions = numDimensions;
+  _mappingName = name;
+  _pixelWidth = 1;
+  _dynamicAttrs = dynamicAttrs;
+  _dynamicArgs = dynamicArgs;
+  _tdata = tdata;
+  _gRecSize = gdataRecSize;
+  
+  _gdataName = gdataName;
+  if (_gdataName == NULL)
+    _gdataName = CreateGDataName(_tdata->GetName(), name);
+  
+  _gdataPath = CreateGDataPath(_gdataName);
+  
+  if (_createGData && _gRecSize < tdata->RecSize()) {
+    _gdata = new GData(_tdata, _gdataPath, _gRecSize, 
+		       maxGDataPages*DEVISE_PAGESIZE);
+  } else {
+    _gdata = NULL;
+  }
+  
+  _x = 0.0; _y = 0.0; _color = BlackColor;
+  _size = 1.0; _pattern = Pattern0; _orientation = 0.0;
+  _shapeId = RectShapeID; 
+  _numShapeAttr = MAX_MAPPING_SHAPE_ATTRS;
+  _shapeAttrs = new Coord[MAX_MAPPING_SHAPE_ATTRS];
+  for (int i=0; i < MAX_MAPPING_SHAPE_ATTRS; i++)
+    _shapeAttrs[i] = 0.1;
+  
+  _boundWidth = 9.0; _boundHeight = 9.0;
+  
+  _hintInitialized = false;
 }
 
-void TDataMap::SetDimensionInfo(VisualFlag *dimensionInfo, int numDimensions){
-	_dimensionInfo = dimensionInfo;
-	_numDimensions = numDimensions;
+void TDataMap::SetDimensionInfo(VisualFlag *dimensionInfo, int numDimensions)
+{
+  _dimensionInfo = dimensionInfo;
+  _numDimensions = numDimensions;
 }
-
 
 /****************************************************************************
 Map record to symbol by calling user defined Map() function.
 Fill in the values for the symbol.  This is called by the query processor
 ****************************************************************************/
+
 #ifdef TDATAMAP_MAP_TO_SYMBOL
-void TDataMap::MapToSymbol(TData *tdata, RecId recId, void *rec, Symbol *sym){
-	/* init default values */
-	sym->recId = recId;
-	sym->x = _x; sym->y = _y; sym->color = _color;
-	sym->size = _size; sym->pattern = _pattern; 
-	sym->orientation = _orientation; sym->shapeID = _shapeId;
-	sym->numShapeAttr = _numShapeAttr;
-	for (int i=0; i < _numShapeAttr; i++)
-		sym->shapeAttrs[i] = _shapeAttrs[i];
+void TDataMap::MapToSymbol(TData *tdata, RecId recId, void *rec, Symbol *sym)
+{
+  /* init default values */
+  sym->recId = recId;
+  sym->x = _x; sym->y = _y; sym->color = _color;
+  sym->size = _size; sym->pattern = _pattern; 
+  sym->orientation = _orientation; sym->shapeID = _shapeId;
+  sym->numShapeAttr = _numShapeAttr;
+  for(int i = 0; i < _numShapeAttr; i++)
+    sym->shapeAttrs[i] = _shapeAttrs[i];
+  
+  /* call the user-defined mapping function */
+  Map(tdata, recId, rec, sym);
+  
+  /*
+     _sym->xLow = sym->x;
+     _sym->yLow = sym->y;
+     _sym->width = 0;
+     _sym->height = 0;
+  */
 
-	/* call the user-defined mapping function */
-	Map(tdata, recId, rec, sym);
-
-	/*
-	_sym->xLow = sym->x;
-	_sym->yLow = sym->y;
-	_sym->width = 0;
-	_sym->height = 0;
-	*/
-
-	/* find the bounding box for the symbol */
-	ShapeRegistrar::SymbolBoundingBox(sym);
-
-	/* update the maximum bounding box found so far */
-	_deltaX = MinMax::max(sym->width, _deltaX);
-	_deltaY = MinMax::max(sym->height, _deltaY);
-
+  /* find the bounding box for the symbol */
+  ShapeRegistrar::SymbolBoundingBox(sym);
+  
+  /* update the maximum bounding box found so far */
+  _deltaX = MinMax::max(sym->width, _deltaX);
+  _deltaY = MinMax::max(sym->height, _deltaY);
 }
 #endif
-
 
 /************************************************************************
 Hint calculation
 ************************************************************************/
 Boolean TDataMap::PageHint(TData *tdata, Coord x, Boolean isPrefetch,
-	RecId &hintId){
-
-	return false;
+			   RecId &hintId)
+{
+  return false;
 #ifdef OLD_CODE
-	if (!_hintInitialized || tdata != _hintTdata ||
-			_numPages != tdata->NumPages() ){
-		/* initialize/reinit hint */
-
-		_hintInitialized = false;	/* set to true later */
-		_hintTdata = tdata;
-
-		_numPages = tdata->NumPages();
-		if (_numPages == 0)
-			return false;
-
-		/* get 1st page and last page and test */
-		int numRecs; 
-		RecId startRid;
-		void **recs;
-		tdata->GetPage(tdata->FirstPage(), numRecs, startRid,recs,isPrefetch);
-		if (numRecs == 0){
-			tdata->FreePage(tdata->FirstPage(), Stay);
-			return false;
-		}
-		ConvertToGData(recs,1, _tmpPtr);
-		tdata->FreePage(tdata->FirstPage(), Stay);
-
-		if (GetDynamicArgs() & VISUAL_X)
-			_minX = ((GDataBinRec *)_tmpPtr[0])->x;
-		else _minX = GetDefaultX();
-
-		tdata->GetPage(tdata->LastPage(), numRecs, startRid, recs,isPrefetch);
-		if (numRecs == 0){
-			tdata->FreePage(tdata->LastPage(), Stay);
-			return false;
-		}
-
-		ConvertToGData(&recs[numRecs-1],1, _tmpPtr);
-		tdata->FreePage(tdata->LastPage(), Stay);
-
-		if (GetDynamicArgs() & VISUAL_X)
-			_maxX = ((GDataBinRec *)_tmpPtr[0])->x;
-		else _minX = GetDefaultX();
-
-		_hintInitialized = true;
-	}
-
-	if ( x < _minX)
-		pageNum = tdata->FirstPage();
-	else if (x > _maxX)
-		pageNum = tdata->LastPage();
-	else {
-		pageNum= ((int)((x-_minX)/(_maxX-_minX)*_numPages))+tdata->FirstPage();
-		if (pageNum > tdata->LastPage())
-			pageNum = tdata->LastPage();
-	}
-	
-	return true;
+  if (!_hintInitialized || tdata != _hintTdata ||
+      _numPages != tdata->NumPages() ) {
+    /* initialize/reinit hint */
+    
+    _hintInitialized = false;	/* set to true later */
+    _hintTdata = tdata;
+    
+    _numPages = tdata->NumPages();
+    if (_numPages == 0)
+      return false;
+    
+    /* get 1st page and last page and test */
+    int numRecs; 
+    RecId startRid;
+    void **recs;
+    tdata->GetPage(tdata->FirstPage(), numRecs, startRid,recs,isPrefetch);
+    if (numRecs == 0) {
+      tdata->FreePage(tdata->FirstPage(), Stay);
+      return false;
+    }
+    ConvertToGData(recs,1, _tmpPtr);
+    tdata->FreePage(tdata->FirstPage(), Stay);
+    
+    if (GetDynamicArgs() & VISUAL_X)
+      _minX = ((GDataBinRec *)_tmpPtr[0])->x;
+    else _minX = GetDefaultX();
+    
+    tdata->GetPage(tdata->LastPage(), numRecs, startRid, recs,isPrefetch);
+    if (numRecs == 0) {
+      tdata->FreePage(tdata->LastPage(), Stay);
+      return false;
+    }
+    
+    ConvertToGData(&recs[numRecs-1],1, _tmpPtr);
+    tdata->FreePage(tdata->LastPage(), Stay);
+    
+    if (GetDynamicArgs() & VISUAL_X)
+      _maxX = ((GDataBinRec *)_tmpPtr[0])->x;
+    else
+      _minX = GetDefaultX();
+    
+    _hintInitialized = true;
+  }
+  
+  if ( x < _minX)
+    pageNum = tdata->FirstPage();
+  else if (x > _maxX)
+    pageNum = tdata->LastPage();
+  else {
+    pageNum= ((int)((x-_minX)/(_maxX-_minX)*_numPages))+tdata->FirstPage();
+    if (pageNum > tdata->LastPage())
+      pageNum = tdata->LastPage();
+  }
+  
+  return true;
 #endif
 }
 
 /*******************************************************
 Strip file name of preceding path name
 *********************************************************/
-static char *StripPath(char *name){
-	char *last = strrchr(name,'/');
-	if (last == NULL)
-		return name;
-	else return (last+1);
+static char *StripPath(char *name)
+{
+  char *last = strrchr(name,'/');
+  if (last == NULL)
+    return name;
+
+  return (last+1);
 }
 
 /***********************************************************
 Make a name for GData
 ************************************************************/
-char *TDataMap::CreateGDataName(char *tdataName, char *mappingName){
-	char *name;
-	char *tname= StripPath(tdataName);
-	name = new char[strlen(tname)+strlen(mappingName)+20];
-	sprintf(name,"%s.%d.%s.G",tname, _incarnation,mappingName);
-	return name;
+char *TDataMap::CreateGDataName(char *tdataName, char *mappingName)
+{
+  char *name;
+  char *tname= StripPath(tdataName);
+  name = new char[strlen(tname)+strlen(mappingName)+20];
+  sprintf(name,"%s.%d.%s.G",tname, _incarnation,mappingName);
+  return name;
 }
 
 /*********************************************************
 Make a path for storing gdata
 *********************************************************/
-char *TDataMap::CreateGDataPath(char *gdataName){
-	char *tmpDir = Init::TmpDir();
-	char *name = new char[strlen(gdataName)+strlen(tmpDir)+2];
-	sprintf(name,"%s/%s",tmpDir,gdataName);
-	return name;
+char *TDataMap::CreateGDataPath(char *gdataName)
+{
+  char *tmpDir = Init::TmpDir();
+  char *name = new char[strlen(gdataName)+strlen(tmpDir)+2];
+  sprintf(name,"%s/%s",tmpDir,gdataName);
+  return name;
 }
 
 /* return the max bounding box found so far */
-void TDataMap::MaxBoundingBox(Coord &width, Coord &height){
-/*
-printf("TDataMap::MaxBoundingBox\n");
-*/
-	width = _boundWidth; height = _boundHeight;
-	/*
-	_gdata->GetBoundingBox(width,height);
-	*/
+void TDataMap::MaxBoundingBox(Coord &width, Coord &height)
+{
+  /*
+     printf("TDataMap::MaxBoundingBox\n");
+  */
+  width = _boundWidth; height = _boundHeight;
+  /*
+     _gdata->GetBoundingBox(width,height);
+  */
 }
 
-void TDataMap::UpdateBoundingBox(Coord width, Coord height){
-	_boundWidth = width;
-	_boundHeight = height;
-	/*
-	_gdata->UpdateBoundingBox(width, height);
-	*/
+void TDataMap::UpdateBoundingBox(Coord width, Coord height)
+{
+  _boundWidth = width;
+  _boundHeight = height;
+  /*
+     _gdata->UpdateBoundingBox(width, height);
+  */
 }
 
-int TDataMap::TDataRecordSize(){
-	return _tdata->RecSize();
+int TDataMap::TDataRecordSize()
+{
+  return _tdata->RecSize();
 }
 
 /* Hint for current focus in GData */
 void TDataMap::SetFocusId(RecId id) { _focusId = id; }
 RecId TDataMap::GetFocusId() { return _focusId; }
 
-void TDataMap::SetDefaultShapeAttr(int attrNum, Coord shapeAttr){
-	if (attrNum < 0 || attrNum >= MAX_MAPPING_SHAPE_ATTRS){
-			fprintf(stderr,"Mapping::SetDefaultShapeAttr: attrNum %d\n",
-					attrNum);
-			Exit::DoExit(1);
-	}
-	_shapeAttrs[attrNum] = shapeAttr;
+void TDataMap::SetDefaultShapeAttr(int attrNum, Coord shapeAttr)
+{
+  if (attrNum < 0 || attrNum >= MAX_MAPPING_SHAPE_ATTRS) {
+    fprintf(stderr,"Mapping::SetDefaultShapeAttr: attrNum %d\n",
+	    attrNum);
+    Exit::DoExit(1);
+  }
+  _shapeAttrs[attrNum] = shapeAttr;
 }
 
 void TDataMap::SetDefaultShape(ShapeID shapeID, int numAttr,
-	ShapeAttr *shapeAttr){
-	if (numAttr > MAX_MAPPING_SHAPE_ATTRS){
-		fprintf(stderr,"Mapping::SetDefaultShape: too many attrs %d\n",
-				numAttr);
-		Exit::DoExit(1);
-	}
-
-	_shapeId = shapeID;
-	_numShapeAttr = numAttr;
-	if (numAttr > 0){
-		for (int i=0; i < numAttr; i++)
-			_shapeAttrs[i] = shapeAttr[i];
-	}
+			       ShapeAttr *shapeAttr)
+{
+  if (numAttr > MAX_MAPPING_SHAPE_ATTRS) {
+    fprintf(stderr,"Mapping::SetDefaultShape: too many attrs %d\n",
+	    numAttr);
+    Exit::DoExit(1);
+  }
+  
+  _shapeId = shapeID;
+  _numShapeAttr = numAttr;
+  if (numAttr > 0) {
+    for(int i=0; i < numAttr; i++)
+      _shapeAttrs[i] = shapeAttr[i];
+  }
 }
 
-void TDataMap::ResetGData(int gRecSize) {
-	if (_gdata != NULL){
-		QueryProc::Instance()->ClearGData(_gdata);
-		delete _gdata;
-		_gdata = NULL;
-		_gRecSize = gRecSize;
-		if (_createGData && _gRecSize < _tdata->RecSize()){
-			_gdata = new GData(_tdata, _gdataPath, _gRecSize,
-				_maxGDataPages*DEVISE_PAGESIZE);
-			QueryProc::Instance()->ResetGData(_tdata,_gdata);
-		}
-	}
+void TDataMap::ResetGData(int gRecSize)
+{
+  if (_gdata != NULL) {
+    QueryProc::Instance()->ClearGData(_gdata);
+    delete _gdata;
+    _gdata = NULL;
+    _gRecSize = gRecSize;
+    if (_createGData && _gRecSize < _tdata->RecSize()) {
+      _gdata = new GData(_tdata, _gdataPath, _gRecSize,
+			 _maxGDataPages*DEVISE_PAGESIZE);
+      QueryProc::Instance()->ResetGData(_tdata,_gdata);
+    }
+  }
 }
