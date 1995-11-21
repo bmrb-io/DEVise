0bc65384f910434cee1b5e6e3c3cab2a5885cc6e
jussi <>
1995-11-21 16:06:14 +0000

    Added copyright notice and cleaned up code a bit.

--- graphics.new/GDataSnap.c
+++ graphics.new/GDataSnap.c
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:56  jussi
+  Added CVS header.
+*/
 
 /* takes snapshot of GData conversion */
 
@@ -21,73 +38,74 @@
 
 class GDataSnapMapping: public ColorCompressionMapping {
 public:
-	GDataSnapMapping(){
-		double attrs[2];
-		attrs[0] = attrs[1] = 1.0;
-		Mapping::SetDefaultShape(RectXShapeID, 2, attrs);
-		Mapping::SetDefaultColor(BlackColor);
-	}
-
-	void Map(Stream *, void *rec, Symbol *symbol){
-		int *data = (int *)rec;
-		/*
-		printf("map %d\n", *data);
-		*/
-		symbol->x = (*data)+0.5;
-		symbol->y = 0.5;
-	}
+  GDataSnapMapping() {
+    double attrs[2];
+    attrs[0] = attrs[1] = 1.0;
+    Mapping::SetDefaultShape(RectXShapeID, 2, attrs);
+    Mapping::SetDefaultColor(BlackColor);
+  }
+  
+  void Map(Stream *, void *rec, Symbol *symbol) {
+    int *data = (int *)rec;
+    /*
+       printf("map %d\n", *data);
+       */
+    symbol->x = (*data)+0.5;
+    symbol->y = 0.5;
+  }
 };
 
 /***********************************************************************
 Constructor
 ***********************************************************************/
-GDataSnap::GDataSnap(GDataVar *gdata, ViewWindow *viewWin){
-	_gdata = gdata;
-	_tdata = gdata->GetTData();
 
+GDataSnap::GDataSnap(GDataVar *gdata, ViewWindow *viewWin) {
+  _gdata = gdata;
+  _tdata = gdata->GetTData();
 
-	/* Create views */
-	_mapping = new GDataSnapMapping();
-	_stream = new StreamCached(NULL, _tdata->NumPages());
-	_symStrm = new SimpleSymStrm(_stream, _mapping);
-	_focus = new SimpleFocus(_symStrm);
-	_view = new SimpleView("GDataSnap", 0.0, -0.25, 
-			(Coord)_tdata->NumPages(), 1.5, true, false, 1, NULL);
-	_view->InsertFocus(_focus);
-
-	_viewWin = viewWin;
+  /* Create views */
+  _mapping = new GDataSnapMapping();
+  _stream = new StreamCached(NULL, _tdata->NumPages());
+  _symStrm = new SimpleSymStrm(_stream, _mapping);
+  _focus = new SimpleFocus(_symStrm);
+  _view = new SimpleView("GDataSnap", 0.0, -0.25, 
+			 (Coord)_tdata->NumPages(), 1.5, true, false, 1, NULL);
+  _view->InsertFocus(_focus);
+  
+  _viewWin = viewWin;
 	_viewWin->Bind(_view);
-
-	/* insert the pages that we already know to have been mapped */
-	_gdata->InitRangeIterator();
-	PageRangeMapRec *rec;
-
-	while((rec=_gdata->NextRange()) != NULL){
-		int i;
-		for (i=rec->tLow; i <= rec->tHigh; i++){
-			/*
-			printf("inserting %d\n", i);
-			*/
-			int *ptr = new int;
-			*ptr = i;
-			_stream->InsertRec(ptr);
-		}
-		
-	}
-	_gdata->InsertCallback(this);
+  
+  /* insert the pages that we already know to have been mapped */
+  _gdata->InitRangeIterator();
+  PageRangeMapRec *rec;
+  
+  while((rec=_gdata->NextRange()) != NULL){
+    int i;
+    for (i=rec->tLow; i <= rec->tHigh; i++){
+      /*
+	 printf("inserting %d\n", i);
+	 */
+      int *ptr = new int;
+      *ptr = i;
+      _stream->InsertRec(ptr);
+    }
+    
+  }
+  _gdata->InsertCallback(this);
 }
 
 /*****************************************************************************
 Process Callback from GData
 ******************************************************************************/
+
 void GDataSnap::PageConverted(int /* GDataLow*/ , int /*GDataHigh*/ ,
-		int tLow, int tHigh){
+			      int tLow, int tHigh){
 	
-	int i;
-	for (i= tLow; i <= tHigh; i++){
-		int *ptr = new int;
-		*ptr = i;
-		_stream->InsertRec(ptr);
-	}
+  int i;
+  for (i= tLow; i <= tHigh; i++){
+    int *ptr = new int;
+    *ptr = i;
+    _stream->InsertRec(ptr);
+  }
 }
 
