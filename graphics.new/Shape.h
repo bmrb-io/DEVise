11720a648ed53f9f44269b52c789e4531f9cdd2f
jussi <>
1995-11-21 23:23:38 +0000

    Added copyright notice and cleaned up the code.

--- graphics.new/Shape.h
+++ graphics.new/Shape.h
@@ -1,10 +1,28 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:35  jussi
+  Added CVS header.
+*/
 
 #ifndef Shape_h
 #define Shape_h
+
 #include <stdio.h>
 #include "DeviseTypes.h"
 
@@ -12,26 +30,25 @@ class WindowRep;
 class TDataMap;
 
 class Shape {
-public:
-	/* Return # of shape attributes needed for this shape */
-	virtual int NumShapeAttrs() { return 0; }
+ public:
+  /* Return # of shape attributes needed for this shape */
+  virtual int NumShapeAttrs() {
+    return 0;
+  }
 
-	/* draw gdata. width == width of rectangle if size of
-	gdata is smaller than 1 pixel. Used to enhance the drawing
-	if the density of points is small.*/
-	virtual void DrawGDataArray(WindowRep *, void **, int, TDataMap *,
-		Coord xPerPixel, Coord yPerPixel, int width){};
+  /* draw gdata. width == width of rectangle if size of
+     gdata is smaller than 1 pixel. Used to enhance the drawing
+     if the density of points is small.*/
+  virtual void DrawGDataArray(WindowRep *, void **, int, TDataMap *,
+			      Coord xPerPixel, Coord yPerPixel, int width) {
+  }
 
-	/* Find bounding box for gdata. By default, set both to 0.0  */
-	virtual void BoundingBoxGData(TDataMap *, void **, int, Coord &width,
-		Coord &height){ width = 0.0; height = 0.0; };
-
-#ifdef OLD_CODE
-	/* do pick inside */
-	virtual Boolean Pick(Symbol *symbol,Coord xlow,Coord ylow,
-		Coord xhigh, Coord yhigh, int button)=0;
-#endif
-
+  /* Find bounding box for gdata. By default, set both to 0.0  */
+  virtual void BoundingBoxGData(TDataMap *, void **, int, Coord &width,
+				Coord &height) {
+    width = 0.0;
+    height = 0.0;
+  }
 };
 
 #endif
