0b14f63e0d436af07dc4c002bce400d71842e78f
jussi <>
1995-11-22 17:04:14 +0000

    Added IsValid() method and cleaned up the code.

--- graphics.new/TDataAsciiInterp.c
+++ graphics.new/TDataAsciiInterp.c
@@ -16,11 +16,16 @@
   $Id$
 
   $Log$
+  Revision 1.3  1995/11/21 23:20:49  jussi
+  Added copyright notice and cleaned up code.
+
   Revision 1.2  1995/09/05 22:15:51  jussi
   Added CVS header.
 */
 
 #include <string.h>
+#include <assert.h>
+
 #include "TDataAsciiInterp.h"
 #include "AttrList.h"
 #include "RecInterp.h"
@@ -58,7 +63,7 @@ TDataAsciiInterpClassInfo::~TDataAsciiInterpClassInfo()
   /*
      printf("TDataAsciiInterpClassInfo: destructor\n");
   */
-  if (_tdata != NULL)
+  if (_tdata)
     delete _tdata;
 }
 
@@ -83,7 +88,7 @@ void TDataAsciiInterpClassInfo::ParamNames(int &argc, char **&argv)
 
 ClassInfo *TDataAsciiInterpClassInfo::CreateWithParams(int argc, char **argv)
 {
-  if (argc != 2){
+  if (argc != 2) {
     fprintf(stderr,"TDataAsciiInterp::CreateWithParams: wrong args\n");
     for (int i=0; i < argc; i++)
       printf("%s\n", argv[i]);
@@ -132,16 +137,20 @@ TDataAsciiInterp::TDataAsciiInterp(char *name, int recSize, AttrList *attrs,
   _attrList = attrs;
   _separators = separators;
   _numSeparators = numSeparators;
-  _commentString = commentString;
   _isSeparator = isSeparator;
-  if (_commentString != NULL){
+
+  _commentString = commentString;
+  if (_commentString)
     _commentStringLength = strlen(_commentString);
-  }
   _numAttrs = _attrList->NumAttrs();
   
   _numMatchingAttrs = 0;
+  hasComposite = false;
+
   for(int i = 0; i < _numAttrs; i++) {
     AttrInfo *info = _attrList->Get(i);
+    if (info->isComposite)
+      hasComposite = true;
     if (info->hasMatchVal){
       if (_numMatchingAttrs >= MAX_MATCHING_ATTRS){
 	fprintf(stderr,"TDataASciiInterp: too many matching attrs\n");
@@ -161,6 +170,22 @@ TDataAsciiInterp::~TDataAsciiInterp()
   */
 }
 
+Boolean TDataAsciiInterp::IsValid(char *line)
+{
+  int numArgs;
+  char **args;
+
+  Parse(line, numArgs, args, _separators, _numSeparators, _isSeparator);
+
+  if (numArgs == 0 || 
+      (_commentString != NULL &&
+       strncmp(args[0], _commentString, _commentStringLength) == 0)) {
+    return false;
+  }
+	
+  return true;
+}
+
 Boolean TDataAsciiInterp::Decode(RecId id, void *recordBuf, char *line)
 {
   /* set buffer for interpreted record */
@@ -168,134 +193,58 @@ Boolean TDataAsciiInterp::Decode(RecId id, void *recordBuf, char *line)
   
   int numArgs;
   char **args;
-  /*
-     printf("1 parsing line %s\n", line);
-  */
-  Parse(line,numArgs, args, _separators, _numSeparators, _isSeparator);
+
+  Parse(line, numArgs, args, _separators, _numSeparators, _isSeparator);
   
-  /*
-     printf("got %d args\n", numArgs);
-  */
-  if (numArgs == 0 || 
-      (_commentString != NULL &&
-       strncmp(args[0],_commentString,_commentStringLength)== 0)) {
-    /*
-       printf("comment\n");
-    */
-    return false;
-  }
-
-  /* See if this record matches if the record specifies a match*/
-  if (_numMatchingAttrs > 0) {
-    /*
-       printf("test for match\n");
-    */
-
-    for(int i = 0; i < _numMatchingAttrs; i++) {
-      AttrInfo *info = _matchingAttrs[i];
-      if (info->attrNum >= numArgs)
-	/* does not match */
-	return false;
-
-      switch(info->type){
-      case IntAttr:
-	int intVal = atoi(args[i]);
-	if (intVal != info->matchVal.intVal)
-	  /* does not match */
-	  return false;
-	break;
-
-      case FloatAttr:
-	float floatVal = UtilAtof(args[i]);
-	if (floatVal != info->matchVal.floatVal)
-	  return false;
-	break;
-
-      case DoubleAttr:
-	double doubleVal = UtilAtof(args[i]);
-	if (doubleVal != info->matchVal.doubleVal)
-	  return false;
-	break;
-
-      case StringAttr:
-	if (strcmp(args[i],info->matchVal.strVal) != 0)
-	  return false;
-	break;
-
-      case DateAttr:
-	fprintf(stderr,"TDataAsciiInterp: date attr not implemented\n");
-	Exit::DoExit(2);
-	break;
-
-      default:
-	fprintf(stderr,"TDataAsciiInterp: unknown attrType\n");
-	Exit::DoExit(2);
-	break;
-      }
-    }
-  }
-
-  /*
-     printf("match\n");
-  */
+  assert(numArgs > 0);
 
   int maxArgs = MinMax::min(numArgs, _numAttrs);
-  Boolean hasComposite = false;
 
-  for(int i = 0; i < _numAttrs; i++) {
-    AttrInfo *attrInfo = _attrList->Get(i);
-    char *ptr = (char *)recordBuf + attrInfo->offset;
-    if (attrInfo->isComposite) {
-      /* ignore composites for now */
-      hasComposite = true;
-      continue;
-    }
+  for(int i = 0; i < _numMatchingAttrs; i++)
+    if (_matchingAttrs[i]->attrNum >= maxArgs)
+      return false;
 
-    if (i >= maxArgs)
-      /* nothing in buffer to convert */
-      continue;
+  for(i = 0; i < maxArgs; i++) {
 
-    switch(attrInfo->type){
+    AttrInfo *info = _attrList->Get(i);
+    char *ptr = (char *)recordBuf + info->offset;
+
+    switch(info->type) {
     case IntAttr:
       int intVal = atoi(args[i]);
+      if (info->hasMatchVal && intVal != info->matchVal.intVal)
+	return false;
       *(int *)ptr = intVal;
-      /*
-	 printf("%d\n",intVal);
-      */
+      // printf("int %d\n", intVal);
       break;
 
     case FloatAttr:
-      float floatVal = UtilAtof(args[i]);
-      *(float *)ptr = floatVal;
-      /*
-	 if (i == 0 )
-	 printf("attr 0 %f\n",floatVal);
-      */
-      break;
-
-    case DoubleAttr:
       double doubleVal = UtilAtof(args[i]);
+      if (info->hasMatchVal && doubleVal != info->matchVal.doubleVal)
+	return false;
       *(double *)ptr = doubleVal;
-      /*
-	 if (i == 0 )
-	 printf("attr 0 %f\n",doubleVal);
-      */
+      // printf("double %f\n", doubleVal);
       break;
 
     case StringAttr:
-      strncpy(ptr, args[i], attrInfo->length);
-      /*
-	 printf("'%s'\n",args[i]);
-      */
+      if (info->hasMatchVal && strcmp(args[i], info->matchVal.strVal))
+	return false;
+      strncpy(ptr, args[i], info->length);
+      ptr[info->length - 1] = '\0';
+      // printf("string '%s'\n", ptr);
       break;
-      
+
     case DateAttr:
+      if (info->hasMatchVal) {
+	fprintf(stderr,"TDataTapeInterp: date attr match not implemented\n");
+	Exit::DoExit(2);
+      }
       time_t tm = atoi(args[i]);
       *(time_t *)ptr = tm;
       break;
-      
+
     default:
-      fprintf(stderr,"TDataAsciiInterp: unknown attribute type\n");
+      fprintf(stderr,"TDataTapeInterp: unknown attribute type\n");
       Exit::DoExit(2);
     }
   }
