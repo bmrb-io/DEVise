6c4c2fc04748f5fea2de94ebe27d2f58e46c9524
jussi <>
1995-11-25 01:12:56 +0000

    Added copyright notice and added #include <unistd.h> for sleep() definition.

--- graphics/Dispatcher.c
+++ graphics/Dispatcher.c
@@ -1,12 +1,28 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:12:41  jussi
+  Added/update CVS header.
+*/
+
+#include <unistd.h>
 
 #include "Dispatcher.h"
-/*
-#include "DeviseWindow.h"
-*/
 #include "Control.h"
 #include "Init.h"
 #include "Selection.h"
