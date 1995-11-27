e2ca853cb9de09e46d662ddfc876de3602c5fe0f
jussi <>
1995-11-27 15:48:19 +0000

    Added copyright notice and cleaned up the code a bit.

--- graphics.new/ShapeID.h
+++ graphics.new/ShapeID.h
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:36  jussi
+  Added CVS header.
+*/
 
 /* definition of shape ID and shape attributes */
 
@@ -11,13 +28,16 @@
 typedef unsigned int ShapeID;
 
 /* ID of predefined shapes */
-const ShapeID RectShapeID = 0;
-const ShapeID SquareShapeID= 1;
-const ShapeID RectXShapeID = 2;
-const ShapeID BarShapeID=3;
-const ShapeID PixelRectShapeID = 4;
-const ShapeID TriangleShapeID= 5;
 
-typedef double ShapeAttr; /* Each attribute of the shape is a double, for now */
+const ShapeID RectShapeID      = 0;
+const ShapeID SquareShapeID    = 1;
+const ShapeID RectXShapeID     = 2;
+const ShapeID BarShapeID       = 3;
+const ShapeID PixelRectShapeID = 4;
+const ShapeID TriangleShapeID  = 5;
+
+/* Each attribute of the shape is a double, for now */
+
+typedef double ShapeAttr;
 
 #endif
