1be6f5efe9798c91569d41cf8f50a7262cbe6100
jussi <>
1995-11-28 16:58:25 +0000

    Added capability to store window images in a user specified file instead of printing it.

--- lib/util.tcl
+++ lib/util.tcl
@@ -15,11 +15,14 @@
 #  $Id$
 
 #  $Log$
+# Revision 1.1  1995/11/28  00:02:52  jussi
+# Initial revision.
+#
 
 ############################################################
 
 proc PrintView {} {
-    global printcmd allviews
+    global toprinter printcmd filename allviews
 
     # see if .printdef window already exists; if so, just return
     set err [catch {wm state .printdef}]
@@ -29,10 +32,12 @@ proc PrintView {} {
     wm title .printdef "Print View"
     wm minsize .printdef 200 100
     wm maxsize .printdef 800 600
-    wm geometry .printdef =500x150+150+150
+    wm geometry .printdef =500x200+150+150
     selection clear .printdef
     
+    set toprinter 1
     set printcmd "lpr "
+    set filename "/tmp/devise_dump.ps"
     set allviews 0
 
     frame .printdef.top -relief groove -borderwidth 2
@@ -42,27 +47,45 @@ proc PrintView {} {
 
     frame .printdef.top.row1
     frame .printdef.top.row2
-    pack .printdef.top.row1 .printdef.top.row2 -side top -pady 3m
+    frame .printdef.top.row3 -relief groove -borderwidth 2
+    pack .printdef.top.row1 .printdef.top.row2 .printdef.top.row3 \
+	    -side top -pady 3m
 
-    label .printdef.top.row1.l1 -text "Print Command:"
+    radiobutton .printdef.top.row1.r1 -text "To Printer" -width 12 \
+	    -variable toprinter -value 1 -command {
+	.printdef.top.row2.e1 configure -state disabled
+	.printdef.top.row1.e1 configure -state normal
+    } -anchor e
+    label .printdef.top.row1.l1 -text "Print Command:" -width 14
     entry .printdef.top.row1.e1 -relief sunken -textvariable printcmd \
 	    -width 30
-    pack .printdef.top.row1.l1 .printdef.top.row1.e1 -side left -padx 2m \
-	    -fill x -expand 1
+    pack .printdef.top.row1.r1 .printdef.top.row1.l1 .printdef.top.row1.e1 \
+	    -side left -padx 2m -fill x -expand 1
 
-    label .printdef.top.row2.l1 -text "Print Views:"
-    radiobutton .printdef.top.row2.r1 -text "All Views" \
+    radiobutton .printdef.top.row2.r1 -text "To File" -width 12 \
+	    -variable toprinter -value 0 -command {
+	.printdef.top.row1.e1 configure -state disabled
+	.printdef.top.row2.e1 configure -state normal
+    } -anchor e
+    label .printdef.top.row2.l1 -text "File name:" -width 14
+    entry .printdef.top.row2.e1 -relief sunken -textvariable filename \
+	    -width 30 -state disabled
+    pack .printdef.top.row2.r1 .printdef.top.row2.l1 .printdef.top.row2.e1 \
+	    -side left -padx 2m -fill x -expand 1
+
+    label .printdef.top.row3.l1 -text "Print Views:"
+    radiobutton .printdef.top.row3.r1 -text "All Views" \
 	    -variable allviews -value 1
-    radiobutton .printdef.top.row2.r2 -text "Selected View" \
+    radiobutton .printdef.top.row3.r2 -text "Selected View" \
 	    -variable allviews -value 0
-    pack .printdef.top.row2.l1 .printdef.top.row2.r1 .printdef.top.row2.r2 \
+    pack .printdef.top.row3.l1 .printdef.top.row3.r1 .printdef.top.row3.r2 \
 	    -side left -padx 2m -fill x -expand 1
 
     frame .printdef.bot.but
     pack .printdef.bot.but -side top
 
     button .printdef.bot.but.ok -text OK -width 10 \
-	    -command {	PrintActual $printcmd $allviews; \
+	    -command {	PrintActual $toprinter $printcmd $filename $allviews; \
 	                destroy .printdef }
     button .printdef.bot.but.cancel -text Cancel -width 10 \
 	    -command { destroy .printdef }
@@ -71,7 +94,7 @@ proc PrintView {} {
     tkwait visibility .printdef
 }
 
-proc PrintActual {printcmd allviews} {
+proc PrintActual {toprinter printcmd filename allviews} {
     global curView
 
     set windowlist ""
@@ -98,13 +121,28 @@ proc PrintActual {printcmd allviews} {
 	}
     }
 
-    set filename "/tmp/devise-dump%d.ps"
+    if {$toprinter} {
+	set template [format "/tmp/devise-%s-%%d.ps" [pid]]
+    } else {
+	set template [format "%s-%%d" $filename]
+    }
 
-    puts "Print windows: $windowlist"
-    set err [ eval DEVise printWindows $filename $windowlist ]
-    if {$err > 0} {
-	dialog .printError "Print Error" \
-		"An error occurred while printing views." "" 0 OK
-	return
+    set i 0
+    foreach win $windowlist {
+	set file [format $template $i]
+	puts "Save window $win to file $file"
+	set err [ DEVise saveWindowImage $win $file ]
+	if {$err > 0} {
+	    dialog .printError "Window Image Save Error" \
+		    "An error occurred while saving window images to files." \
+		    "" 0 OK
+	    return
+	}
+	if {$toprinter} {
+	    puts "File $file not actually printed:"
+	    puts "  $printcmd $file"
+	    puts "  rm $file"
+	}
+	incr i
     }
 }
