628a802da28a9de9eb0587f7b99711fa87656d15
jussi <>
1995-11-28 00:08:20 +0000

    Added copyright notice and cleaned up the code a bit.

--- graphics.new/Connector.h
+++ graphics.new/Connector.h
@@ -1,25 +1,44 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:38  jussi
+  Added CVS header.
+*/
 
 #ifndef Connector_h
 #define Connector_h
+
 #include "DeviseTypes.h"
 #include "Color.h"
 #include "ShapeID.h"
 #include "Pattern.h"
 #include "ConnectorShape.h"
 
-const unsigned MAX_CONNECTOR_SHAPE_ATTRS=10;
+const unsigned MAX_CONNECTOR_SHAPE_ATTRS = 10;
+
 struct Connector {
 public:
-	Coord x1,y1,x2,y2; /* end points of the connector */
-	Color color;
-	Pattern pattern;
-	ConnectorShapeID cShapeID; /* ID of connector shape */
-	int numShapeAttrs;	/* # of attributes for shape */
-	ShapeAttr shapeAttrs[MAX_CONNECTOR_SHAPE_ATTRS];
+  Coord x1,y1,x2,y2;                    /* end points of the connector */
+  Color color;
+  Pattern pattern;
+  ConnectorShapeID cShapeID;            /* ID of connector shape */
+  int numShapeAttrs;                    /* # of attributes for shape */
+  ShapeAttr shapeAttrs[MAX_CONNECTOR_SHAPE_ATTRS];
 };
 
 #endif
--- graphics.new/GDataRec.h
+++ graphics.new/GDataRec.h
@@ -1,23 +1,44 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:55  jussi
+  Added CVS header.
+*/
 
 #ifndef GDataRec_h
 #define GDataRec_h
+
 #include "Color.h"
-#include "Pattern.h"
 #include "ShapeID.h"
+#include "Pattern.h"
 
 /* full gdata record */
+
 const int MAX_GDATA_ATTRS = 10;
+
 struct GDataRec {
-	double x, y;
-	Color color;
-	double size;
-	Pattern  pattern; 
-	ShapeID shape; 
-	double orientation;
-	double shapeAttrs[MAX_GDATA_ATTRS];
+  double    x;
+  double    y;
+  Color     color;
+  double    size;
+  Pattern   pattern; 
+  ShapeID   shape; 
+  double    orientation;
+  ShapeAttr shapeAttrs[MAX_GDATA_ATTRS];
 };
 #endif
