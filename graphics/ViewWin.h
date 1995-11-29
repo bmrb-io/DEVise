ae828e9fd98f4e04e71acf6797931f1af658f7f7
jussi <>
1995-11-29 15:06:19 +0000

    Added copyright notice.

--- graphics/ViewWin.h
+++ graphics/ViewWin.h
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:13:19  jussi
+  Added/updated CVS header.
+*/
 
 /*
    A ViewWin is consist of two parts:
@@ -20,6 +37,7 @@
 
 #ifndef ViewWin_h
 #define ViewWin_h
+
 #include "DList.h"
 #include "DeviseTypes.h"
 #include "WindowRep.h"
