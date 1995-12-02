b082c0821a4da2fe46ac18c1b6d5699d6b007e8e
jussi <>
1995-12-02 21:07:33 +0000

    Substituted DeviseTime for Time and added copyright notice.

--- graphics.new/Command.c
+++ graphics.new/Command.c
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:33  jussi
+  Added CVS header.
+*/
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -169,7 +186,7 @@ char **argv;
 	if (strcmp(argv[0],"stat")== 0){
 		/* print statistics */
 		printf("%f (%f)seconds have passed\n", Timer::Now()/1000.0,
-			Time::Now()/1000.0);
+			DeviseTime::Now()/1000.0);
 		/*
 		_mgr->PrintStat();
 		*/
