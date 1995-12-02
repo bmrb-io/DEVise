c9e2dcf50d65f561a4095d6beedfdfa5c529dd40
jussi <>
1995-12-02 21:26:48 +0000

    Added horizontal view layout and added copyright notice.

--- graphics/WinClassInfo.h
+++ graphics/WinClassInfo.h
@@ -1,69 +1,93 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:13:31  jussi
+  Added/updated CVS header.
+*/
 
 /* Window class info */
 
 #ifndef WinClassInfo_h
-#define  WinClassInfo_h
+#define WinClassInfo_h
+
 #include "ClassDir.h"
 
 class ViewWinVer;
+class ViewWinHor;
+
 class WinVerInfo: public ClassInfo {
 public:
-	WinVerInfo();	/* class constructor */
-	WinVerInfo(char *name, ViewWinVer *win); /* instance constructor */
-	virtual ~WinVerInfo();	/* class constructor */
+  WinVerInfo();					/* class constructor */
+  WinVerInfo(char *name, ViewWinVer *win);	/* instance constructor */
+  virtual ~WinVerInfo();			/* class constructor */
 
-	/*********** Class methods **************************/
-    virtual char *CategoryName() { return "window"; }
-	virtual char *ClassName() { return "WinVertical"; }
+  /*********** Class methods **************************/
+  virtual char *CategoryName() { return "window"; }
+  virtual char *ClassName() { return "WinVertical"; }
 
-	/* Get name of parameters */
-	virtual void ParamNames(int &argc, char **&argv) ;
+  /* Get name of parameters */
+  virtual void ParamNames(int &argc, char **&argv) ;
 
-	/* Create instance using the supplied parameters. Return
-	the instance info if successful, otherwise return NULL. */
-	virtual ClassInfo *CreateWithParams(int argc, char **argv);
+  /* Create instance using the supplied parameters. Return
+     the instance info if successful, otherwise return NULL. */
+  virtual ClassInfo *CreateWithParams(int argc, char **argv);
 
-	/************* Instance Methods ***************************/
-	virtual char *InstanceName();
-	virtual void *GetInstance();
+  /************* Instance Methods ***************************/
+  virtual char *InstanceName();
+  virtual void *GetInstance();
 
-	/* Get parameters that can be used to re-create this instance */
-	virtual void CreateParams(int &argc, char **&argv);
+  /* Get parameters that can be used to re-create this instance */
+  virtual void CreateParams(int &argc, char **&argv);
 
 private:
-	char *arg[5];
-	char *_name;
-	ViewWinVer *_win;
+  char *arg[5];
+  char *_name;
+  ViewWinVer *_win;
 };
 
-#ifdef WIN_HOR
 class WinHorInfo: public ClassInfo {
-    virtual char *CategoryName() { return "window"; }
-	virtual char *ClassName() { return "WinHorizontal"; }
+public:
+  WinHorInfo();					/* class constructor */
+  WinHorInfo(char *name, ViewWinHor *win);	/* instance constructor */
+  virtual ~WinHorInfo();			/* class constructor */
 
-	/* Get name of parameters */
-	virtual void ParamNames(int &argc, char **&argv) {
-		argc = 5;
-		argv = arg;
-		arg[0] = "name";
-		arg[1] = "x";
-		arg[2] = "y";
-		arg[3] = "width";
-		arg[4] = "height";
-	}
+  /*********** Class methods **************************/
+  virtual char *CategoryName() { return "window"; }
+  virtual char *ClassName() { return "WinHorizontal"; }
 
-	/* Create instance using the supplied parameters. Return
-	the instance info if successful, otherwise return NULL. */
-	virtual InstanceInfo *CreateWithParams(int argc, char **argv);
+  /* Get name of parameters */
+  virtual void ParamNames(int &argc, char **&argv) ;
+
+  /* Create instance using the supplied parameters. Return
+     the instance info if successful, otherwise return NULL. */
+  virtual ClassInfo *CreateWithParams(int argc, char **argv);
+
+  /************* Instance Methods ***************************/
+  virtual char *InstanceName();
+  virtual void *GetInstance();
+
+  /* Get parameters that can be used to re-create this instance */
+  virtual void CreateParams(int &argc, char **&argv);
+
 private:
-	char *arg[5];
-
+  char *arg[5];
+  char *_name;
+  ViewWinHor *_win;
 };
-#endif
-
 
 #endif
