0a7bf8aa0b4fb06cd5a69ee31d8cc1bf6d26f849
jussi <>
1995-12-05 17:06:37 +0000

    Moved _stats from View (subclass) so that statistics can be turned on and displayed without having to redisplay the data itself.

--- graphics.new/ViewGraph.c
+++ graphics.new/ViewGraph.c
@@ -1,51 +1,85 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:17  jussi
+  Added CVS header.
+*/
 
 #include "ViewGraph.h"
 #include "TDataMap.h"
 #include "ActionDefault.h"
 
 ViewGraph::ViewGraph(char *name, VisualFilter &initFilter, 
-		AxisLabel *xAxis, AxisLabel *yAxis,
-		Color fg, Color bg,
-		Action *action):
-		View(name,action,initFilter, fg, bg, xAxis, yAxis){
-	if (action == NULL){
-		SetAction(new ActionDefault("default"));
-	}
+		     AxisLabel *xAxis, AxisLabel *yAxis,
+		     Color fg, Color bg,
+		     Action *action) :
+	View(name,action,initFilter, fg, bg, xAxis, yAxis)
+{
+  if (action == NULL)
+    SetAction(new ActionDefault("default"));
+
+  _DisplayStats = false;
 }
 
-void ViewGraph::InsertMapping(TDataMap *map){
-	_mappings.Append(map);
+void ViewGraph::InsertMapping(TDataMap *map)
+{
+  _mappings.Append(map);
 
-	AttrList *attrList = map->GDataAttrList();
-	if (attrList != NULL){
-		AttrInfo *info = attrList->Find("x");
-		if (info != NULL && info->type == DateAttr)
-			SetXAxisAttrType(DateAttr);
-
-		info = attrList->Find("y");
-		if (info != NULL && info->type == DateAttr)
-			SetYAxisAttrType(DateAttr);
-	}
-
-
-	Refresh();
+  AttrList *attrList = map->GDataAttrList();
+  if (attrList != NULL) {
+    AttrInfo *info = attrList->Find("x");
+    if (info != NULL && info->type == DateAttr)
+      SetXAxisAttrType(DateAttr);
+    info = attrList->Find("y");
+    if (info != NULL && info->type == DateAttr)
+      SetYAxisAttrType(DateAttr);
+  }
+  
+  Refresh();
 }
 
-void ViewGraph::InitMappingIterator(){
-	_index = _mappings.InitIterator();
+void ViewGraph::InitMappingIterator()
+{
+  _index = _mappings.InitIterator();
 }
 
-Boolean ViewGraph::MoreMapping(){
-	return _mappings.More(_index);
+Boolean ViewGraph::MoreMapping()
+{
+  return _mappings.More(_index);
 }
 
-TDataMap *ViewGraph::NextMapping(){
-	return _mappings.Next(_index);
+TDataMap *ViewGraph::NextMapping()
+{
+  return _mappings.Next(_index);
 }
-void ViewGraph::DoneMappingIterator(){
-	_mappings.DoneIterator(_index);
+
+void ViewGraph::DoneMappingIterator()
+{
+  _mappings.DoneIterator(_index);
+}
+
+/* Turn On/Off DisplayStats */
+void ViewGraph::SetDisplayStats(Boolean stat)
+{
+  _DisplayStats = stat;
+
+  if (_DisplayStats)
+    _stats.Report();
+  else
+    Refresh();
 }
--- graphics.new/ViewGraph.h
+++ graphics.new/ViewGraph.h
@@ -1,33 +1,60 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:17  jussi
+  Added CVS header.
+*/
 
 #ifndef ViewGraph_h
 #define ViewGraph_h
+
 #include "View.h"
 #include "DList.h"
 #include "Color.h"
+#include "BasicStats.h"
 
 class TDataMap;
 DefinePtrDList(TDataMapList, TDataMap *)
 
-class ViewGraph: public View{
+class ViewGraph: public View {
 public:
-	ViewGraph(char *name, VisualFilter &initFilter, 
-		AxisLabel *xAxis, AxisLabel *yAxis,
-		Color fg, Color bg,
-		Action *action = NULL);
+  ViewGraph(char *name, VisualFilter &initFilter, 
+	    AxisLabel *xAxis, AxisLabel *yAxis,
+	    Color fg, Color bg,
+	    Action *action = NULL);
 	
-	virtual void InsertMapping(TDataMap *map);
+  virtual void InsertMapping(TDataMap *map);
 
-	void InitMappingIterator();
-	Boolean MoreMapping();
-	TDataMap *NextMapping();
-	void DoneMappingIterator();
+  void InitMappingIterator();
+  Boolean MoreMapping();
+  TDataMap *NextMapping();
+  void DoneMappingIterator();
 
-private:
-TDataMapList _mappings;
-int _index;
+  /* Toggle the value of DisplayStats */
+  Boolean GetDisplayStats() {return _DisplayStats; }
+  void SetDisplayStats(Boolean stat);
+
+protected:
+  TDataMapList _mappings;
+  int _index;
+
+  /* TRUE if Statistics need to be displayed along with data */
+  Boolean _DisplayStats;
+  BasicStats _stats;
 };
 #endif
