c4e8e09ee3016c5b19ad702dc84557970aeda71c
jussi <>
1995-12-05 21:57:55 +0000

    Added some 20 missing colors which were defined in ColorMgr.c but not listed here. Now Devise can refer to all colors by the color constants. Also added the copyright notice.

--- graphics/Color.h
+++ graphics/Color.h
@@ -1,25 +1,70 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:12:27  jussi
+  Added/updated CVS header.
+*/
 
 #ifndef Color_h
 #define Color_h
 
 typedef unsigned long Color;
 
-/* default colors */
-const Color BlackColor = 0;
-const Color WhiteColor = 1;
-const Color RedColor = 2;
-const Color BlueColor = 3;
-const Color OrangeColor = 4;
-const Color YellowColor = 5;
-const Color GreenColor = 6;
-const Color PurpleColor = 7;
+const Color BlackColor		= 0;
+const Color WhiteColor		= 1;
+const Color RedColor		= 2;
+const Color BlueColor		= 3;
+const Color OrangeColor		= 4;
+const Color TanColor		= 5;
+const Color GreenColor		= 6;
+const Color PurpleColor		= 7;
+const Color AquamarineColor	= 8;
+const Color PeruColor		= 9;
+const Color ChocolateColor	= 10;
+const Color TomatoColor		= 11;
+const Color PinkColor		= 12;
+const Color PlumColor		= 13;
+const Color AzureColor		= 14;
+const Color CyanColor		= 15;
+const Color SeaGreenColor	= 16;
+const Color KhakiColor		= 17;
+const Color GoldenRodColor	= 18;
+const Color YellowColor		= 19;
+const Color SiennaColor		= 20;
+const Color GreenColor_2	= 21;
+const Color BlueColor_2		= 22;
+const Color OrangeColor_2	= 23;
+const Color LightGrayColor	= 24;
+const Color RedColor1		= 25;
+const Color RedColor2		= 26;
+const Color GreenColor1		= 27;
+const Color GreenColor2		= 28;
+const Color BlueColor1		= 29;
+const Color BlueColor2		= 30;
+const Color GoldColor1		= 31;
+const Color GoldColor2		= 32;
+const Color BlueColor1_2	= 33;
+const Color BlueColor2_2	= 34;
+const Color BlueColor3		= 35;
+const Color BlueColor4		= 36;
 
-const Color ForegroundColor = 0;
-const Color BackgroundColor = 1;
-const Color HighlightColor = 2;
+const Color ForegroundColor	= BlackColor;
+const Color BackgroundColor	= WhiteColor;
+const Color HighlightColor	= RedColor;
 
 #endif
