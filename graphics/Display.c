9f4f18c4b95a0d8c74ba97dfcba44e55fad1692a
jussi <>
1995-12-14 16:59:30 +0000

    Small fixes, and added copyright notice.

--- graphics/Display.c
+++ graphics/Display.c
@@ -1,76 +1,98 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:12:43  jussi
+  Added/updated CVS header.
+*/
 
 #include "XDisplay.h"
 
-/* DList <DeviseDisplay *> DeviseDisplay::_displays;*/
 DeviseDisplayList DeviseDisplay::_displays;
 DeviseDisplay *DeviseDisplay::_defaultDisplay= NULL;
 
 /********************************************************************
 Get the default display
 *********************************************************************/
-DeviseDisplay *DeviseDisplay::DefaultDisplay(){
 
-	if (_defaultDisplay == NULL)
-		_defaultDisplay = new XDisplay();
-	return _defaultDisplay;
+DeviseDisplay *DeviseDisplay::DefaultDisplay()
+{
+  if (_defaultDisplay == NULL)
+    _defaultDisplay = new XDisplay();
+  return _defaultDisplay;
 }
 
 /************************************************************/
-DeviseDisplay::DeviseDisplay(){
-	_dispatcher = Dispatcher::Current();
-	_dispatcher->Register(this,AllState,true);
-/*
-printf("after DeviseDisplay register 0x%x\n", this);
-*/
 
-	_numColors = 0;
-	_colorMapSize = InitColorMapSize;
-	_colorMap = new Color[InitColorMapSize];
-	_displays.Append(this);
+DeviseDisplay::DeviseDisplay()
+{
+  _dispatcher = Dispatcher::Current();
+  _dispatcher->Register(this,AllState,true);
+
+  _numColors = 0;
+  _colorMapSize = InitColorMapSize;
+  _colorMap = new Color[InitColorMapSize];
+  _displays.Append(this);
 }
 
 /************************************************************/
-DeviseDisplay::~DeviseDisplay(){
-	_dispatcher->Unregister(this);
-	delete _colorMap;
-	_displays.Delete(this);
-}
 
+DeviseDisplay::~DeviseDisplay()
+{
+  _dispatcher->Unregister(this);
+  delete _colorMap;
+  _displays.Delete(this);
+}
 
 /***************************************************************
  map local color to global color.
-XXX: Need to check for duplicate globalColor 
+ XXX: Need to check for duplicate globalColor 
 ********************************************************************/
-void DeviseDisplay::MapColor(Color localColor, Color globalColor){
-	if (globalColor >= _colorMapSize-1){
-		/* overflow, create a new array  */
-		_colorMapSize = globalColor+AdditionalColorMapSize+1;
-		Color *tempMap = new Color[_colorMapSize];
-		for(int i=0; i < _numColors; i++)
-			tempMap[i] = _colorMap[i];
-		delete _colorMap;
-		_colorMap = tempMap;
-	}
-	_numColors = globalColor+1;
-	_colorMap[globalColor] = localColor;
+
+void DeviseDisplay::MapColor(Color localColor, Color globalColor)
+{
+  if (globalColor >= _colorMapSize - 1){
+    /* overflow, create a new array  */
+    _colorMapSize = globalColor + AdditionalColorMapSize + 1;
+    Color *tempMap = new Color[_colorMapSize];
+    for(int i = 0; i < _numColors; i++)
+      tempMap[i] = _colorMap[i];
+    delete _colorMap;
+    _colorMap = tempMap;
+  }
+  _numColors = globalColor + 1;
+  _colorMap[globalColor] = localColor;
 }
 
 /**************************************************************************
 get local color given global color. 
 ***************************************************************************/
-Color DeviseDisplay::GetLocalColor(Color globalColor){
-	if (globalColor >= _numColors){
-		fprintf(stderr,"Display::GetLocalColor: undefined color %d\n",
-			globalColor);
-		Exit::DoExit(1);
-	}
-	return _colorMap[globalColor];
+
+Color DeviseDisplay::GetLocalColor(Color globalColor)
+{
+  if (globalColor >= _numColors){
+    fprintf(stderr,"Display::GetLocalColor: undefined color %ld\n",
+	    globalColor);
+    Exit::DoExit(1);
+  }
+  return _colorMap[globalColor];
 }
 
-char *DeviseDisplay::DispatchedName(){
-	return "Display";
+char *DeviseDisplay::DispatchedName()
+{
+  return "Display";
 }
