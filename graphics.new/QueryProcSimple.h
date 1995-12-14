38dcd68ffa1d05a2fe9592c99adee0e31c87b4ab
jussi <>
1995-12-14 17:43:39 +0000

    Small fix to get rid of g++ -Wall warnings.

--- graphics.new/QueryProcSimple.h
+++ graphics.new/QueryProcSimple.h
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:18  jussi
+  Added CVS header.
+*/
 
 #ifndef QueryProcSimple_h
 #define QueryProcSimple_h
@@ -181,9 +198,6 @@ private:
 
 	/* Scatter plot */
 	void ProcessScatterQuery();
-
-	/* record number being scanned, for either regular scan or binary search,
-	RecId _tScanId;	/* for tdata */
 
 	/* regular TData scan */
 	void DoScan();
