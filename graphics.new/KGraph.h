fc1550042842f40e14a915f9d9f722d0c0cb7be3
ravim <>
1995-12-16 00:48:20 +0000

    Handles window events for popup and resize.

--- graphics.new/KGraph.h
+++ graphics.new/KGraph.h
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.3  1995/12/08 23:44:53  ravim
+  Polygon filled.
+
   Revision 1.2  1995/12/06 05:44:38  ravim
   Initial version.
 */
@@ -26,11 +29,11 @@
 #include "Geom.h"
 #include "Display.h"
 
-class KGraph 
+class KGraph: protected WindowRepCallback
 {
 public:
   KGraph(DeviseDisplay *dis);
-  ~KGraph();
+  virtual ~KGraph();
 
   // Initialize the settings
   void Init(char *winname, char *statname);
@@ -64,6 +67,16 @@ private:
   void Rotate(Coord xval, int degree, Coord &retx, Coord &rety);
   Coord Scale(Coord xval, Coord max);
 
+  // Handle window events
+  virtual void HandlePress(WindowRep *w, int xlow, int ylow, int xhigh,
+			   int yhigh, int button);
+  
+  virtual Boolean HandlePopUp(WindowRep *w, int x, int y, int button, 
+			      char **&msgs, int &numMsgs);
+  virtual void HandleResize(WindowRep *w, int xlow, int ylow, unsigned width,
+			    unsigned height);
+
+  char **msgBuf;
 };
 
 #endif
