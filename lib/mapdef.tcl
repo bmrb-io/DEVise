ddcbc4ce3e05529d9f494e70c71ee5e1a5c7baaf
ravim <>
1995-12-20 02:24:35 +0000

    idxFormat and LoadPgm added to handle automatic generation of coral pgms.

--- lib/mapdef.tcl
+++ lib/mapdef.tcl
@@ -1,3 +1,21 @@
+# This file contains several pieces of information that are relevant while
+# generating the mapping table and while using them for visualization of
+# data
+
+# Format of the index files as a list of pairs of the form ...
+# <field name, type> where the type can be int or string (CORAL types)
+
+set idxFormat(COMPUSTAT) {{OFFSET int} {DNUM int} {CUSIP string} {CIC string} \
+	{NAME string} {SMBL string} {YEAR int} {STATE int} {COUNTY int}}
+set idxFormat(CRSP) {{OFFSET int} {STARTREC int} {PERMNO int} {CUSIP string} \
+	{HEXCD int} {HSICCD int} {NAME string} {BEGDAT int} {ENDDAT int}}
+
+
+# Programs to convert index files into coral loader programs
+
+set LoadPgm(COMPUSTAT) compcreate
+set LoadPgm(CRSP) crspcreate
+
 
 # Mapping tables for different pairs of data types
 # The info for a data type A consists of a list of entries each of which is:
