73817756a4a679bf75ba0c12eb2ecef1968b89a4
jussi <>
1995-12-28 20:47:21 +0000

    Added copyright notice and cleaned up the code a bit.

--- graphics.new/ActionDefault.h
+++ graphics.new/ActionDefault.h
@@ -1,41 +1,54 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:02  jussi
+  Added CVS header.
+*/
 
 #ifndef ActionDefault_h
 #define ActionDefault_h
+
 #include "Action.h"
 #include "Util.h"
 
 /* An action acts on events. Currently we support only
-symol selected event*/
+   symbol selected event */
 
 class ActionDefault: public Action {
 public:
-	ActionDefault(char *name, Coord leftEdge = 0.0, 
+  ActionDefault(char *name, Coord leftEdge = 0.0, 
 		Boolean useLeftFlag = false,
 		Coord rightEdge = 0.0, Boolean useRightFlag = false);
 
-	virtual void AreaSelected(View *, Coord /*xlow*/, Coord /*ylow*/,
-		Coord /*xhigh*/, Coord /*yhigh*/, int /*button*/);
+  void KeySelected(View *, char c, Coord x, Coord y);
 
-	/* key pressed */
-	virtual void KeySelected(View *, char c, Coord x, Coord y);
-
-	virtual Boolean PopUp(View *view, Coord x, Coord y, Coord xHigh,
-			Coord yHigh,
-			int /* button*/, char **& msgs, int & numMsgs);
+  Boolean PopUp(View *view, Coord x, Coord y, Coord xHigh,
+		Coord yHigh, int button, char **& msgs, int & numMsgs);
 
 private:
-	Boolean PrintRecords(View *view, Coord x, Coord y, Coord xHigh, Coord yHigh,
-		char *&errorMsg);
+  Boolean PrintRecords(View *view, Coord x, Coord y, Coord xHigh, Coord yHigh,
+		       char *&errorMsg);
 
-Coord left, right;
-Boolean useLeft, useRight;
-private:
-char buf1[80];
-char buf2[80];
-char *msgBuf[2];
+  Coord left, right;
+  Boolean useLeft, useRight;
+
+  char buf1[80];
+  char buf2[80];
+  char *msgBuf[2];
 };
 #endif
--- graphics.new/RecInterp.h
+++ graphics.new/RecInterp.h
@@ -1,12 +1,28 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
-
-/* Interpreted record */
+  $Log$
+  Revision 1.2  1995/09/05 22:15:31  jussi
+  Added CVS header.
+*/
 
 #ifndef RecInterp_h
 #define RecInterp_h
+
 #include <sys/types.h>
 #include <sys/time.h>
 #include "DeviseTypes.h"
@@ -15,31 +31,32 @@ class AttrList;
 class AttrInfo;
 class RecInterp {
 public:
-	RecInterp();
+  RecInterp();
 
-	void SetBuf(void *buf);
-	void *GetBuf() {return _buf;}
+  void SetBuf(void *buf);
+  void *GetBuf() {return _buf;}
 
-	void SetAttrs(AttrList *attrs);
+  void SetAttrs(AttrList *attrs);
 
-	/* Get the address of attributes, or NULL if not found */
-	char *GetString(char *attrName);
-	double *GetFloat(char *attrName);
-	int *GetInt(char *attrName);
-	time_t *GetDate(char *attrName);
+  /* Get the address of attributes, or NULL if not found */
+  char *GetString(char *attrName);
+  double *GetFloat(char *attrName);
+  int *GetInt(char *attrName);
+  time_t *GetDate(char *attrName);
+  
+  /* Get attribute info, or NULL */
+  AttrInfo *GetAttrInfo(char *attrName);
 
-	/* Get attribute info, or NULL */
-	AttrInfo *GetAttrInfo(char *attrName);
+  /* print Attr names as heading */
+  void PrintAttrHeading(); 
+  /* Print record */
+  void Print(Boolean printAttrName = false);
+  
+  /* Print info about ith attribute into buffer. */
+  void PrintAttr(char *buf, int attrNum, Boolean printAttrName = false);
 
-	/* print Attr names as heading */
-	void PrintAttrHeading(); 
-	/* Print record */
-	void Print(Boolean printAttrName = false);
-
-	/* Print info about ith attribute into buffer. */
-	void PrintAttr(char *buf, int attrNum, Boolean printAttrName = false);
 private:
-	AttrList *_attrs;
-	void *_buf;
+  AttrList *_attrs;
+  void *_buf;
 };
 #endif
