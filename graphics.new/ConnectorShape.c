683b13e06605e34c102a285c94ec205afd23aee9
jussi <>
1995-12-29 22:43:16 +0000

    Added the copyright message and cleaned up the code a bit.

--- graphics.new/ConnectorShape.c
+++ graphics.new/ConnectorShape.c
@@ -1,54 +1,72 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:38  jussi
+  Added CVS header.
+*/
 
 #include "LineConnector.h"
 
 ConnectorShapeRegistrar 
 	*ConnectorShapeRegistrar::_theConnectorShapeRegistrar = NULL;
 
-
 /******************************************************************
-constructor: This is where new shapes are added int the array
-of shapes in the shape registrar.
+  constructor: This is where new shapes are added int the array
+  of shapes in the shape registrar.
 *******************************************************************/
-ConnectorShapeRegistrar::ConnectorShapeRegistrar(){
-	
-	int i;
-	for (i=0; i < MaxConnectorShapes; i++){
-		_connectorShapes[i] = NULL;
-	}
 
-	_connectorShapes[DiscLineConnectorID] = new DiscLineConnector;
-	_connectorShapes[ContLineConnectorID] = new ContLineConnector;
-	_connectorShapes[BarConnectorID] = new BarConnector;
-	_connectorShapes[DiscLineConnector2ID] = new DiscLineConnector2;
+ConnectorShapeRegistrar::ConnectorShapeRegistrar()
+{
+  for(int i = 0; i < MaxConnectorShapes; i++)
+    _connectorShapes[i] = NULL;
+
+  _connectorShapes[DiscLineConnectorID] = new DiscLineConnector;
+  _connectorShapes[ContLineConnectorID] = new ContLineConnector;
+  _connectorShapes[BarConnectorID] = new BarConnector;
+  _connectorShapes[DiscLineConnector2ID] = new DiscLineConnector2;
 }
 
 /******************************************************************
-Register a connector shape 
+  Register a connector shape 
 *****************************************************************/
-void ConnectorShapeRegistrar::DoRegisterConnectorShape(
-	ConnectorShapeID id, ConnectorShape *cShape){
-	if (id < 0 || id >= MaxConnectorShapes){
-		fprintf(stderr,"ConnectorShapeRegistrar: id %d invalid\n",id);
-		Exit::DoExit(1);
-	}
 
-	if (_connectorShapes[id] != NULL){
-		fprintf(stderr,"ConnectorShapeRegistrar: id %d already in use\n",id);
-		Exit::DoExit(1);
-	}
-	_connectorShapes[id] = cShape;
+void ConnectorShapeRegistrar::DoRegisterConnectorShape(ConnectorShapeID id,
+						       ConnectorShape *cShape)
+{
+  if (id < 0 || id >= MaxConnectorShapes) {
+    fprintf(stderr,"ConnectorShapeRegistrar: id %d invalid\n", id);
+    Exit::DoExit(1);
+  }
+
+  if (_connectorShapes[id] != NULL) {
+    fprintf(stderr,"ConnectorShapeRegistrar: id %d already in use\n", id);
+    Exit::DoExit(1);
+  }
+
+  _connectorShapes[id] = cShape;
 }
-
 
 /***************************************************************
-draw connector
+  draw connector
 *****************************************************************/
-void ConnectorShapeRegistrar::DoDrawConnection
-	(WindowRep *win, Connector *connector){
-	_connectorShapes[connector->cShapeID]->DrawConnection(win,connector);
-}
 
+void ConnectorShapeRegistrar::DoDrawConnection(WindowRep *win,
+					       Connector *connector)
+{
+  _connectorShapes[connector->cShapeID]->DrawConnection(win, connector);
+}
--- graphics.new/ConnectorShape.h
+++ graphics.new/ConnectorShape.h
@@ -1,12 +1,30 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:39  jussi
+  Added CVS header.
+*/
 
 #ifndef ConnectorShape_h
 #define ConnectorShape_h
-#include "DeviseTypes.h"
+
 #include <stdio.h>
+#include "DeviseTypes.h"
 
 /* ID for shapes. NumShapes is used as placeholder for # of shapes. */
 typedef int ConnectorShapeID;
@@ -18,50 +36,43 @@ const ConnectorShapeID BarConnectorID = 2;
 const ConnectorShapeID DiscLineConnector2ID = 3;
 
 class WindowRep;
-
 class Connector;
+
 class ConnectorShape {
 public:
-	
-	/* Draw the shape for the symbol */
-	virtual void DrawConnection(WindowRep *win, Connector *connector)=0;
+  /* Draw the shape for the symbol */
+  virtual void DrawConnection(WindowRep *win, Connector *connector) = 0;
 };
 
-/* max connector shapes that can be registered with the connector
-registrar */
-const int MaxConnectorShapes = 20;
+/* max connector shapes that can be registered with the connector registrar */
+const int MaxConnectorShapes = 10;
 
 class ConnectorShapeRegistrar {
 public:
+  /* Register a connector shape */
+  static void RegisterConnectorShape(ConnectorShapeID id,
+				     ConnectorShape *cShape) {
+    if (_theConnectorShapeRegistrar == NULL)
+      _theConnectorShapeRegistrar = new ConnectorShapeRegistrar;
+    _theConnectorShapeRegistrar->DoRegisterConnectorShape(id, cShape);
+  }
 
-	/* Register a connector shape */
-	void RegisterConnectorShape(ConnectorShapeID id, ConnectorShape *cShape){
-		if (_theConnectorShapeRegistrar == NULL){
-			_theConnectorShapeRegistrar = new ConnectorShapeRegistrar;
-		}
-		_theConnectorShapeRegistrar->DoRegisterConnectorShape(id,cShape);
-	};
-
-	/* Draw the shape of connector */
-	static void DrawConnection(WindowRep *win, Connector *connector){
-		if (_theConnectorShapeRegistrar == NULL){
-			_theConnectorShapeRegistrar = new ConnectorShapeRegistrar;
-		}
-		_theConnectorShapeRegistrar->DoDrawConnection(win,connector);
-	};
+  /* Draw the shape of connector */
+  static void DrawConnection(WindowRep *win, Connector *connector) {
+    if (_theConnectorShapeRegistrar == NULL)
+      _theConnectorShapeRegistrar = new ConnectorShapeRegistrar;
+    _theConnectorShapeRegistrar->DoDrawConnection(win, connector);
+  }
 
 private:
-	/* constructor */
-	ConnectorShapeRegistrar();
+  ConnectorShapeRegistrar();
 
-	void DoRegisterConnectorShape(ConnectorShapeID id, ConnectorShape *cShape);
+  void DoRegisterConnectorShape(ConnectorShapeID id, ConnectorShape *cShape);
+  void DoDrawConnection(WindowRep *win, Connector *connector);
 
-	void DoDrawConnection(WindowRep *win, Connector *connector);
+  static ConnectorShapeRegistrar *_theConnectorShapeRegistrar; 
 
-
-ConnectorShape *_connectorShapes[MaxConnectorShapes];
-/*the one and only shape registrar*/
-static ConnectorShapeRegistrar *_theConnectorShapeRegistrar; 
+  ConnectorShape *_connectorShapes[MaxConnectorShapes];
 };
 
 #endif
