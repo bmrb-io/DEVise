b22dcac1c8492388b8bc0c15cf26d2e3507907e3
jussi <>
1995-12-29 18:28:18 +0000

    Added FilterAboutToChange() method to facilitate the new cursor mechanism.

--- graphics/ViewCallback.h
+++ graphics/ViewCallback.h
@@ -1,28 +1,51 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1995
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 21:13:18  jussi
+  Added/updated CVS header.
+*/
 
 #ifndef ViewCallback_h
 #define ViewCallback_h
+
 #include "DeviseTypes.h"
 #include "DList.h"
 
-/* Report filter added for a view */
 class View;
+
 class ViewCallback {
 public:
-	/* Called by View when its visual filter has changed.
-	flushed == index of 1st element in the history that has been flushed,
-		or -1 if none. index 0 == oldest element .*/
-	virtual void FilterChanged(View *view, VisualFilter &filter,
-		int flushed) = 0;
+  /* Called by View when its visual filter is about to be changed. */
+  virtual void FilterAboutToChange(View *view) = 0;
 
-	virtual void ViewCreated(View *view)=0;
-	virtual void ViewDestroyed(View *view)=0;
+  /* Called by View when its visual filter has changed.
+     flushed == index of 1st element in the history that has been flushed,
+     or -1 if none. index 0 == oldest element .*/
+  virtual void FilterChanged(View *view, VisualFilter &filter,
+			     int flushed) = 0;
+  
+  /* Called by View when view is created. */
+  virtual void ViewCreated(View *view) = 0;
+
+  /* Called by View when view is destroyed */
+  virtual void ViewDestroyed(View *view) = 0;
 };
 
 DefinePtrDList(ViewCallbackList, ViewCallback *);
-
 
 #endif
