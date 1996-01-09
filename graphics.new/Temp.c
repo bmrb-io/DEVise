1a56cb1a4edcc036537abc757ee719d85a973ee1
jussi <>
1996-01-09 22:14:12 +0000

    Added support for 3D data.

--- graphics.new/Temp.c
+++ graphics.new/Temp.c
@@ -1,11 +1,30 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:06  jussi
+  Added CVS header.
+*/
 
 #include "Temp.h"
 
 Coord _x[WINDOWREP_BATCH_SIZE];
 Coord _y[WINDOWREP_BATCH_SIZE];
+Coord _z[WINDOWREP_BATCH_SIZE];
 Coord _width[WINDOWREP_BATCH_SIZE];
 Coord _height[WINDOWREP_BATCH_SIZE];
+Coord _depth[WINDOWREP_BATCH_SIZE];
--- graphics.new/Temp.h
+++ graphics.new/Temp.h
@@ -1,16 +1,36 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:07  jussi
+  Added CVS header.
+*/
 
 #ifndef Temp_h
 #define Temp_h
+
 #include "DeviseTypes.h"
 #include "WindowRep.h"
 
 extern Coord _x[WINDOWREP_BATCH_SIZE];
 extern Coord _y[WINDOWREP_BATCH_SIZE];
+extern Coord _z[WINDOWREP_BATCH_SIZE];
 extern Coord _width[WINDOWREP_BATCH_SIZE];
 extern Coord _height[WINDOWREP_BATCH_SIZE];
+extern Coord _depth[WINDOWREP_BATCH_SIZE];
 
 #endif
