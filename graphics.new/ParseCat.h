d67d9aade8ae717b0e6793de31a0e5ba8e37aa71
jussi <>
1996-01-10 18:46:29 +0000

    Removed unnecessary function prototypes.

--- graphics.new/ParseCat.h
+++ graphics.new/ParseCat.h
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
   $Log$
+  Revision 1.6  1995/12/20 07:04:45  ravim
+  High and low values of attrs can be specified.
+
   Revision 1.5  1995/12/13 02:07:50  ravim
   Groups identified by logical schema file name - new function to extract the
   file name from a path string.
@@ -18,35 +35,31 @@
 
 #ifndef ParseCat_h
 #define ParseCat_h
+
 #include "DeviseTypes.h"
 #include "ClassDir.h"
 #include "AttrList.h"
 
 /* Parse a catalog file and register new file type with the system.
-Return name of new file type if successful, else return NULL */
+   Return name of new file type if successful, else return NULL */
 extern char *ParseCat(char *catFile);
-extern char *ParseCatOriginal(char *catFile);
-extern char *ParseCatPhysical(char *catFile);
-extern char *ParseCatLogical(char *catFile, char *sname);
-extern char *getTail(char *fname);
-extern void SetVal(AttrVal *aval, char *valstr, AttrType valtype);
-
 
 /* Register a new constructor for class. The
-constructor is called depending on the "source" statment stored
-in the catalog file. For example, "source tape" will call
-the function that generates a TData class that reads from tape */
+   constructor is called depending on the "source" statment stored
+   in the catalog file. For example, "source tape" will call
+   the function that generates a TData class that reads from tape */
+
 class AttrList;
 class GenClassInfo {
 public:
-/* Generate a new TData interpreter */
-    virtual ClassInfo *Gen(char *source, char *className, AttrList *attrList,
-		int recSize, char *separators, int numSeparators,
-		Boolean isSeparator, char *commentString)= 0;
+  /* Generate a new TData interpreter */
+  virtual ClassInfo *Gen(char *source, char *className, AttrList *attrList,
+			 int recSize, char *separators, int numSeparators,
+			 Boolean isSeparator, char *commentString) = 0;
 
 };
-extern void RegisterGenClassInfo(char *source, GenClassInfo *gen);
 
+extern void RegisterGenClassInfo(char *source, GenClassInfo *gen);
 
 /* Return a list of catalog files */
 extern void CatFiles(int &numFiles, char **&fileNames);
