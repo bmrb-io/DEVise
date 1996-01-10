a292db322d8c21ebcff9d4b02e5e2fe7795b603a
jussi <>
1996-01-10 18:45:32 +0000

    Removed bugs and improved code.

--- graphics/Parse.c
+++ graphics/Parse.c
@@ -1,7 +1,7 @@
 /*
   ========================================================================
   DEVise Data Visualization Software
-  (c) Copyright 1992-1995
+  (c) Copyright 1992-1996
   By the DEVise Development Group
   Madison, Wisconsin
   All Rights Reserved.
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.4  1995/12/22 20:00:34  jussi
+  Fixed time zone item in tm structure.
+
   Revision 1.3  1995/12/14 17:06:04  jussi
   Added copyright notice and made small fixes.
 
@@ -29,6 +32,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <libc.h>
+
 #include "Config.h"
 #include "Exit.h"
 
@@ -60,65 +64,64 @@ void Parse(char *str, int &numArgs, char **&returnArgs, char *blanks,
 {
   returnArgs = args;
   numArgs = 0;
-  if (!isSeparator){
-    /* process line with whitespace */
+
+  if (!isSeparator) {
     str = SkipBlanks(str, blanks, numBlanks);
-    while (*str != '\0'){
-      /* If we got here, *str is not a blank. skip non-blank 
-	 to find next blank */
+
+    while(*str) {
       char *start = str;
-      if (*str == '\'' ){
+      if (*str == '\'') {
 	/* reading a literal */
 	str++;
 	start = str;
-	while (*str != '\0' && *str != '\'')
+	while(*str && *str != '\'')
 	  str++;
-	if (*str == '\0')
-	  break;
+      } else {
+	while(*str && !IsBlank(*str, blanks, numBlanks)) 
+	  str++;
       }
-      else while (*str != '\0' && !IsBlank(*str, blanks, numBlanks)) 
-	str++;
-      /* make an arg */
-      if (*str != '\0'){
-	*str = '\0';
+
+      if (*str) {
+	*str = 0;
 	str++;
       }
       
-      if (numArgs >= MAXARGS-1){
+      if (numArgs >= MAXARGS - 1) {
 	fprintf(stderr,"parse: no more args\n");
 	Exit::DoExit(1);
       }
       args[numArgs++] = start;
-      
       str = SkipBlanks(str, blanks, numBlanks);
     }
-  } else {
-    /* process line with separators */
-    char *start;
-    do {
-      start = str;
-      while (*str != '\0' && !IsBlank(*str, blanks, numBlanks)){
-	str++;
-      }
-      /* from start to str is the field */
-      char *end = str;
-      if (*str != '\0')
-	str++;
-      *end = '\0';
-      
-      if (numArgs >= MAXARGS-1){
-	fprintf(stderr,"parse: no more args\n");
-	Exit::DoExit(1);
-      }
-      args[numArgs++] = start;
-    } while (*str != '\0');
+
+    return;
   }
+
+  /* process line with separators */
+  char *start;
+  do {
+    start = str;
+    while(*str && !IsBlank(*str, blanks, numBlanks))
+      str++;
+
+    /* from start to str is the field */
+    if (*str) {
+      *str = 0;
+      str++;
+    }
+    
+    if (numArgs >= MAXARGS - 1) {
+      fprintf(stderr, "parse: too many args\n");
+      Exit::DoExit(1);
+    }
+    args[numArgs++] = start;
+  } while(*str);
 }
 
 void Parse(char *str, int &numArgs, char **&returnArgs)
 {
   char blanks[2] = {' ', '\t'};
-  Parse(str,numArgs, returnArgs, blanks, 2, 0);
+  Parse(str, numArgs, returnArgs, blanks, 2, 0);
 }
 
 static char *monthNames[12] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
@@ -127,12 +130,11 @@ static char *monthNames[12] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
 static int GetMonth(char *month)
 {
   static int monthHint = -1;
-  if (monthHint >= 0 && strcmp(monthNames[monthHint], month) == 0) {
+  if (monthHint >= 0 && !strcmp(monthNames[monthHint], month) == 0)
     return monthHint;
-  }
 
   for(int i = 0; i < 12; i++) {
-    if (strcmp(monthNames[i], month) == 0) {
+    if (!strcmp(monthNames[i], month)) {
       monthHint = i;
       return i;
     }
@@ -150,21 +152,25 @@ int ParseFloatDate(char *input, double &val)
 {
   char line[256];
   strcpy(line, input);
+
+  // convert string to upper case
   char *ptr = line;
-  for (; *ptr != '\0'; ptr++){
-    if (isalpha(*ptr)) *ptr  = toupper(*ptr);
-  }
+  for(; *ptr; ptr++)
+    if (isalpha(*ptr))
+      *ptr = toupper(*ptr);
 
   int numArgs;
   char **args;
   Parse(line, numArgs, args);
 
-  struct tm timestr;
+  // if there is a single argument, it is the date already in numeric format
   if (numArgs == 1) {
     val = atof(args[0]);
     return 1;
   }
-  else if (numArgs == 4) {
+
+  // otherwise, date must be in the format: MMM DD YYYY HH:MM:SS
+  if (numArgs == 4) {
     if (strlen(args[0]) != 3)
       goto error;
     if (strlen(args[2]) != 4)
@@ -172,6 +178,7 @@ int ParseFloatDate(char *input, double &val)
     if (strlen(args[3]) != 8)
       goto error;
     
+    struct tm timestr;
     timestr.tm_mon = GetMonth(args[0]);
     timestr.tm_mday = atoi(args[1]);
     timestr.tm_year = atoi(args[2])- 1900;
