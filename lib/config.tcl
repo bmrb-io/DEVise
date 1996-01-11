36f0d4d0e43691982a334ee89d2965b8b6777606
jussi <>
1996-01-11 18:59:02 +0000

    Changed hostnames for a couple of data sources.

--- lib/config.tcl
+++ lib/config.tcl
@@ -1,11 +1,11 @@
 # Data source configuration
 
 set "sourceConfig(WWW)" {www_fetch}
-set "sourceConfig(NETWORK)" {network_client blondie 1400}
+set "sourceConfig(NETWORK)" {network_client blondie.cs.wisc.edu 1400}
 set "sourceConfig(UNIXFILE)" {}
 set "sourceConfig(COMMAND)" {}
 set "sourceConfig(COMPUSTAT)" {/dev/nrst4 23 512 32768}
 set "sourceConfig(CRSP)" {/dev/nrst4 20 512 32768}
 set "sourceConfig(ISSM)" {/dev/nrst4 4 0 32768}
-set "sourceConfig(SEQ)" {paradise 6003}
-set "sourceConfig(SQL)" {sql_client paradise 130}
+set "sourceConfig(SEQ)" {sun22.cs.wisc.edu 6003}
+set "sourceConfig(SQL)" {sql_client paradise.cs.wisc.edu 130}
