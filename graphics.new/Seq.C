6b16bd67e1da9634f0b2852be6f7e5a069f026e2
jussi <>
1996-01-11 21:00:02 +0000

    Added parsing of range information and reduced data buffer size to 16 kB.

--- graphics.new/Seq.C
+++ graphics.new/Seq.C
@@ -16,6 +16,11 @@
   $Id$
 
   $Log$
+  Revision 1.2  1996/01/10 00:40:52  jussi
+  Added computation of hi/lo values in schema. Improved data buffering
+  so that data can be read in small (32 kB) chunks instead of reading
+  into memory in one large chunk.
+
   Revision 1.1  1996/01/09 16:33:44  jussi
   Initial revision.
 */
@@ -32,7 +37,6 @@
 #include <iostream.h>
 #include <assert.h>
 #include <ctype.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -60,11 +64,44 @@ extern "C" {
 }
 #endif
 
+#include "Util.h"
+#include "Parse.h"
+
 //#define DEBUG
+//#define DEBUG_IO
+
+#define COMPUTE_MIN_MAX
+#define USE_RANGE_INFO
+
+static const char *resultHeader = "Result of Execution\n";
+static const char *schemaFmt =
+	"Schema ( < %[^>]> ) OrderAttr %d density %f, range %n";
+static const char *dateAttr = "hmdy";
+static const char *intAttr = "int";
+static const char *doubleAttr = "double";
+static const char *stringAttr = "string";
+static const char *attrSep = ", ";
+static const char attrTypeSep = ':';
+static const char *rangeSep = " ... ";
+static const char dataFieldSep = '\t';
+
+static const char newline    = '\n';
+static const char dateChar   = 'd';
+static const char intChar    = 'i';
+static const char doubleChar = 'f';
+static const char stringChar = 's';
+
+static char *schema = 0;
+static const int maxNumAttrs = 16;
+static char schemaDef[maxNumAttrs + 1];
+static double minVal[maxNumAttrs];
+static double maxVal[maxNumAttrs];
+static int orderAttr = -1;
+static float density = 0.0;
 
 static int readn(int fd, char *buf, int nbytes)
 {
-#ifdef DEBUG
+#ifdef DEBUG_IO
   char *orig = buf;
 #endif
 
@@ -81,17 +118,15 @@ static int readn(int fd, char *buf, int nbytes)
       break;
   }
 
-#ifdef DEBUG
+#ifdef DEBUG_IO
   printf("readn: got %d bytes: %.*s\n", nbytes - nleft, nbytes - nleft, orig);
 #endif
 
   return nbytes - nleft;
 }
   
-int ParseSEQHeader(char *&buf, int &len)
+static int ParseSEQHeader(char *&buf, int &len)
 {
-  const char *resultHeader = "Result of Execution\n";
-
   if (memcmp(buf, resultHeader, strlen(resultHeader))) {
     fprintf(stderr, "Unexpected header from SEQ server:\n");
     fprintf(stderr, "  %.256s\n", buf);
@@ -104,25 +139,14 @@ int ParseSEQHeader(char *&buf, int &len)
   return TCL_OK;
 }
 
-int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
-		   char *&schema, double *&minVal, double *&maxVal,
-		   char *&sdef)
+static int ParseSEQSchema(char *&buf, int &len, char *schemafile,
+			  char *schematype)
 {
-  const char *schemaHdr = "Schema ( < ";
-  const char *orderAttr = " >) OrderAttr ";
-  const char *dateField = "hmdy";
-  const char *intField = "int";
-  const char *floatField = "float";
-  const char *stringField = "string";
-  const char *attrSep = ", ";
-  const char attrTypeSep = ':';
-
   // when this function is called the first time, space is allocated for
-  // for sdef and the schema is stored there in SEQ language;
-  // upon the next call, the schema is used from sdef
+  // for schema and minVal/maxVal are initialized
 
-  if (!sdef) {
-    char *eol = (char *)memchr(buf, '\n', len);
+  if (!schema) {
+    char *eol = (char *)memchr(buf, newline, len);
     if (!eol) {
       fprintf(stderr, "Unexpected schema from SEQ server:\n");
       fprintf(stderr, "  %.256s\n", buf);
@@ -131,34 +155,64 @@ int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
 
     *eol = 0;
 
-    sdef = new char [strlen(buf) + 1];
-    if (!sdef) {
-      fprintf(stderr, "Cannot allocate schema definition.\n");
-      return TCL_ERROR;
+#ifdef DEBUG
+    printf("Initializing schema definition %s\n", buf);
+#endif
+
+    schema = CopyString(buf);
+    memset(schemaDef, 0, maxNumAttrs + 1);
+    for(int a = 0; a < maxNumAttrs; a++) {
+      minVal[a] = 1.0;
+      maxVal[a] = 0.0;
     }
-    strcpy(sdef, buf);
-    
+    orderAttr = -1;
+    density = 0.0;
+
     eol++;
     len -= (eol - buf);
     buf = eol;
   }
 
-  if (strncmp(sdef, schemaHdr, strlen(schemaHdr))) {
-    fprintf(stderr, "Invalid schema header: %s\n", sdef);
-    return TCL_ERROR;
-  }
-  char *attrName = &sdef[strlen(schemaHdr)];
+  assert(schema);
 
-  int sortedAttr = -1;
-  char *order = strchr(sdef, orderAttr[1]);
-  if (order && !strncmp(order - 1, orderAttr, strlen(orderAttr))
-      && isdigit(*(order - 1 + strlen(orderAttr)))) {
-    sortedAttr = atoi(order - 1 + strlen(orderAttr));
-    *(order - 1) = 0;
-  } else {
-    fprintf(stderr, "Invalid schema definition: %s\n", sdef);
+  // allocate space that is large enough to hold the attribute
+  // list; schema definition includes it so it must be enough
+  // space
+  char attrDef[strlen(schema) + 1];
+
+  int bytes;
+  if (sscanf(schema, schemaFmt, attrDef, &orderAttr, &density, &bytes) < 3) {
+    fprintf(stderr, "Invalid schema: %s\n", schema);
     return TCL_ERROR;
   }
+
+  // remove trailing blank from attribute definition
+  attrDef[strlen(attrDef) - 1] = 0;
+
+#ifdef USE_RANGE_INFO
+  char *rangeDef = &schema[bytes];
+
+  // isolate low and high values in the range definition
+  char *rangeBegin = rangeDef;
+  char *rangeEnd = strstr(rangeDef, rangeSep);
+  if (!rangeEnd) {
+    fprintf(stderr, "Range end not specified: %s\n", rangeDef);
+    return TCL_ERROR;
+  }
+  rangeEnd += strlen(rangeSep);
+
+  // attach range information to corresponding attribute; note that
+  // the range information may be parsed incorrectly (as floats) the
+  // first time the schema is parsed; in the second round, the attribute
+  // types are known and dates will be parsed accordingly
+  if (schemaDef[orderAttr] == dateChar) {
+    minVal[orderAttr] = ParseSQLTimestamp(rangeBegin);
+    maxVal[orderAttr] = ParseSQLTimestamp(rangeEnd);
+  } else {
+    minVal[orderAttr] = atof(rangeBegin);
+    maxVal[orderAttr] = atof(rangeEnd);
+  }
+#endif
 
   FILE *fp = 0;
 
@@ -175,30 +229,11 @@ int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
     fprintf(fp, "separator ','\n");
   }
 
-  const int maxNumAttrs = 16;
-  static char schemaDef[maxNumAttrs + 1];
-  static double minVals[maxNumAttrs];
-  static double maxVals[maxNumAttrs];
-
-  if (!schema) {
-    assert(!minVal && !maxVal);
-    schema = schemaDef;
-    minVal = minVals;
-    maxVal = maxVals;
-    memset(schemaDef, 0, maxNumAttrs + 1);
-    for(int a = 0; a < maxNumAttrs; a++) {
-      minVal[a] = 1.0;
-      maxVal[a] = 0.0;
-    }
-  } else {
-    assert(minVal && maxVal);
-  }
-
-  char *beginDef = attrName;
+  char *attrName = attrDef;
 
   for(int i = 0;; i++) {
     if (i >= maxNumAttrs) {
-      fprintf(stderr, "Too many attributes in schema: %s\n", beginDef);
+      fprintf(stderr, "Too many attributes in schema: %s\n", attrDef);
       if (fp)
 	fclose(fp);
       return TCL_ERROR;
@@ -215,50 +250,56 @@ int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
     colon++;
 
     if (fp) {
-      if (i == sortedAttr)
+      if (i == orderAttr)
 	fprintf(fp, "sorted ");
       fprintf(fp, "attr %s ", attrName);
     }
 
     char *nextAttr = 0;
-    if (!strncmp(colon, dateField, strlen(dateField))) {
-      if (schema[i] == 0)
-	schema[i] = 'd';
-      if (fp)
-	fprintf(fp, "date hi %ld lo %ld\n", (long)maxVal[i], (long)minVal[i]);
-      nextAttr = colon + strlen(dateField);
-    } else if (!strncmp(colon, intField, strlen(intField))) {
-      if (schema[i] == 0)
-	schema[i] = 'i';
-      if (fp)
-	fprintf(fp, "int hi %ld lo %ld\n", (long)maxVal[i], (long)minVal[i]);
-      nextAttr = colon + strlen(intField);
-    } else if (!strncmp(colon, floatField, strlen(floatField))) {
-      if (schema[i] == 0)
-	schema[i] = 'f';
-      if (fp)
-	fprintf(fp, "float hi %g lo %g\n", minVal[i], maxVal[i]);
-      nextAttr = colon + strlen(floatField);
-    } else if (!strncmp(colon, stringField, strlen(stringField))) {
-      if (schema[i] == 0)
-	schema[i] = 's';
+    if (!strncmp(colon, dateAttr, strlen(dateAttr))) {
+      schemaDef[i] = dateChar;
+      if (fp) {
+	fprintf(fp, "date");
+	if (maxVal[i] >= minVal[i])
+	  fprintf(fp, " hi %ld lo %ld", (long)maxVal[i], (long)minVal[i]);
+      }
+      nextAttr = colon + strlen(dateAttr);
+    } else if (!strncmp(colon, intAttr, strlen(intAttr))) {
+      schemaDef[i] = intChar;
+      if (fp) {
+	fprintf(fp, "int");
+	if (maxVal[i] >= minVal[i])
+	  fprintf(fp, " hi %ld lo %ld", (long)maxVal[i], (long)minVal[i]);
+      }
+      nextAttr = colon + strlen(intAttr);
+    } else if (!strncmp(colon, doubleAttr, strlen(doubleAttr))) {
+      schemaDef[i] = doubleChar;
+      if (fp) {
+	fprintf(fp, "double");
+	if (maxVal[i] >= minVal[i])
+	  fprintf(fp, " hi %g lo %g", maxVal[i], minVal[i]);
+      }
+      nextAttr = colon + strlen(doubleAttr);
+    } else if (!strncmp(colon, stringAttr, strlen(stringAttr))) {
+      schemaDef[i] = stringChar;
       if (fp)
 	fprintf(fp, "string");
-      nextAttr = colon + strlen(stringField);
+      nextAttr = colon + strlen(stringAttr);
     } else {
-      fprintf(stderr, "Invalid field type: %s\n", attrName);
+      fprintf(stderr, "Invalid attribute type: %s\n", colon);
       if (fp)
 	fclose(fp);
       return TCL_ERROR;
     }
 
-    *(colon - 1) = attrTypeSep;
+    if (fp)
+      fprintf(fp, "\n");
 
     if (!*nextAttr)
       break;
 
     if (strncmp(nextAttr, attrSep, strlen(attrSep))) {
-      fprintf(stderr, "Invalid field separator: %s\n", nextAttr);
+      fprintf(stderr, "Invalid attribute separator: %s\n", nextAttr);
       if (fp)
 	fclose(fp);
       return TCL_ERROR;
@@ -267,11 +308,9 @@ int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
     attrName = nextAttr + strlen(attrSep);
   }
   
-  *(order - 1) = orderAttr[0];
-
 #ifdef DEBUG
-  printf("Parsed schema as %s, ordered attribute %d\n", schema,
-	 sortedAttr);
+  printf("Parsed schema as %s, ordered attribute %d\n", schemaDef,
+	 orderAttr);
 #endif
 
   if (fp && fclose(fp)) {
@@ -283,79 +322,60 @@ int ParseSEQSchema(char *&buf, int &len, char *schemafile, char *schematype,
   return TCL_OK;
 }
 
-int WriteSEQRecord(char *rec, int len, FILE *fp, char *cachefile,
-		   char *schema, double *minVal, double *maxVal)
+static int WriteSEQRecord(char *rec, int len, FILE *fp, char *cachefile)
 {
-  assert(rec && fp && cachefile && schema && minVal && maxVal);
+  assert(rec && fp && cachefile);
 
-#ifdef DEBUG
+#ifdef DEBUG_IO
   printf("Converting record (%d bytes): %s\n", len, rec);
 #endif
 
-  const char attrSep = '\t';
+  const int numAttrs = strlen(schemaDef);
 
-  for(int i = 0; i < strlen(schema); i++) {
-    char *fieldSep = strchr(rec, attrSep);
+  for(int i = 0; i < numAttrs; i++) {
+    char *fieldSep = strchr(rec, dataFieldSep);
     if (!fieldSep) {
-      fprintf(stderr, "Illegal data record: %s\n", rec);
+      fprintf(stderr, "Illegal data record at field %d: %s\n", i, rec);
       fclose(fp);
       return TCL_ERROR;
     }
     *fieldSep = 0;
 
-    if (schema[i] == 'd') {
-      // convert SQL time format to Unix time format
-      struct tm t;
-      if (sscanf(rec, "%d-%d-%d %d:%d:%d", &t.tm_year, &t.tm_mon,
-		 &t.tm_mday, &t.tm_hour, &t.tm_min, &t.tm_sec) != 6) {
-	fprintf(stderr, "Invalid date field: %s\n", rec);
-	fclose(fp);
-	return TCL_ERROR;
-      }
-      // HACK HACK until seq try1 relation fixed
-#if 1
-      t.tm_year += 1896;
+#ifdef COMPUTE_MIN_MAX
+    double value = 0.0;
 #endif
-      t.tm_year -= 1900;
-      t.tm_mon--;
-      t.tm_isdst = -1;
-      time_t tim = mktime(&t);
-#ifdef DEBUG
-      printf("Converting time %04d-%02d-%02d %02d:%02d:%02d\n", t.tm_year,
-	     t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
-#endif
+
+    if (schemaDef[i] == dateChar) {
+      time_t tim = ParseSQLTimestamp(rec);
       if (tim == (time_t)-1) {
 	fprintf(stderr, "Invalid date field: %s\n", rec);
 	fclose(fp);
 	return TCL_ERROR;
       }
       fprintf(fp, "%ld", (long int)tim);
-      if (minVal[i] > maxVal[i])
-	minVal[i] = maxVal[i] = (double)tim;
-      else if (tim < minVal[i])
-	minVal[i] = (double)tim;
-      else if (tim > maxVal[i])
-	maxVal[i] = (double)tim;
-    } else if (schema[i] == 'i' || schema[i] == 'd') {
+#ifdef COMPUTE_MIN_MAX
+      value = (double)tim;
+#endif
+    } else {
       // write other attribute types as-is
       fwrite(rec, strlen(rec), 1, fp);
-      double value = atof(rec);
-      if (minVal[i] > maxVal[i])
-	minVal[i] = maxVal[i] = value;
-      else if (value < minVal[i])
-	minVal[i] = value;
-      else if (value > maxVal[i])
-	maxVal[i] = value;
-    } else if (schema[i] == 's') {
-      // write string attributes types as-is
-      fwrite(rec, strlen(rec), 1, fp);
-    } else {
-      assert(0);
+#ifdef COMPUTE_MIN_MAX
+      value = atof(rec);
+#endif
     }
+
+#ifdef COMPUTE_MIN_MAX
+    if (minVal[i] > maxVal[i])
+      minVal[i] = maxVal[i] = value;
+    else if (value < minVal[i])
+      minVal[i] = value;
+    else if (value > maxVal[i])
+      maxVal[i] = value;
+#endif
 
     rec = fieldSep + 1;
 
-    if (i < strlen(schema) - 1)
+    if (i < numAttrs - 1)
       fprintf(fp, ",");
   }
 
@@ -364,11 +384,9 @@ int WriteSEQRecord(char *rec, int len, FILE *fp, char *cachefile,
   return TCL_OK;
 }
 
-int ParseSEQData(char *buf, int size, int len, int sock,
-                 char *cachefile, char *schema, double *minVal,
-		 double *maxVal)
+int ParseSEQData(char *buf, int size, int len, int sock, char *cachefile)
 {
-  assert(buf && cachefile && schema && minVal && maxVal);
+  assert(buf && cachefile);
 
   const char *resultTrailer = "\n";
 
@@ -381,22 +399,27 @@ int ParseSEQData(char *buf, int size, int len, int sock,
 
   int pos = 0;
 
+  printf("Transferring data.");
+  fflush(stdout);
+#ifdef DEBUG
+  printf("\n");
+#endif
+
   while(1) {
     char *line = buf + pos;
     if (!memcmp(line, resultTrailer, strlen(resultTrailer)))
       break;
 
-    char *eol = (char *)memchr(line, '\n', len);
+    char *eol = (char *)memchr(line, newline, len);
 
-#ifdef DEBUG
-    printf("Pos %d, len %d, eol %d\n", pos, len, (eol - buf));
-#endif
-
-    if (!eol || *(eol + 1) != '\n') {
+    if (!eol || *(eol + 1) != newline) {
       if (line[len - 1] != 0) {
 	// terminating null not received yet, so go fetch more data
 #ifdef DEBUG
 	printf("Partial record, fetching more data\n");
+#else
+	printf(".");
+	fflush(stdout);
 #endif
 	// shift partial line to beginning of buffer
 	memcpy(buf, line, len);
@@ -409,9 +432,9 @@ int ParseSEQData(char *buf, int size, int len, int sock,
 	  return TCL_ERROR;
 	}
 	len += bytes;
-	eol = (char *)memchr(line, '\n', len);
+	eol = (char *)memchr(line, newline, len);
       }
-      if (!eol || *(eol + 1) != '\n') {
+      if (!eol || *(eol + 1) != newline) {
 	fprintf(stderr, "Unexpected data from SEQ server:\n");
 	fprintf(stderr, "  %.256s\n", line);
 	fclose(fp);
@@ -420,8 +443,7 @@ int ParseSEQData(char *buf, int size, int len, int sock,
     }
 
     *eol = 0;                           // replace newline with null
-    int status = WriteSEQRecord(line, strlen(line), fp, cachefile, schema,
-				minVal, maxVal);
+    int status = WriteSEQRecord(line, strlen(line), fp, cachefile);
     if (status != TCL_OK)
       return status;
 
@@ -429,6 +451,10 @@ int ParseSEQData(char *buf, int size, int len, int sock,
     len -= (eol - line);
     pos += (eol - line);
   }
+
+#ifndef DEBUG
+  printf("\n");
+#endif
 
   if (fclose(fp)) {
     fprintf(stderr, "Cannot close cache file %s: ", cachefile);
@@ -475,8 +501,6 @@ int seq_extract(ClientData cd, Tcl_Interp *interp, int argc, char **argv)
     return TCL_ERROR;
   }
 
-  printf("Transferring data...\n");
-
   char *cmdbuf = new char [4 + strlen(argv[3]) + 1];
   if (!cmdbuf) {
     fprintf(stderr, "Cannot allocate command buffer\n");
@@ -490,14 +514,15 @@ int seq_extract(ClientData cd, Tcl_Interp *interp, int argc, char **argv)
 	 cmdbuf);
 #endif
 
-  if (write(sock, cmdbuf, strlen(cmdbuf)) != strlen(cmdbuf)) {
+  // send query plus terminating null to SEQ server
+  if (write(sock, cmdbuf, strlen(cmdbuf) + 1) != strlen(cmdbuf) + 1) {
     perror("Error in SEQ server connection: write");
     return TCL_ERROR;
   }
   
   delete cmdbuf;
 
-  char recvBuf[32 * 1024];
+  char recvBuf[16 * 1024];
   int bytes;
   if ((bytes = readn(sock, recvBuf, sizeof recvBuf)) < 0) {
     perror("Error in SEQ server connection: read");
@@ -511,11 +536,7 @@ int seq_extract(ClientData cd, Tcl_Interp *interp, int argc, char **argv)
     return status;
   }
 
-  char *sdef = 0;                       // schema in SEQ
-  char *schema = 0;                     // attribute characters
-  double *minVal = 0;                   // minimum values
-  double *maxVal = 0;                   // maximum values
-  status = ParseSEQSchema(buf, bytes, 0, 0, schema, minVal, maxVal, sdef);
+  status = ParseSEQSchema(buf, bytes, 0, 0);
   if (status != TCL_OK) {
     close(sock);
     return status;
@@ -523,21 +544,20 @@ int seq_extract(ClientData cd, Tcl_Interp *interp, int argc, char **argv)
 
   // shift data to beginning of buffer
   memcpy(recvBuf, buf, bytes);
-  status = ParseSEQData(recvBuf, sizeof recvBuf, bytes, sock,
-			cachefile, schema, minVal, maxVal);
+  status = ParseSEQData(recvBuf, sizeof recvBuf, bytes, sock, cachefile);
   if (status != TCL_OK) {
     close(sock);
     return status;
   }
 
-  status = ParseSEQSchema(buf, bytes, schemafile, schematype, schema,
-			  minVal, maxVal, sdef);
+  status = ParseSEQSchema(buf, bytes, schemafile, schematype);
   if (status != TCL_OK) {
     close(sock);
     return status;
   }
 
-  delete sdef;
+  delete schema;
+  schema = 0;
 
   if (close(sock)) {
     perror("Error in SEQ server connection: close");
