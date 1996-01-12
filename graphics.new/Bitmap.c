c9bebf970caffa48dafec4e4f8f77f86ea54cf87
jussi <>
1996-01-12 15:15:18 +0000

    Replaced libc.h with stdlib.h. Added copyright notice.

--- graphics.new/Bitmap.c
+++ graphics.new/Bitmap.c
@@ -1,78 +1,105 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:05  jussi
+  Added CVS header.
+*/
 
 #include <stdio.h>
-#include <libc.h>
+#include <stdlib.h>
+
 #include "Exit.h"
 #include "Bitmap.h"
 
 /**************************************************************
 Create a new bitmap
 **************************************************************/
-Bitmap::Bitmap(int numBits){
-	if (numBits <= 0){
-		fprintf(stderr,"Bitmap::Bitmap: numBits %d <= 0\n", numBits);
-		Exit::DoExit(1);
-	}
 
-	_numBits = numBits;
-	_bits = new char[ArraySize(_numBits)];
-	ClearBitmap();
+Bitmap::Bitmap(int numBits)
+{
+  if (numBits <= 0) {
+    fprintf(stderr, "Bitmap::Bitmap: numBits %d <= 0\n", numBits);
+    Exit::DoExit(1);
+  }
+  
+  _numBits = numBits;
+  _bits = new char[ArraySize(_numBits)];
+  ClearBitmap();
 }
 
-/*****************************************************************8
+/******************************************************************
 Set a bit in the bitmap
 *******************************************************************/
-void Bitmap::SetBit(int bitNum){
-	if (bitNum < 0 || bitNum >= _numBits){
-		fprintf(stderr,"Bitmap::SetBit: invalid bit %d\n", bitNum);
-		Exit::DoExit(1);
-	}
 
-	int index = ArrayIndex(bitNum);
-	int bitPos = BitPos(bitNum);
-
-	_bits[index] |= (1<< bitPos);
+void Bitmap::SetBit(int bitNum)
+{
+  if (bitNum < 0 || bitNum >= _numBits) {
+    fprintf(stderr, "Bitmap::SetBit: invalid bit %d\n", bitNum);
+    Exit::DoExit(1);
+  }
+  
+  int index = ArrayIndex(bitNum);
+  int bitPos = BitPos(bitNum);
+  
+  _bits[index] |= (1<< bitPos);
 }
 
-/*****************************************************************8
+/******************************************************************
 Clear a bit in the bitmap
 *******************************************************************/
-void Bitmap::ClearBit(int bitNum){
-	if (bitNum < 0 || bitNum >= _numBits){
-		fprintf(stderr,"Bitmap::ClearBit: invalid bit %d\n", bitNum);
-		Exit::DoExit(1);
-	}
 
-	int index = ArrayIndex(bitNum);
-	int bitPos = BitPos(bitNum);
-
-	_bits[index] &= (0xff ^ (1<< bitPos));
+void Bitmap::ClearBit(int bitNum)
+{
+  if (bitNum < 0 || bitNum >= _numBits) {
+    fprintf(stderr, "Bitmap::ClearBit: invalid bit %d\n", bitNum);
+    Exit::DoExit(1);
+  }
+  
+  int index = ArrayIndex(bitNum);
+  int bitPos = BitPos(bitNum);
+  
+  _bits[index] &= (0xff ^ (1<< bitPos));
 }
 
-/*****************************************************************
+/******************************************************************
 Test if a bit has been set in the bitmap 
 *******************************************************************/
-Boolean Bitmap::TestBit(int bitNum){
-	if (bitNum < 0 || bitNum >= _numBits){
-		fprintf(stderr,"Bitmap::TestBit: invalid bit %d\n", bitNum);
-		Exit::DoExit(1);
-	}
 
-	int index = ArrayIndex(bitNum);
-	int bitPos = BitPos(bitNum);
+Boolean Bitmap::TestBit(int bitNum)
+{
+  if (bitNum < 0 || bitNum >= _numBits) {
+    fprintf(stderr, "Bitmap::TestBit: invalid bit %d\n", bitNum);
+    Exit::DoExit(1);
+  }
 
-	return (_bits[index] & (1<< bitPos));
+  int index = ArrayIndex(bitNum);
+  int bitPos = BitPos(bitNum);
+  
+  return (_bits[index] & (1<< bitPos));
 }
 
-/*******************************************************************
+/********************************************************************
 Clear the whole bitmap
 *********************************************************************/
-void Bitmap::ClearBitmap(){
-	int i;
-	int arraySize = ArraySize(_numBits);
-	for (i=0; i < arraySize; i++)
-		_bits[i] = 0;
+
+void Bitmap::ClearBitmap()
+{
+  int arraySize = ArraySize(_numBits);
+  for(int i = 0; i < arraySize; i++)
+    _bits[i] = 0;
 }
--- graphics.new/BufHash.c
+++ graphics.new/BufHash.c
@@ -1,107 +1,133 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:06  jussi
+  Added CVS header.
+*/
 
 #include <stdio.h>
-#include <libc.h>
+#include <stdlib.h>
+
 #include "Exit.h"
 #include "BufHash.h"
-
 
 /*****************************************************************
 Constructor
 ******************************************************************/
-BufHash::BufHash(){
-	for (int i=0;i < HASH_TABLE_SIZE; i++)
-		_hashTable[i] = NULL;
+
+BufHash::BufHash()
+{
+  for(int i = 0;i < HASH_TABLE_SIZE; i++)
+    _hashTable[i] = NULL;
 }
 
 /******************************************************************
 Insert buffer into hash table. Error if already exists 
 ********************************************************************/
-void BufHash::Insert(BufPage *buf){
-	DiskFile *pfile= buf->GetDiskFile();
-	int page = buf->PageNum();
 
-	BufPage *temp;
-	if (Find(pfile,page, temp)){
-		fprintf(stderr,"BufHash::Insert: buffer already exists\n");
-		Exit::DoExit(1);
-	}
-
-	int bucket = Hash(pfile, page);
-	buf->nextHash = _hashTable[bucket];
-	buf->prevHash = NULL;
-	if (buf->nextHash != NULL)
-		buf->nextHash->prevHash = buf;
-	_hashTable[bucket] = buf;
-
+void BufHash::Insert(BufPage *buf)
+{
+  DiskFile *pfile= buf->GetDiskFile();
+  int page = buf->PageNum();
+  
+  BufPage *temp;
+  if (Find(pfile,page, temp)) {
+    fprintf(stderr,"BufHash::Insert: buffer already exists\n");
+    Exit::DoExit(1);
+  }
+  
+  int bucket = Hash(pfile, page);
+  buf->nextHash = _hashTable[bucket];
+  buf->prevHash = NULL;
+  if (buf->nextHash != NULL)
+    buf->nextHash->prevHash = buf;
+  _hashTable[bucket] = buf;
 }
 
 /******************************************************************
 Delete buffer from hash table.
 ********************************************************************/
-void BufHash::Delete(BufPage *buf){
-	DiskFile *pfile = buf->GetDiskFile();
-	int page = buf->PageNum();
 
-	if (buf->prevHash == NULL){
-		/* deleting 1st element */
-		int bucket = Hash(pfile, page);
-		_hashTable[bucket] = buf->nextHash;
-		if (buf->nextHash != NULL)
-			buf->nextHash->prevHash = NULL;
-	}
-	else {
-		/* somewhere in the middle or end of list in hash table  */
-		if (buf->nextHash != NULL)
-			buf->nextHash->prevHash = buf->prevHash;
-		buf->prevHash->nextHash = buf->nextHash;
-	}
-	buf->nextHash = buf->prevHash = NULL;
+void BufHash::Delete(BufPage *buf)
+{
+  DiskFile *pfile = buf->GetDiskFile();
+  int page = buf->PageNum();
+  
+  if (buf->prevHash == NULL){
+    /* deleting 1st element */
+    int bucket = Hash(pfile, page);
+    _hashTable[bucket] = buf->nextHash;
+    if (buf->nextHash != NULL)
+      buf->nextHash->prevHash = NULL;
+  } else {
+    /* somewhere in the middle or end of list in hash table  */
+    if (buf->nextHash != NULL)
+      buf->nextHash->prevHash = buf->prevHash;
+    buf->prevHash->nextHash = buf->nextHash;
+  }
+  buf->nextHash = buf->prevHash = NULL;
 }
-
 
 /******************************************************************
 Find buffer entry for the given page. Return TRUE if found 
 ********************************************************************/
-Boolean BufHash::Find(DiskFile *pfile, int page, BufPage *& buf){
-	int bucket = Hash(pfile, page);
-	BufPage *entry; 
-	_numFind++;
-	for (entry= _hashTable[bucket]; entry != NULL; entry= entry->nextHash){
-		_numFindSearch++;
-		if (entry->GetDiskFile()== pfile&& entry->PageNum() == page){
-			/* found */
-			buf = entry;
-			return true;
-		}
-	}
-	/* not found */
-	return false;
+
+Boolean BufHash::Find(DiskFile *pfile, int page, BufPage *& buf)
+{
+  int bucket = Hash(pfile, page);
+  _numFind++;
+  for(BufPage *entry= _hashTable[bucket]; entry != NULL;
+      entry = entry->nextHash) {
+    _numFindSearch++;
+    if (entry->GetDiskFile()== pfile && entry->PageNum() == page) {
+      /* found */
+      buf = entry;
+      return true;
+    }
+  }
+
+  /* not found */
+  return false;
 }
 
 /****************************************************************
 print the hash table
 *****************************************************************/
-void BufHash::Print(){
-	int i;
-	printf("Hash Table:\n");
-	printf("buf\t\tpfile\t\tpage\n");
-	for (i=0;i < HASH_TABLE_SIZE; i++){
-		BufPage *entry;
-		for (entry= _hashTable[i]; entry != NULL; entry= entry->nextHash){
-			printf("%x\t\t%x\t\t%d\n",entry,
-					entry->GetDiskFile(), entry->PageNum());
-		}
-	}
+
+void BufHash::Print()
+{
+  printf("Hash Table:\n");
+  printf("buf\t\tpfile\t\tpage\n");
+  for(int i = 0;i < HASH_TABLE_SIZE; i++) {
+    for(BufPage *entry = _hashTable[i]; entry != NULL;
+	entry = entry->nextHash) {
+      printf("%x\t\t%x\t\t%d\n", entry,
+	     entry->GetDiskFile(), entry->PageNum());
+    }
+  }
 }
 
 /********************************************************************
 Print Statistics
 *********************************************************************/
-void BufHash::PrintStat(){
-	printf("BufPage: Find called %d times, %d steps, %f steps/find\n",
-		_numFind, _numFindSearch, (double)_numFindSearch/(double)_numFind);
+
+void BufHash::PrintStat()
+{
+  printf("BufPage: Find called %d times, %d steps, %f steps/find\n",
+	 _numFind, _numFindSearch, (double)_numFindSearch / (double)_numFind);
 }
