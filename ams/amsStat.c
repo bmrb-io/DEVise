007c8694f72e41d9fac17e1ed26f4077098c5b53
jussi <>
1996-01-12 15:48:14 +0000

    Replaced libc.h with stdlib.h.

--- ams/amsStat.c
+++ ams/amsStat.c
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.4  1995/09/22 16:01:11  jussi
+  Added copyright message.
+
   Revision 1.3  1995/09/05 20:23:47  jussi
   Updated CVS header.
 
@@ -29,50 +32,52 @@
 */
 
 #include <stdio.h>
-#include <libc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <limits.h>
-#include <sys/uio.h>
 #include <unistd.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include "TDataAppend.h"
 #include "ams.h"
 
-void Usage(char *name){
-	printf("Usage: %s infile\n",name);
-	exit(1);
+void Usage(char *name)
+{
+  printf("Usage: %s infile\n",name);
+  exit(1);
 }
 
-main(int argc, char **argv){
-int numTimes = 1;
-char *infile, *outfile;
-
-	if (argc < 2)
-		Usage(argv[0]);
-	
-	infile = argv[1];
-
-
-	/* Open output file */
-	TDataAppend *tdata = new TDataAppend(infile,sizeof(AmsData));
-	printf("There are %d pages\n", tdata->NumPages());
-	printf("There are %d records\n", tdata->NumRecords());
-	if (tdata->NumRecords() > 0){
-		/* get 1st record */
-		int numRecs;
-		RecId startRid;
-		void **recs;
-		tdata->GetPage(tdata->FirstPage(),numRecs,startRid,recs);
-		AmsData *data = (AmsData *)recs[0];
-		printf("1st record, time = %f\n",data->time);
-		tdata->FreePage(tdata->FirstPage());
-
-		/* get last record */
-		tdata->GetPage(tdata->LastPage(),numRecs,startRid,recs);
-		data = (AmsData *)recs[numRecs-1];
-		printf("1st record, time = %f\n",data->time);
-		tdata->FreePage(tdata->LastPage());
-	}
-	delete tdata;
+main(int argc, char **argv)
+{
+  int numTimes = 1;
+  char *infile, *outfile;
+  
+  if (argc < 2)
+    Usage(argv[0]);
+  
+  infile = argv[1];
+  
+  /* Open output file */
+  TDataAppend *tdata = new TDataAppend(infile,sizeof(AmsData));
+  printf("There are %d pages\n", tdata->NumPages());
+  printf("There are %d records\n", tdata->NumRecords());
+  if (tdata->NumRecords() > 0){
+    /* get 1st record */
+    int numRecs;
+    RecId startRid;
+    void **recs;
+    tdata->GetPage(tdata->FirstPage(),numRecs,startRid,recs);
+    AmsData *data = (AmsData *)recs[0];
+    printf("1st record, time = %f\n",data->time);
+    tdata->FreePage(tdata->FirstPage());
+    
+    /* get last record */
+    tdata->GetPage(tdata->LastPage(),numRecs,startRid,recs);
+    data = (AmsData *)recs[numRecs-1];
+    printf("1st record, time = %f\n",data->time);
+    tdata->FreePage(tdata->LastPage());
+  }
+  delete tdata;
 }
