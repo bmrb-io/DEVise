62de7186cff7456448b2c310d7a256838bd69900
jussi <>
1996-01-25 20:23:17 +0000

    Added copyright notice and improved code.

--- graphics.new/DispQueryProc.c
+++ graphics.new/DispQueryProc.c
@@ -1,34 +1,51 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:44  jussi
+  Added CVS header.
+*/
 
 #include "DispQueryProc.h"
 
-/******************************************************************
-constructor
-*********************************************************************/
-DispQueryProcSimple::DispQueryProcSimple(){
-	Dispatcher::Current()->Register(this, 20);
+DispQueryProcSimple::DispQueryProcSimple()
+{
+  Dispatcher::Current()->Register(this, 20);
 }
 
 /********************************************************************8
 Called by dispatcher when we can run
 **********************************************************************/
-void DispQueryProcSimple::Run(){
-	ProcessQuery();
+
+void DispQueryProcSimple::Run()
+{
+  ProcessQuery();
 }
 
-/******************************************************************
-constructor
-*********************************************************************/
-DispQueryProcFull::DispQueryProcFull(){
-	Dispatcher::Current()->Register(this,20);
+DispQueryProcFull::DispQueryProcFull()
+{
+  Dispatcher::Current()->Register(this, 20);
 }
 
 /********************************************************************8
 Called by dispatcher when we can run
 **********************************************************************/
-void DispQueryProcFull::Run(){
-	ProcessQuery();
+
+void DispQueryProcFull::Run()
+{
+  ProcessQuery();
 }
