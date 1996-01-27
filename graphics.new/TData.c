727599a788302831fdc8e4476e48532dddd27d19
jussi <>
1996-01-27 00:17:27 +0000

    Added copyright notice and cleaned up a bit.

--- graphics.new/TData.c
+++ graphics.new/TData.c
@@ -1,21 +1,39 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:44  jussi
+  Added CVS header.
+*/
 
 #include <stdio.h>
+
 #include "TData.h"
 #include "Exit.h"
 
-TData::~TData(){
-/*
-printf("TData destructor\n");
-*/
+TData::~TData()
+{
 }
 
 /* For writing records. Default: not implemented. */
-void TData::WriteRecs(RecId startId, int numRecs, void *buf){
-	fprintf(stderr,"TData::WRiteRecs: not implemented\n");
-	Exit::DoExit(2);
+
+void TData::WriteRecs(RecId startId, int numRecs, void *buf)
+{
+  fprintf(stderr, "TData::WRiteRecs: not implemented\n");
+  Exit::DoExit(2);
 }
 
