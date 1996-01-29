3c0ef41668abdceebc351274c867533d3ccc1823
jussi <>
1996-01-29 23:58:16 +0000

    Added copyright notice and cleaned up the code a bit.

--- graphics.new/ViewClassInfo.h
+++ graphics.new/ViewClassInfo.h
@@ -1,73 +1,81 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:16  jussi
+  Added CVS header.
+*/
 
 #ifndef ViewClassInfo_h
 #define ViewClassInfo_h
+
 #include "ClassDir.h"
 
 class TDataViewX;
 class View;
+
 class ViewXInfo: public ClassInfo {
 public:
-	ViewXInfo(); /* class constructor */
-	ViewXInfo(char *name, char *bgName,
-		TDataViewX *view); /* instance constructor */
-	virtual ~ViewXInfo();
+  ViewXInfo();
+  ViewXInfo(char *name, char *bgName, TDataViewX *view);
+  virtual ~ViewXInfo();
 
-	/************** Class Info ******************************/
-    virtual char *CategoryName() { return "view"; }
-	virtual char *ClassName() { return "SortedX"; }
+  virtual char *CategoryName() { return "view"; }
+  virtual char *ClassName() { return "SortedX"; }
 
-	/* Get name of parameters */
-	 virtual void ParamNames(int &argc, char **&argv);
+  virtual void ParamNames(int &argc, char **&argv);
+  virtual ClassInfo *CreateWithParams(int argc, char **argv);
+  
+  virtual char *InstanceName();
+  virtual void *GetInstance();
 
-	virtual ClassInfo *CreateWithParams(int argc, char **argv);
-
-	/****************** Instance Info *****************************/
-    virtual char *InstanceName();
-	virtual void *GetInstance();
-
-	/* Get parameters that can be used to re-create this instance */
-	virtual void CreateParams(int &argc, char **&argv);
+  virtual void CreateParams(int &argc, char **&argv);
 
 private:
-	char *arg[6];
-	TDataViewX *_view;
-	char *_name;
-	char *_bgColorName;
+  char *arg[6];
+  TDataViewX *_view;
+  char *_name;
+  char *_bgColorName;
 };
 
 class ViewScatter;
+
 class ViewScatterInfo : public ClassInfo {
 public:
-	ViewScatterInfo(); /* class constructor */
-	ViewScatterInfo(char *name, 
-		char *bgName, ViewScatter *view); /* instance constructor*/
-	virtual ~ViewScatterInfo();
+  ViewScatterInfo();
+  ViewScatterInfo(char *name, char *bgName, ViewScatter *view);
+  virtual ~ViewScatterInfo();
 
-	/************* Class Info ***********************/
-    virtual char *CategoryName() { return "view"; }
-	virtual char *ClassName() { return "Scatter"; }
+  virtual char *CategoryName() { return "view"; }
+  virtual char *ClassName() { return "Scatter"; }
 
-	/* Get name of parameters */
-	 virtual void ParamNames(int &argc, char **&argv);
+  virtual void ParamNames(int &argc, char **&argv);
+  virtual ClassInfo *CreateWithParams(int argc, char **argv);
+  
+  virtual char *InstanceName();
+  virtual void *GetInstance();
 
-	 virtual ClassInfo *CreateWithParams(int argc, char **argv);
-
-	 /************** Instance Info *****************************/
-	 virtual char *InstanceName();
-	 virtual void *GetInstance();
-
-	 /* Get parameters that can be used to re-create this instance */
-	 virtual void CreateParams(int &argc, char **&argv);
+  virtual void CreateParams(int &argc, char **&argv);
 
 private:
-char *arg[6];
-ViewScatter *_view;
-char *_name;
-char *_bgName;
+  char *arg[6];
+  ViewScatter *_view;
+  char *_name;
+  char *_bgName;
 };
 
 #endif
