e68e1b7a97624967547926208e94752824cc1545
yuc <>
1996-02-28 17:32:39 +0000

    Add global block_data for 3D Block Shape.

--- graphics.new/Temp.c
+++ graphics.new/Temp.c
@@ -16,11 +16,17 @@
   $Id$
 
   $Log$
+  Revision 1.3  1996/01/09 22:13:34  jussi
+  Added support for 3D data.
+
   Revision 1.2  1995/09/05 22:16:06  jussi
   Added CVS header.
 */
 
 #include "Temp.h"
+
+
+BLOCK block_data[WINDOWREP_BATCH_SIZE]; 
 
 Coord _x[WINDOWREP_BATCH_SIZE];
 Coord _y[WINDOWREP_BATCH_SIZE];
@@ -28,3 +34,4 @@ Coord _z[WINDOWREP_BATCH_SIZE];
 Coord _width[WINDOWREP_BATCH_SIZE];
 Coord _height[WINDOWREP_BATCH_SIZE];
 Coord _depth[WINDOWREP_BATCH_SIZE];
+
