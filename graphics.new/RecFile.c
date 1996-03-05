6a749595a2df45e9b95267c183754bc153fc0c17
jussi <>
1996-03-05 21:54:56 +0000

    Added copyright notice and cleaned up the code a bit.

--- graphics.new/RecFile.c
+++ graphics.new/RecFile.c
@@ -1,16 +1,33 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:28  jussi
+  Added CVS header.
+*/
 
 #include "UnixRecFile.h"
 
-/*******************************************************************/
-RecFile *RecFile::CreateFile(char *name, int recSize){
-	return UnixRecFile::CreateFile(name, recSize);
+RecFile *RecFile::CreateFile(char *name, int recSize)
+{
+  return UnixRecFile::CreateFile(name, recSize);
 }
 
-/*******************************************************************/
-RecFile *RecFile::OpenFile(char *name, int recSize, Boolean trunc){
-	return UnixRecFile::OpenFile(name, recSize, trunc);
+RecFile *RecFile::OpenFile(char *name, int recSize, Boolean trunc)
+{
+  return UnixRecFile::OpenFile(name, recSize, trunc);
 }
