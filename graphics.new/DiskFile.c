bd52f41ec933b61fe9a271b46c11cdd0f718ac9f
jussi <>
1996-03-05 21:33:35 +0000

    Added copyright notice and cleaned up a bit.

--- graphics.new/DiskFile.c
+++ graphics.new/DiskFile.c
@@ -1,7 +1,24 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:14:43  jussi
+  Added CVS header.
+*/
 
 #ifdef USE_AMSDISKFILE
 #include <stdio.h>
@@ -10,20 +27,20 @@
 #include "UnixDiskFile.h"
 #endif
 
-/*******************************************************************/
-DiskFile *DiskFile::CreateFile(char *name){
+DiskFile *DiskFile::CreateFile(char *name)
+{
 #ifdef USE_AMSDISKFILE
-	return NULL;
+  return NULL;
 #else
-	return UnixDiskFile::CreateFile(name);
+  return UnixDiskFile::CreateFile(name);
 #endif
 }
 
-/*******************************************************************/
-DiskFile *DiskFile::OpenFile(char *name){
+DiskFile *DiskFile::OpenFile(char *name)
+{
 #ifdef USE_AMSDISKFILE
-	return new AmsDiskFile();
+  return new AmsDiskFile();
 #else
-	return UnixDiskFile::OpenFile(name);
+  return UnixDiskFile::OpenFile(name);
 #endif
 }
