2d14590b82d2512249e1186b5ed960ae48759487
jussi <>
1996-05-07 21:13:11 +0000

    Renamed MappingInfo to MultiMappingInfo.

--- multi/MappingClassInfo.c
+++ multi/MappingClassInfo.c
@@ -1,7 +1,7 @@
 /*
   ========================================================================
   DEVise Data Visualization Software
-  (c) Copyright 1992-1995
+  (c) Copyright 1992-1996
   By the DEVise Development Group
   Madison, Wisconsin
   All Rights Reserved.
@@ -15,7 +15,11 @@
 /*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/12/14 19:24:13  jussi
+  Added copyright notice and CVS header. Removed reference to
+  ViewWinVer.h.
+*/
 
 #include <sys/time.h>
 #include <time.h>
@@ -51,56 +55,69 @@ struct MapInfo *mapInfo;
 
 #include "mapfile.h"
 
+MultiMappingInfo::MultiMappingInfo()
+{
+}
 
-MappingInfo::MappingInfo() {}
-
-MappingInfo::MappingInfo(char *tdataAlias, char *name, TDataMap *mapping){
-	_tdataAlias = tdataAlias;
-	_name = name;
-	_map = mapping;
+MultiMappingInfo::MultiMappingInfo(char *tdataAlias, char *name,
+				   TDataMap *mapping)
+{
+  _tdataAlias = tdataAlias;
+  _name = name;
+  _map = mapping;
 }
 
 static char buf1[256], buf2[256];
 static char *arg[5];
+
 /* Get name of parameters */
-void MappingInfo::ParamNames(int &argc, char **&argv){
-    argc = 2;
-	argv = arg;
-	arg[0] = buf1;
-	sprintf(buf1,"File_Alias %s", ControlPanel::Instance()->FileAlias());
-	arg[1] = buf2;
-	sprintf(buf2,"GData_Name %s",ControlPanel::Instance()->GDataName());
+void MultiMappingInfo::ParamNames(int &argc, char **&argv)
+{
+  argc = 2;
+  argv = arg;
+  arg[0] = buf1;
+  sprintf(buf1,"File_Alias %s", ControlPanel::Instance()->FileAlias());
+  arg[1] = buf2;
+  sprintf(buf2, "GData_Name %s", ControlPanel::Instance()->GDataName());
 }
 
 /* Create instance using the supplied parameters. Return
-the instance info if successful, otherwise return NULL. */
-ClassInfo *MappingInfo::CreateWithParams(int argc, char **argv){
-    if (argc != 2){
-		fprintf(stderr,"MultiMapInfo::CreateWithParams\n");
-		return NULL;
-	}
-	TData *tdata;
-	char *tdataAlias = CopyString(argv[0]);
-	if ((tdata=(TData *)ControlPanel::FindInstance(tdataAlias)) == NULL){
-		fprintf(stderr,"MultiMappingInfo::CreateWithParams: can't find tdata\n");
-		return NULL;
-	}
-	char *gdataName = CopyString(argv[1]);
-	TDataMap *map = new MultiMapping(tdata,gdataName,NULL, 0,
-		Init::MaxGDataPages(), mapInfo);
-	return new MappingInfo(tdataAlias, gdataName, map);
+   the instance info if successful, otherwise return NULL. */
+ClassInfo *MultiMappingInfo::CreateWithParams(int argc, char **argv)
+{
+  if (argc != 2) {
+    fprintf(stderr,"MultiMapInfo::CreateWithParams\n");
+    return NULL;
+  }
+
+  TData *tdata;
+  char *tdataAlias = CopyString(argv[0]);
+  if ((tdata = (TData *)ControlPanel::FindInstance(tdataAlias)) == NULL) {
+    fprintf(stderr,
+	    "MultiMultiMappingInfo::CreateWithParams: can't find tdata\n");
+    return NULL;
+  }
+  char *gdataName = CopyString(argv[1]);
+  TDataMap *map = new MultiMapping(tdata, gdataName, NULL, 0,
+				   Init::MaxGDataPages(), mapInfo);
+  return new MultiMappingInfo(tdataAlias, gdataName, map);
 }
 
-/**************************************************
-	Instance Info.
-***************************************************/
-char *MappingInfo::InstanceName() { return _name; }
-void *MappingInfo::GetInstance() { return _map; }
+char *MultiMappingInfo::InstanceName()
+{
+  return _name;
+}
+
+void *MultiMappingInfo::GetInstance()
+{
+  return _map;
+}
 
 /* Get parameters that can be used to re-create this instance */
-void MappingInfo::CreateParams(int &argc, char **&argv){
-	argc =2;
-	argv = arg;
-	arg[0] = _tdataAlias;
-	arg[1] = _name;
+void MultiMappingInfo::CreateParams(int &argc, char **&argv)
+{
+  argc = 2;
+  argv = arg;
+  arg[0] = _tdataAlias;
+  arg[1] = _name;
 }
--- multi/MappingClassInfo.h
+++ multi/MappingClassInfo.h
@@ -1,49 +1,68 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$*/
+
 #ifndef MappingClassInfo_h
 #define MappingClassInfo_h
+
 #include "ClassDir.h"
 
 /* Info needed by mapping */
 struct MapInfo {
-	int *job_ordering;
-		Color **colorArrays;
+  int *job_ordering;
+  Color **colorArrays;
 };
+
 extern Color suspendColor;
 extern Color abortColor;
 extern MapInfo *mapInfo;
 
-
-class MappingInfo : public ClassInfo {
+class MultiMappingInfo : public ClassInfo {
 public:
-	MappingInfo();	/* for creating class */
+  /* for creating class */
+  MultiMappingInfo();
 
-	/* for creating instance */
-	MappingInfo(char *tdataAlias, char *name, TDataMap *mapping);
+  /* for creating instance */
+  MultiMappingInfo(char *tdataAlias, char *name, TDataMap *mapping);
 
-	/* Info for category */
-	virtual char *CategoryName() { return "mapping"; }
+  /* Info for category */
+  virtual char *CategoryName() { return "mapping"; }
 
-	/* Info for class */
-	virtual char *ClassName(){ return "MultiMap"; }   /* name of class */
+  /* Info for class */
+  virtual char *ClassName() { return "MultiMap"; }
 
-	/* Get name of parameters */
-	virtual void ParamNames(int &argc, char **&argv); 
+  /* Get name of parameters */
+  virtual void ParamNames(int &argc, char **&argv); 
 
-	/* Create instance using the supplied parameters. Return
-	the instance info if successful, otherwise return NULL. */
-	virtual ClassInfo *CreateWithParams(int argc, char **argv);
+  /* Create instance using the supplied parameters. Return
+     the instance info if successful, otherwise return NULL. */
+  virtual ClassInfo *CreateWithParams(int argc, char **argv);
 
-	/**************************************************
-		Instance Info.
-	***************************************************/
-	virtual char *InstanceName();
-	virtual void *GetInstance();
+  virtual char *InstanceName();
+  virtual void *GetInstance();
 
-	/* Get parameters that can be used to re-create this instance */
-	virtual void CreateParams(int &argc, char **&argv);
+  /* Get parameters that can be used to re-create this instance */
+  virtual void CreateParams(int &argc, char **&argv);
+
 private:
-	char *_name;
-	char *_tdataAlias;
-	TDataMap *_map;
+  char *_name;
+  char *_tdataAlias;
+  TDataMap *_map;
 };
 
 #endif
