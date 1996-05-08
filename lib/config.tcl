ca08b0e1260048570c4dee5227331ed4a3113477
jussi <>
1996-05-08 20:59:01 +0000

    Added CRSP_NSDQ data source type.

--- lib/config.tcl
+++ lib/config.tcl
@@ -1,11 +1,12 @@
 # Data source configuration
 
-set "sourceConfig(WWW)" {}
-set "sourceConfig(NETWORK)" {network_client blondie.cs.wisc.edu 1400}
-set "sourceConfig(UNIXFILE)" {}
-set "sourceConfig(COMMAND)" {}
+set "sourceConfig(WWW)"       {}
+set "sourceConfig(NETWORK)"   {network_client blondie.cs.wisc.edu 1400}
+set "sourceConfig(UNIXFILE)"  {}
+set "sourceConfig(COMMAND)"   {}
 set "sourceConfig(COMPUSTAT)" {/dev/nrst4 23 512 32768}
-set "sourceConfig(CRSP)" {/dev/nrst4 20 512 32768}
-set "sourceConfig(ISSM)" {/dev/nrst4 4 0 32768}
-set "sourceConfig(SEQ)" {sun22.cs.wisc.edu 6003}
-set "sourceConfig(SQL)" {sql_client paradise.cs.wisc.edu 130}
+set "sourceConfig(CRSP)"      {/dev/nrst4 20 512 32768}
+set "sourceConfig(CRSP_NSDQ)" {/dev/nrst4 21 512 32768}
+set "sourceConfig(ISSM)"      {/dev/nrst4 4 0 32768}
+set "sourceConfig(SEQ)"       {sun22.cs.wisc.edu 6003}
+set "sourceConfig(SQL)"       {sql_client paradise.cs.wisc.edu 130}
