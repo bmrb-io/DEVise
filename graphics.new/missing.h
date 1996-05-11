cf23cdcd0487b73636e80465dbe4e15746cf786f
jussi <>
1996-05-11 01:57:28 +0000

    Added copyright notice and changed the prototypes of the functions defined here.

--- graphics.new/missing.h
+++ graphics.new/missing.h
@@ -1,6 +1,25 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
 
-/* Some of the missing function prototypes.
- */
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$*/
+
+/*
+   Some of the missing function prototypes.
+*/
 
 #ifndef MISSING_H
 #define MISSING_H
@@ -14,30 +33,26 @@ extern "C"
 {
 #endif  
 
-extern int socket (int __family, int __type, int __protocol);
-extern int bind (int __sockfd, struct sockaddr *__my_addr,
-		 int __addrlen);
-
-extern int select (int __width, fd_set * __readfds,
-		   fd_set * __writefds, fd_set * __exceptfds,
-		   struct timeval * __timeout);
-
-extern int sendto (int __sockfd, __const void *__buff, int __len,
-		   unsigned int __flags, __const struct sockaddr *__to,
-		   int __tolen);
-extern int recvfrom (int __sockfd, void *__buff, int __len,
-		     unsigned int __flags, struct sockaddr *__from,
-		     int *__fromlen);
-
-extern int setitimer (int __which, __const struct itimerval *__value,
-		      struct itimerval *__ovalue);
+extern int accept(int, struct sockaddr *, int *);
+extern int bind(int, struct sockaddr *, int);
+extern int connect(int, struct sockaddr *, int);
+extern int getpeername(int, struct sockaddr *, int *);
+extern int getsockname(int, struct sockaddr *, int *);
+extern int getsockopt(int, int, int, char *, int *);
+extern int listen(int, int);
+extern int recv(int, char *, int, int);
+extern int recvfrom(int, char *, int, int, struct sockaddr *, int *);
+extern int send(int, const char *, int, int);
+extern int sendto(int, const char *, int, int, struct sockaddr *, int);
+extern int setsockopt(int, int, int, const char *, int);
+extern int socket(int, int, int);
+extern int recvmsg(int, struct msghdr *, int);
+extern int sendmsg(int, struct msghdr *, int);
+extern int shutdown(int, int);
+extern int socketpair(int, int, int, int *);
 
 #ifdef __cplusplus
 }
 #endif  
 
-
-#endif /* MISSING_H */
-				  
-				  
-
+#endif
