fcc83fed54df44b1847cf3c831b2f1c103dd897d
jussi <>
1996-05-11 03:14:12 +0000

    Removed some control panel variables like FileName, GDataName, WindowName etc.

--- graphics.new/TkControl.h
+++ graphics.new/TkControl.h
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.6  1996/04/16 19:46:24  jussi
+  Added DoAbort() method.
+
   Revision 1.5  1996/01/27 00:21:36  jussi
   Added ExecuteScript() method.
 
@@ -33,23 +36,20 @@
 
 #ifndef TkControl_h
 #define TkControl_h
+
 #include <tcl.h>
 #include <tk.h>
 #include "Dispatcher.h"
 #include "Control.h"
 #include "ViewCallback.h"
 
-
 class View;
 class MapInterpClassInfo;
+
 class TkControlPanel: public ControlPanel, public DispatcherCallback,
-private ViewCallback{
+                      private ViewCallback{
 public:
 	TkControlPanel();
-
-	/* Return name of file current being worked on */
-	virtual char *FileName() { return _fileName; }
-	virtual char *FileAlias() { return _fileAlias; }
 
 #ifdef TK_WINDOW
 	virtual Tcl_Interp *GetInterp() { return _interp; }
@@ -58,20 +58,8 @@ public:
 
 	virtual void SelectView(View *view);
 
-	/* Return name of window last accessed */
-	virtual char *WindowName(){ return _winName; }
-
-	/* Return name of gData last accessed */
-	virtual char *GDataName() { return _gdataName; }
-					
-	/* Return name of view last accessed */
-	virtual char *ViewName() { return _viewName; }
-
 	/* Get current mode */
 	virtual Mode GetMode();
-
-	/* Return TRUE if restoring a session */
-	virtual Boolean Restoring();
 
 	/* Set busy status, should be called in pairs. */
 	virtual void SetBusy();
@@ -81,8 +69,6 @@ public:
 	virtual Boolean IsBusy();
 
 	virtual void StartSession();
-	virtual char *SessionName() ;
-	virtual void SetSessionName(char *name);
 
 	/* Execute script */
 	virtual void ExecuteScript(char *script);
@@ -91,14 +77,18 @@ public:
 	virtual void DoAbort(char *reason);
 
 protected:
-    virtual void SubclassInsertDisplay(DeviseDisplay *disp,Coord x, Coord y,
-			Coord w,Coord h);
+	virtual void SubclassInsertDisplay(DeviseDisplay *disp,
+					   Coord x, Coord y,
+					   Coord w,Coord h);
 	virtual void SubclassRegisterView(View *view);
 	virtual void SubclassUnregisterView(View *view);
 	virtual void SubclassDoInit();
 	virtual void SubclassDoViewPerspectiveChanged(View *view,
-		Coord x, Coord y, Coord w, Coord h);
+						      Coord x, Coord y,
+						      Coord w, Coord h);
 	
+	virtual void DestroyClientData();
+
 private:
 	virtual void FilterAboutToChange(View *view) {}
 	virtual void FilterChanged(View *view, VisualFilter &filter,
@@ -119,17 +109,10 @@ private:
 	Tcl_Interp *_interp;
 	Tk_Window _mainWindow;
 
-	char *_fileName;
-	char *_fileAlias;
-	char *_winName;
-	char *_gdataName;
-	char *_viewName;
-	char *_sessionName;
 	char *_argv0;
-	int _template; /* true if we are loading file as a template */
-	int _restoring;
 	int _busy; /* >0 if system is busy processing queries */
 	static ControlPanel::Mode _mode;
 	static MapInterpClassInfo *_interpProto; /* proto interpreted mapping */
 };
+
 #endif
