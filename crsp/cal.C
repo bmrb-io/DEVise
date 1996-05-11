66cf002a64bdbe8667c190dce9f7ec2c799f7875
jussi <>
1996-05-11 04:00:39 +0000

    Minor improvement in code readability.

--- crsp/cal.C
+++ crsp/cal.C
@@ -1,7 +1,7 @@
 /*
   ========================================================================
   DEVise Data Visualization Software
-  (c) Copyright 1992-1995
+  (c) Copyright 1992-1996
   By the DEVise Development Group
   Madison, Wisconsin
   All Rights Reserved.
@@ -16,18 +16,26 @@
   $Id$
 
   $Log$
+  Revision 1.2  1995/12/14 18:36:01  jussi
+  Small fix to get rid of g++ -Wall warning.
+
   Revision 1.1  1995/11/09 15:30:50  ravim
   Initial revision
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream.h>
+
 #include "cal.h"
 
-// PUBLIC FUNCTIONS
-Calendar::Calendar(int fdes)
+#ifdef SUN
+#define SEEK_SET 0
+#endif
+
+Calendar::Calendar(FILE *file)
 {
-  fildes = fdes;
+  fp = file;
   cal.caldt = cal.totcnt = cal.usdcnt = 0;
   cal.vwretd = cal.vwretx = cal.ewretd = cal.ewretx = cal.usdval = 0;
   cal.spindx = cal.sprtrn = 0;
@@ -35,15 +43,13 @@ Calendar::Calendar(int fdes)
 
 Calendar::~Calendar()
 {
-
 }
 
 int Calendar::load_day(int recno)
 {
   // Get the correct record into buffer
-  if (get_rec(recno) == 0)
-  {
-    cout << "Could not load record for current day. "<<endl;
+  if (get_rec(recno) == 0) {
+    cout << "Could not load record for current day. " << endl;
     return 0;
   }
 
@@ -65,68 +71,63 @@ int Calendar::load_day(int recno)
 
 void Calendar::print_day()
 {
-  cout << "Current day information" <<endl;
-  cout << "caldt : "<<cal.caldt <<endl;
-  cout <<"vwretd , vwretx: "<<cal.vwretd<<cal.vwretx<<endl;
-  cout << "ewretd, ewretx : "<<cal.ewretd<<cal.ewretx<<endl;
-  cout << "totval, totcnt : "<<cal.totval<<cal.totcnt<<endl;
-  cout << "usdval, usdcnt : "<<cal.usdval<<cal.usdcnt<<endl;
-  cout << "spindx, sprtrn : "<<cal.spindx<<cal.sprtrn<<endl;
+  cout << "Current day information" << endl;
+  cout << "caldt : " << cal.caldt << endl;
+  cout << "vwretd , vwretx: " << cal.vwretd << cal.vwretx << endl;
+  cout << "ewretd, ewretx : " << cal.ewretd << cal.ewretx << endl;
+  cout << "totval, totcnt : " << cal.totval << cal.totcnt << endl;
+  cout << "usdval, usdcnt : " << cal.usdval << cal.usdcnt << endl;
+  cout << "spindx, sprtrn : " << cal.spindx << cal.sprtrn << endl;
 }
 
-
-// PRIVATE FUNCTIONS
-
-// Returns 0 if eof reached ,else 1
+// Returns 0 if eof reached, else 1
 int Calendar::get_rec(int recno) 
 {
   // Seek to the appropriate record
-  if (lseek(fildes, REC_LEN*recno, SEEK_SET) == -1)
-  {
-    cout << "Error in seeking to the record in calendar file"<< endl;
-    exit(0);
+  if (fseek(fp, REC_LEN * recno, SEEK_SET) != 0) {
+    perror("fseek");
+    return 0;
   }
 
   // Read REC_LEN bytes into currec
-  if (read(fildes, (void *)currec, (size_t)REC_LEN) == 0)
+  if (fread((void *)currec, (size_t)REC_LEN, 1, fp) != 1) {
+    perror("fread");
     return 0;
-  else return 1;
-}
+  }
 
+  return 1;
+}
 
 void Calendar::get_field(int pos, char type, int len, int pre, void *dst)
 {
-  char *buf;
   // Position p means offset of (p-1)
   char *from = &(currec[pos -1]);
+  char oldchar;
 
   switch (type) {
-    case 'A': 
-      memcpy((char *)dst, from, len);
-      ((char *)dst)[len] = '\0';
-      break;
+  case 'A': 
+    memcpy((char *)dst, from, len);
+    ((char *)dst)[len] = '\0';
+    break;
 
-    case 'I':
-      buf = new char(len+1);
-      memcpy(buf, from, len);
-      buf[len] = '\0';
-      *((int *)dst) = atoi(buf);
-      delete buf;
-      break;
+  case 'I':
+    oldchar = from[len];
+    from[len] = 0;
+    *((int *)dst) = atoi(from);
+    from[len] = oldchar;
+    break;
 
-    case 'F':
-      // Here I am assuming that the decimal point is present in the 
-      // field. If the point is implied, different actions need to be done.
-      buf = new char(len+1);
-      memcpy(buf, from, len);
-      buf[len] = '\0';
-      *((float *)dst) = atof(buf);
-      delete buf;
-      break;
+  case 'F':
+    // Here I am assuming that the decimal point is present in the 
+    // field. If the point is implied, different actions need to be done.
+    oldchar = from[len];
+    from[len] = 0;
+    *((float *)dst) = atof(from);
+    from[len] = oldchar;
+    break;
 
-    default:  
-      cout << "Unknown type of field"<<endl;
-      break;
-
-    };
+  default:  
+    cout << "Unknown type of field" << endl;
+    break;
+  }
 }
