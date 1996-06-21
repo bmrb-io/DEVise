83a474c17c2b378a394653bde900039f6d8fc1c3
jussi <>
1996-06-21 19:27:00 +0000

    Moved computation of block vertices and edges to Map3D.C. Added support for drawing solid sides for blocks.

--- graphics.new/MapInterpShape.C
+++ graphics.new/MapInterpShape.C
@@ -17,6 +17,10 @@
   $Id$
 
   $Log$
+  Revision 1.4  1996/06/15 07:11:02  yuc
+  Small changes and clean-up DrawGDataArray, also add view and
+  dimensions.
+
   Revision 1.3  1996/04/13 00:11:17  jussi
   Added View * parameter to DrawGDataArray() and the calls to
   DrawPixelArray() and GetColor().
@@ -25,148 +29,76 @@
   Added CVS header.
 */
 
-
 #include "MapInterpShape.h"
+#include "Map3D.h"
 
-// ---------------------------------------------------------- 
-// sample block = unit cube
-//    cube center at (0, 0, 0), edge size = 1
-//    pt0 (-0.5, -0.5,  0.5)       pt1( 0.5, -0.5,  0.5)
-//    pt2 ( 0.5, -0.5, -0.5)       pt3(-0.5, -0.5, -0.5)
-//    pt4 (-0.5,  0.5,  0.5)       pt5(-0.5,  0.5, -0.5)
-//    pt6 ( 0.5,  0.5,  0.5)       pt7( 0.5,  0.5, -0.5)
-void
-FullMapping_BlockShape::MapBlockVertex(int i)
+// uncomment the following line if you want blocks drawn with wire
+// frames, otherwise you'll get solid shapes
+//#define WIRE_FRAME
+
+void FullMapping_BlockShape::DrawGDataArray(WindowRep *win, 
+					    void **gdataArray,
+					    int numSyms, TDataMap *map, 
+					    View *view, int pixelSize)
 {
-	double W = _width[i]  / 2.0,
-	       H = _height[i] / 2.0,
-	       D = _depth[i]  / 2.0;
+  view->SetNumDimensions(3);
+  Map3D::CompViewingTransf(view->GetCamera());
 
-	block_data[i].W = W;
-	block_data[i].H = H;
-	block_data[i].D = D;
+  int x, y, w, h;
+  view->GetDataArea(x, y, w, h);
+  view->SetViewDir(w / 2, h / 2);
 
-	block_data[i].vt[0].x_ = block_data[i].pt.x_ - W;
-	block_data[i].vt[0].y_ = block_data[i].pt.y_ - D; 
-	block_data[i].vt[0].z_ = block_data[i].pt.z_ + H;
+  GDataAttrOffset *offset = map->GetGDataOffset();
 
-	block_data[i].vt[1].x_ = block_data[i].pt.x_ + W;
-	block_data[i].vt[1].y_ = block_data[i].pt.y_ - D;
-	block_data[i].vt[1].z_ = block_data[i].pt.z_ + H;
+  for(int i = 0; i < numSyms; i++) {
+    char *gdata = (char *)gdataArray[i];
+    
+    block_data[i].pt.x_ = GetX(gdata, map, offset);
+    block_data[i].pt.y_ = GetY(gdata, map, offset);
+    block_data[i].pt.z_ = GetZ(gdata, map, offset);
+    block_data[i].W = fabs(GetShapeAttr0(gdata, map, offset));
+    block_data[i].H = fabs(GetShapeAttr1(gdata, map, offset));
+    block_data[i].D = fabs(GetShapeAttr2(gdata, map, offset));
+    block_data[i].color = GetColor(view, gdata, map, offset);
 
-	block_data[i].vt[2].x_ = block_data[i].pt.x_ + W;
-	block_data[i].vt[2].y_ = block_data[i].pt.y_ - D;
-	block_data[i].vt[2].z_ = block_data[i].pt.z_ - H;
-
-	block_data[i].vt[3].x_ = block_data[i].pt.x_ - W;
-	block_data[i].vt[3].y_ = block_data[i].pt.y_ - D;
-	block_data[i].vt[3].z_ = block_data[i].pt.z_ - H;
-
-	block_data[i].vt[4].x_ = block_data[i].pt.x_ - W;
-	block_data[i].vt[4].y_ = block_data[i].pt.y_ + D;
-	block_data[i].vt[4].z_ = block_data[i].pt.z_ + H;
-
-	block_data[i].vt[5].x_ = block_data[i].pt.x_ - W;
-	block_data[i].vt[5].y_ = block_data[i].pt.y_ + D;
-	block_data[i].vt[5].z_ = block_data[i].pt.z_ - H;
-
-	block_data[i].vt[6].x_ = block_data[i].pt.x_ + W;
-	block_data[i].vt[6].y_ = block_data[i].pt.y_ + D;
-	block_data[i].vt[6].z_ = block_data[i].pt.z_ + H;
-
-	block_data[i].vt[7].x_ = block_data[i].pt.x_ + W;
-	block_data[i].vt[7].y_ = block_data[i].pt.y_ + D;
-	block_data[i].vt[7].z_ = block_data[i].pt.z_ - H;
-} // end of MapBlockVertex()
-
-// ---------------------------------------------------------- 
-void FullMapping_BlockShape::MapBlockEdges(int i)
-{
-	block_data[i].ed[0].p  = 0; block_data[i].ed[0].q  = 3; 
-	block_data[i].ed[1].p  = 3; block_data[i].ed[1].q  = 2; 
-	block_data[i].ed[2].p  = 2; block_data[i].ed[2].q  = 1; 
-	block_data[i].ed[3].p  = 1; block_data[i].ed[3].q  = 0; 
-	block_data[i].ed[4].p  = 0; block_data[i].ed[4].q  = 4; 
-	block_data[i].ed[5].p  = 4; block_data[i].ed[5].q  = 5; 
-	block_data[i].ed[6].p  = 5; block_data[i].ed[6].q  = 3; 
-	block_data[i].ed[7].p  = 4; block_data[i].ed[7].q  = 6; 
-	block_data[i].ed[8].p  = 6; block_data[i].ed[8].q  = 7; 
-	block_data[i].ed[9].p  = 7; block_data[i].ed[9].q  = 5; 
-	block_data[i].ed[10].p = 6; block_data[i].ed[10].q = 1; 
-	block_data[i].ed[11].p = 2; block_data[i].ed[11].q = 7; 
-
-} // end of MapBlockEdges()
-
-// #define YLC
-
-// ---------------------------------------------------------- 
-void 
-FullMapping_BlockShape::DrawGDataArray(WindowRep *win, 
-	void **gdataArray, int numSyms, TDataMap *map, 
-	View *view, int pixelSize)
-{
-    view->SetNumDimensions(3);
-    win->CompViewingTransf(view->GetCamera());
-
-    int X=0, Y=0, H=0, V=0;
-    view->GetDataArea (X, Y, H, V);
-#ifdef YLC
-	cout << "win pixel -> X = "<<X<<" Y = "<<Y<<" H = "<<H<<" V = "<<V<<endl;
+    Map3D::AssignBlockVertices(block_data[i]);
+#ifdef WIRE_FRAME
+    Map3D::AssignBlockEdges(block_data[i]);
+#else
+    Map3D::AssignBlockSides(block_data[i]);
 #endif
-    view->SetViewDir(H / 2, V / 2);
-//    view->SetLabelInfo(1); // ture = labe occupies top of view
 
-    GDataAttrOffset *offset = map->GetGDataOffset();
-    Boolean fixedSymSize = (offset->shapeAttrOffset[0] < 0 &&
-                   offset->shapeAttrOffset[1] < 0 ? true : false);
-
-   Color firstColor = 0;
-   int i = 0, count;
-
-   while (i < numSyms) {
-	count = 0;
-
-	for (; i < numSyms; i++) {
-		char *gdata = (char *)gdataArray[i];
-		if (count == 0)
-			firstColor = GetColor(view, gdata, map, offset);
-		else if (count > 0 && 
-			GetColor(view, gdata, map, offset) != firstColor)
-			break;
-
-		_width[count]  = fabs(GetShapeAttr0(gdata, map, offset));
-		_height[count] = fabs(GetShapeAttr1(gdata, map, offset));
-		_depth[count]  = fabs(GetShapeAttr2(gdata, map, offset));
-
-		block_data[count].pt.x_ = GetX(gdata, map, offset);
-		block_data[count].pt.y_ = GetY(gdata, map, offset);
-		block_data[count].pt.z_ = GetZ(gdata, map, offset);
-
-		MapBlockVertex(count);
-		MapBlockEdges (count);
-		count++;				// get the next pt
-#ifdef YLC1
-     cout << "numSyms = " << numSyms <<"\n\n"<<"  x = "
-		<<block_data[i].pt.x_ << "  y = " <<block_data[i].pt.y_
-          << "  z = " <<block_data[i].pt.z_ << "\n" ;
-     for (int j = 0; j < BLOCK_VERTEX; j++) {
-          cout << "\t" << block_data[i].vt[j].x_ << "  "
-               << block_data[i].vt[j].y_ << "  "
-               << block_data[i].vt[j].z_ << "\n";
-     }
-	cout<<"numSyms = "<<numSyms<<"  i = "<<i<<" firstC = "<<firstColor<<endl;
+#ifdef DEBUG
+    cout << "sym " << i << " of " << numSyms << endl
+         << "  x = " << block_data[i].pt.x_
+	 << "  y = " << block_data[i].pt.y_
+	 << "  z = " << block_data[i].pt.z_ << endl;
+    for(int j = 0; j < BLOCK_VERTEX; j++) {
+      cout << "  "
+	   << block_data[i].vt[j].x_ << "  "
+	   << block_data[i].vt[j].y_ << "  "
+	   << block_data[i].vt[j].z_ << endl;
+    }
 #endif
-	} // end of for loop
+  }
 
-     win->SetFgColor(0);         // 0 = black
-	win->MapAllXPoints(block_data, count, view->GetCamera(), H, V);
-	win->MapAllXSegments(block_data, count, view->GetCamera(), H, V);
-	win->SetFgColor(firstColor);
-	win->DrawXSegments();
-   } // end of while-loop
-   win->SetFgColor(0);         // 0 = black
-   // draw the x,y,z axis
-   win->DrawRefAxis(view->GetCamera());
-} // end of DrawGDataArray()
+  // map blocks to points, segments, and planes
+  Map3D::MapBlockPoints(block_data, numSyms, view->GetCamera(), w, h);
+#ifdef WIRE_FRAME
+  Map3D::MapBlockSegments(block_data, numSyms, view->GetCamera(), w, h);
+#else
+  Map3D::MapBlockPlanes(block_data, numSyms, view->GetCamera(), w, h);
+#endif
 
+  // draw blocks
+#ifdef WIRE_FRAME
+  win->SetFgColor(BlackColor);
+  Map3D::DrawSegments(win);
+#else
+  Map3D::DrawPlanes(win);
+#endif
 
+  // now draw reference axes
+  win->SetFgColor(BlackColor);
+  Map3D::DrawRefAxis(win, view->GetCamera());
+}
