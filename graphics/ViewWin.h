0d52149ba817968995ed78233de6dde9ba18b3b7
jussi <>
1996-07-10 19:36:23 +0000

    Cleaned up code.

--- graphics/ViewWin.h
+++ graphics/ViewWin.h
@@ -1,7 +1,7 @@
 /*
   ========================================================================
   DEVise Data Visualization Software
-  (c) Copyright 1992-1995
+  (c) Copyright 1992-1996
   By the DEVise Development Group
   Madison, Wisconsin
   All Rights Reserved.
@@ -16,6 +16,10 @@
   $Id$
 
   $Log$
+  Revision 1.9  1996/06/15 13:47:50  jussi
+  Added SetFgBgColor() which allows Devise to change the view
+  foreground and background colors at runtime.
+
   Revision 1.8  1996/04/11 17:54:04  jussi
   Added Raise() and Lower().
 
@@ -76,147 +80,144 @@ class WindowRep;
 
 class ViewWin: protected WindowRepCallback {
 public:
-	ViewWin(char *name, Color foreground = ForegroundColor,
-		Color background = BackgroundColor, int weight = 1,
-		Boolean boundary = true);
+    ViewWin(char *name, Color foreground = ForegroundColor,
+            Color background = BackgroundColor, int weight = 1,
+            Boolean boundary = true);
+    virtual ~ViewWin();
 
-	/* Iconify window, if top level. Not guaranteed to succeed */
-	void Iconify();
+    /* Iconify window, if top level. Not guaranteed to succeed */
+    void Iconify();
 
-	/* Insert ViewWin into a parent */
-	void AppendToParent(ViewWin *parent);
+    /* Insert ViewWin into a parent */
+    void AppendToParent(ViewWin *parent);
 
-	/* Delete ViewWin from parent */
-	void DeleteFromParent();
+    /* Delete ViewWin from parent */
+    void DeleteFromParent();
 	
-	/* These are called by parents */
-	virtual void Map(int x, int y, unsigned w, unsigned h);
-	virtual void Unmap();
-	virtual void Iconify(Boolean iconified) = 0;
+    /* These are called by parents */
+    virtual void Map(int x, int y, unsigned w, unsigned h);
+    virtual void Unmap();
+    virtual void Iconify(Boolean iconified) = 0;
 
-	Boolean Mapped();
+    Boolean Mapped();
 
-	char *GetName(){ return _name; }
+    char *GetName(){ return _name; }
 	
-	virtual ~ViewWin();
+    int GetWeight() { return _weight; }
 
-	int GetWeight() { return _weight; }
-
-	virtual void Raise();
-	virtual void Lower();
+    virtual void Raise();
+    virtual void Lower();
 
 #if defined(MARGINS) || defined(TK_WINDOW)
-	/* Get size of margin controls */
-	void GetMargins(unsigned int &lm, unsigned int &rm,
-			unsigned int &tm, unsigned int &bm);
-	/* Get real (non-margin-adjusted) geometry of window */
-	void RealGeometry(int &x, int &y, unsigned &w, unsigned &h);
+    /* Get size of margin controls */
+    void GetMargins(unsigned int &lm, unsigned int &rm,
+                    unsigned int &tm, unsigned int &bm);
+    /* Get real (non-margin-adjusted) geometry of window */
+    void RealGeometry(int &x, int &y, unsigned &w, unsigned &h);
 #endif
 
-	/* Get current geometry of window */
-	void Geometry(int &x, int &y, unsigned &w, unsigned &h);
+    /* Get current geometry of window */
+    void Geometry(int &x, int &y, unsigned &w, unsigned &h);
 
-	/* Get absolute origin of window, if mapped */
-	void AbsoluteOrigin(int &x, int &y);
+    /* Get absolute origin of window, if mapped */
+    void AbsoluteOrigin(int &x, int &y);
 
-	/* Move/Resize self. Base class 
-	creates a new WindowRep if not already there
-	and then resizes it */
-	virtual void MoveResize(int x, int y, unsigned w, unsigned h);
+    /* Move/Resize self. Base class creates a new WindowRep
+       if not already there and then resizes it */
+    virtual void MoveResize(int x, int y, unsigned w, unsigned h);
+    
+    /* Get the Window Rep of this View window */
+    WindowRep *GetWindowRep(){ return _windowRep; }
 
-	/* Get the Window Rep of this View window */
-	WindowRep *GetWindowRep(){ return _windowRep; }
+    /* Append child. Base class just inserts the child.
+       Derived class should implement functions to MoveResize() child.*/
+    virtual void Append(ViewWin *child);
 
-	/* Append child. Base class just inserts the child.
-	Derived class should implement functions to MoveResize() child.*/
-	virtual void Append(ViewWin *child);
+    /* Delete child */
+    virtual void Delete(ViewWin *child);
 
-	/* Delete child */
-	virtual void Delete(ViewWin *child);
+    /* Replace child1 by child2. child1 must be a valid child.
+       Base class only updates list of children.*/
+    virtual void Replace(ViewWin *child1, ViewWin *child2);
 
-	/* Replace child1 by child2. child1 must be a valid child.
-	Base class only updates list of children.*/
-	virtual void Replace(ViewWin *child1, ViewWin *child2);
+    /* Swap child1 and child2. Both must be valid. Base
+       class updates only list of children. */
+    virtual void SwapChildren(ViewWin *child1, ViewWin *child2);
 
-	/* swap child1 and child2. Both must be valid. Base
-	class updates only list of children. */
-	virtual void SwapChildren(ViewWin *child1, ViewWin *child2);
+    /* Clear parent without doing anything else. 
+       This is to facilitate implementation of Replace() */
+    void SetParent(ViewWin *win) { _parent = win; }
 
-	/* Clear parent without doing anything else. 
-	This is to facilitate implementation of Replace() */
-	void SetParent(ViewWin *win) { _parent = win; }
+    /* Get the children */
+    /* return # of children */
+    int NumChildren() { return _children.Size(); }
 
-	/* Get the children */
-	/* return # of children */
-	int NumChildren() { return _children.Size(); }
-
-	/* iterator for children */
-	int InitIterator(Boolean backwards = false) {
-	  return _children.InitIterator(backwards);
-	}
-	Boolean More(int index) { return _children.More(index); }
-	virtual ViewWin *Next(int index) { return _children.Next(index); }
-	void DoneIterator(int index) { _children.DoneIterator(index); }
-
-	ViewWin *GetParent() { return _parent; }
-
-	/* Return true if window is iconified */
-	Boolean Iconified();
-
-	Color GetBgColor() { return _background; }
-	Color GetFgColor() { return _foreground; }
-	virtual void SetFgBgColor(Color fg, Color bg);
+    /* Iterator for children */
+    int InitIterator(Boolean backwards = false) {
+        return _children.InitIterator(backwards);
+    }
+    Boolean More(int index) { return _children.More(index); }
+    virtual ViewWin *Next(int index) { return _children.Next(index); }
+    void DoneIterator(int index) { _children.DoneIterator(index); }
+    
+    ViewWin *GetParent() { return _parent; }
+    
+    /* Return true if window is iconified */
+    Boolean Iconified();
+    
+    Color GetBgColor() { return _background; }
+    Color GetFgColor() { return _foreground; }
+    virtual void SetFgBgColor(Color fg, Color bg);
 
 protected:
-	/* called by base class when it has been mapped/unmapped */
-	virtual void SubClassMapped() = 0;
-	virtual void SubClassUnmapped() = 0;
+    /* called by base class when it has been mapped/unmapped */
+    virtual void SubClassMapped() = 0;
+    virtual void SubClassUnmapped() = 0;
 
-	virtual void HandleResize(WindowRep * w, int xlow,
-			int ylow, unsigned width, unsigned height);
-	
-	virtual void HandleWindowMappedInfo(WindowRep *winRep, Boolean mapped);
+    virtual void HandleResize(WindowRep * w, int xlow,
+                              int ylow, unsigned width, unsigned height);
+    
+    virtual void HandleWindowMappedInfo(WindowRep *winRep, Boolean mapped);
+    
+    /* Get total weight of children */
+    int TotalWeight();
 
-	/* Get total weight of children */
-	int TotalWeight();
-
-private:
-	Boolean _iconified;
-	Boolean _winBoundary;
-	Boolean _hasGeometry; /* TRUE if we have the geometry */
-	int _x, _y;           /* current location */
-	unsigned int _width;  /* current width */
-	unsigned int _height; /* current height */
-	ViewWinList _children;
-	ViewWin *_parent;
-	WindowRep *_windowRep;
-	char *_name;
-	int _weight;
-	Boolean _mapped;      /* TRUE if this window is mapped */
-	Color _background;    /* background color */
-	Color _foreground;    /* foreground color */
+    Boolean _iconified;
+    Boolean _winBoundary;
+    Boolean _hasGeometry; /* TRUE if we have the geometry */
+    int _x, _y;           /* current location */
+    unsigned int _width;  /* current width */
+    unsigned int _height; /* current height */
+    ViewWinList _children;
+    ViewWin *_parent;
+    WindowRep *_windowRep;
+    char *_name;
+    int _weight;
+    Boolean _mapped;      /* TRUE if this window is mapped */
+    Color _background;    /* background color */
+    Color _foreground;    /* foreground color */
 
 #ifdef MARGINS
-	void DrawMargins();
+    void DrawMargins();
 #endif
 
 #ifdef TK_WINDOW
-	void AddMarginControls();
-	void DropMarginControls();
-	void DestroyMarginControl(char *side);
-	void ReparentMarginControl(char *side, int xoff, int yoff);
-	void ResizeMargins(unsigned int w, unsigned int h);
-	void ToggleMargins();
-
-	Boolean _marginsOn;
-	char    _tkPathName[32];
+    void AddMarginControls();
+    void DropMarginControls();
+    void DestroyMarginControl(char *side);
+    void ReparentMarginControl(char *side, int xoff, int yoff);
+    void ResizeMargins(unsigned int w, unsigned int h);
+    void ToggleMargins();
+    
+    Boolean _marginsOn;
+    char    _tkPathName[32];
 #endif
-
+    
 #if defined(MARGINS) || defined(TK_WINDOW)
-	unsigned int _leftMargin;
-	unsigned int _rightMargin;
-	unsigned int _topMargin;
-	unsigned int _bottomMargin;
+    unsigned int _leftMargin;
+    unsigned int _rightMargin;
+    unsigned int _topMargin;
+    unsigned int _bottomMargin;
 #endif
 };
 
