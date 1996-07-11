31a898e2f17c4074a10dde3cf01f0d89939392ab
jussi <>
1996-07-11 18:17:04 +0000

    Removed all non-essential code. The Tcl batch client no longer uses any Tk (X11) features.

--- lib/batch.tcl
+++ lib/batch.tcl
@@ -15,37 +15,19 @@
 #  $Id$
 
 #  $Log$
+#  Revision 1.2  1996/05/14 15:09:21  jussi
+#  Added a few global variables.
+#
 #  Revision 1.1  1996/04/18 18:18:02  jussi
 #  Initial revision.
-#
 
 ############################################################
-
-set DEViseColors \
-	{{0 black} {1 white} {2 red} {3 blue} {4 orange} \
-	{5 tan} {6 green} {7 purple} {8 aquamarine} {9 peru} {10 chocolate} \
-	{11 tomato} {12 pink} {13 plum} {14 azure1} {15 cyan1} {16 SeaGreen1} \
-	{17 khaki1} {18 goldenrod1} {19 yellow} {20 sienna1} \
-	{21 LightCoral} {22 AntiqueWhite} {23 LemonChiffon} {24 LightGray}\
-	{25 Lavender} {26 LavenderBlush} {27 MistyRose} {28 NavyBlue} \
-	{29 SlateBlue} {30 MediumBlue} {31 DeepSkyBlue} {32 SkyBlue} \
-	{33 red1} {34 red2} {35 green1} {36 green2} {37 blue1} \
-	{38 blue2} {39 blue3} {40 blue4} {41 gold1} {42 gold2}}
-
-set DEViseShapes \
-	{{0 Rect} {1 RectX} {2 Bar} {3 Polygon} {4 Oval} {5 Vector} \
-	{6 Block} {7 3DVector} {8 HorLine}}
 
 set restoring 0
 set template 0
 set sessionName "session.tk"
 set curView ""
-set lastView ""
-set historyWinOpened 0
-set queryWinOpened 0
-set stackWinOpened 0
-set helpWindowCreated 0
-set UserMode 1
+set mode 0
 
 set libdir "."
 if { [info exists env(DEVISE_LIB)] } {
@@ -79,16 +61,9 @@ if {![catch {set hostCfg $libdir/config.$env(HOSTNAME)}]} {
     }
 }
 
-source $libdir/dialog.tk
 source $libdir/macrodef.tk
 source $libdir/util.tcl
 source $libdir/sources.tcl
-
-set mode 0
-DEVise changeMode $mode
-
-set backGround DarkOliveGreen
-set foreGround white
 
 set progName [ file tail $argv0 ]
 set envName [ format "%s_RC" [string toupper $progName] ]
@@ -112,12 +87,3 @@ if {$rcFile != ""} {
     puts "Reading initialization file $rcFile"
     source $rcFile
 }
-
-wm geometry . +0+0
-
-tk_setPalette background $backGround foreground $foreGround \
-	selectBackground $foreGround selectForeground $backGround
-. configure -bg $backGround
-
-message .msg -justify center -width 10c -text "Executing batch file..."
-pack .msg -side top -padx 5m -pady 5m
