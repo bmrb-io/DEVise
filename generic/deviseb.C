6997858508841d0970a75dff3c62793fbec766d7
jussi <>
1996-07-16 23:34:36 +0000

    Improved parsing and added handling of comment lines, blank lines, and lines prefixed with the DEVise keyword.

--- generic/deviseb.C
+++ generic/deviseb.C
@@ -16,6 +16,11 @@
   $Id$
 
   $Log$
+  Revision 1.9  1996/07/09 15:59:37  wenger
+  Added master version number and compile date to C++ code (also displayed
+  in the user interface); added -usage and -version command line arguments;
+  updated usage message.
+
   Revision 1.8  1996/05/20 18:46:37  jussi
   Merged with ClientServer library code.
 
@@ -46,12 +51,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "ClientAPI.h"
 #include "Version.h"
 
 #define DOASSERT(c,r) { if (!(c)) DoAbort(r); }
 //#define DEBUG
+
+static char *_deviseKeyword = "DEVise ";
+static char _commentChar = '#';
 
 static char *_progName = 0;
 static char *_hostName = "localhost";
@@ -65,7 +74,7 @@ void DoAbort(char *reason)
 {
   fprintf(stderr, "An internal error has occurred. Reason:\n  %s\n", reason);
   (void)NetworkClose(_deviseFd);
-  exit(1);
+  exit(2);
 }
 
 void ControlCmd(int argc, char **argv)
@@ -91,7 +100,7 @@ int DEViseCmd(int argc, char **argv)
 
   if (NetworkSend(_deviseFd, API_CMD, 0, argc, argv) < 0) {
     fprintf(stderr, "Server has terminated. Client exits.\n");
-    exit(1);
+    exit(3);
   }
 
   u_short flag;
@@ -100,10 +109,12 @@ int DEViseCmd(int argc, char **argv)
     char **rargv;
     if (NetworkReceive(_deviseFd, 1, flag, rargc, rargv) <= 0) {
       fprintf(stderr, "Server has terminated. Client exits.\n");
-      exit(1);
+      exit(3);
     }
     if (flag == API_CTL)
       ControlCmd(rargc, rargv);
+    if (flag == API_NAK)
+      printf("Received error message: %s\n", rargv[0]);
   } while (flag != API_ACK && flag != API_NAK);
 
   if (flag == API_NAK)
@@ -112,21 +123,27 @@ int DEViseCmd(int argc, char **argv)
   return 1;
 }
 
-int ExecuteFile(char *script)
+int ExecuteCommands()
 {
-  FILE *fp = fopen(script, "r");
-  if (!fp) {
-    printf("Cannot open scrip file: %s\n", script);
-    perror("fopen");
-    return -1;
-  }
+  FILE *fp = stdin;
 
-  const int MAXARGC = 20;
+  const int MAXARGC = 64;
   char *argv[MAXARGC];
   int i;
   for(i = 0; i < MAXARGC; i++) {
     argv[i] = new char [64];
     DOASSERT(argv[i], "Out of memory");
+  }
+
+#ifdef DEBUG
+  printf("Setting server to batch mode\n");
+#endif
+
+  strcpy(argv[0], "setBatchMode");
+  strcpy(argv[1], "1");
+  if (DEViseCmd(2, argv) < 0) {
+    fprintf(stderr, "Error setting server to batch mode\n");
+    return -1;
   }
 
   char line[256];
@@ -136,26 +153,54 @@ int ExecuteFile(char *script)
       return -1;
     }
     line[strlen(line) - 1] = 0;
-    if (!strlen(line))
+
+    char *ptr = line;
+    while(isspace(*ptr)) ptr++;
+
+    if (*ptr == _commentChar) {
+#ifdef DEBUG
+      printf("Read comment line: \"\%s\"\n", ptr);
+#endif
       continue;
+    }
+
+    if (*ptr == _deviseKeyword[0]
+        && !strncmp(ptr, _deviseKeyword, strlen(_deviseKeyword))) {
+      ptr += strlen(_deviseKeyword);
+      while(isspace(*ptr)) ptr++;
+    }
+
+    if (!*ptr) {
+#ifdef DEBUG
+      printf("Read blank line (maybe with DEVise keyword)\n");
+#endif
+      continue;
+    }
 
     int argc = 0;
     for(i = 0; i < MAXARGC; i++)
       argv[i][0] = 0;
 
-    char *ptr = line;
     int braces = 0;
+    int inarg = 0;
+    char *argPtr = argv[0];
+
     while(*ptr) {
       if (*ptr == '{') {
 	if (braces > 0)
-	  strcat(argv[argc], "{");
+          *argPtr++ = '{';
 	++braces;
+        inarg = 1;
+        ptr++;
       } else if (*ptr == '}') {
 	--braces;
 	if (braces > 0)
-	  strcat(argv[argc], "}");
+          *argPtr++ = '}';
+        inarg = 1;
+        ptr++;
       } else if (*ptr == ' ') {
 	if (!braces) {
+          *argPtr = 0;
 #ifdef DEBUG
 	  printf("Argument %d is \"%s\"\n", argc, argv[argc]);
 #endif
@@ -164,18 +209,27 @@ int ExecuteFile(char *script)
 	    fprintf(stderr, "Line has too many elements: %s\n", line);
 	    return -1;
 	  }
-	} else
-	  strcat(argv[argc], " ");
+          argPtr = argv[argc];            
+          while(isspace(*ptr)) ptr++;
+          inarg = 0;
+	} else {
+          *argPtr++ = ' ';
+          inarg = 1;
+          ptr++;
+        }
       }	else {
-	char two[2];
-	two[0] = *ptr;
-	two[1] = 0;
-	strcat(argv[argc], two);
+        *argPtr++ = *ptr++;
+        inarg = 1;
       }
-      ptr++;
     }
 
-    ++argc;
+    if (inarg) {
+      *argPtr = 0;
+#ifdef DEBUG
+      printf("Argument %d is \"%s\"\n", argc, argv[argc]);
+#endif
+      ++argc;
+    }
 
     if (argc == 1 && !strcmp(argv[0], "sync"))
       _syncDone = 0;
@@ -196,7 +250,7 @@ int ExecuteFile(char *script)
 	char **rargv;
 	if (NetworkReceive(_deviseFd, 1, flag, rargc, rargv) <= 0) {
 	  fprintf(stderr, "Server has terminated. Client exits.\n");
-	  exit(1);
+	  exit(3);
 	}
 	if (flag == API_CTL)
 	  ControlCmd(rargc, rargv);
@@ -206,8 +260,6 @@ int ExecuteFile(char *script)
     }
   }
 
-  fclose(fp);
-
   for(i = 0; i < MAXARGC; i++)
     delete argv[i];
 
@@ -216,8 +268,7 @@ int ExecuteFile(char *script)
 
 void Usage()
 {
-  fprintf(stderr, "Usage: %s [-h host] [-p port] script\n",
-	  _progName);
+  fprintf(stderr, "Usage: %s [-h host] [-p port]\n", _progName);
 }
 
 int main(int argc, char **argv)
@@ -229,7 +280,7 @@ int main(int argc, char **argv)
       if (i + 1 >= argc) {
 	fprintf(stderr, "No host name specified with -h option.\n");
 	Usage();
-	exit(1);
+	exit(4);
       }
       _hostName = argv[i + 1];
       i++;
@@ -237,22 +288,14 @@ int main(int argc, char **argv)
       if (i + 1 >= argc) {
 	fprintf(stderr, "No port number specified with -p option.\n");
 	Usage();
-	exit(1);
+	exit(4);
       }
       _portNum = atoi(argv[i + 1]);
       i++;
     } else {
-      if (_scriptFile) {
-	Usage();
-	exit(1);
-      }
-      _scriptFile = argv[i];
+      Usage();
+      exit(4);
     }
-  }
-
-  if (!_scriptFile) {
-    Usage();
-    exit(1);
   }
 
   Version::PrintInfo();
@@ -265,12 +308,12 @@ int main(int argc, char **argv)
 
   _deviseFd = NetworkOpen(_hostName, _portNum);
   if (_deviseFd < 0)
-    exit(1);
+    exit(2);
 	
   printf("Connection established.\n\n");
 
-  if (ExecuteFile(_scriptFile) < 0)
-    return 2;
+  if (ExecuteCommands() < 0)
+    exit(5);
 
   printf("Closing connection.\n");
 
