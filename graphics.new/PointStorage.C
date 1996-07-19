4b9382d3415d139fc07d7dc5f3585c5a10b70516
jussi <>
1996-07-19 03:23:59 +0000

    Initial revision.

---
+++ graphics.new/PointStorage.C
@@ -0,0 +1,28 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$*/
+
+#ifdef __GNUG__
+#pragma implementation "HashTable.h"
+#endif
+
+#include "PointStorage.h"
+
+#ifdef __GNUG__
+template class HashTable<RecId, PointStorage::PointRec>;
+#endif
---
+++ graphics.new/PointStorage.h
@@ -0,0 +1,99 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$*/
+
+#ifndef PointStorage_h
+#define PointStorage_h
+
+#include <stdio.h>
+
+#include "DeviseTypes.h"
+#include "RecId.h"
+#include "Color.h"
+#include "HashTable.h"
+
+//#define DEBUGPS
+
+class PointStorage {
+  public:
+    PointStorage() : _table(30, RecIdHash) {}
+        
+    void Clear() {
+#ifdef DEBUGPS
+        printf("Clearing point storage 0x%p\n", this);
+#endif
+        _table.clear();
+    }
+
+    void Insert(RecId id, Coord x, Coord y, Color c) {
+        PointRec point;
+        if (_table.lookup(id, point) >= 0) {
+            printf("Warning: record %ld already in point storage\n", id);
+            return;
+        }
+#ifdef DEBUGPS
+        printf("Inserting <%ld,%.2f,%.2f,%ld> to point storage 0x%p\n",
+               id, x, y, c, this);
+#endif
+        point.x = x;
+        point.y = y;
+        point.c = c;
+        _table.insert(id, point);
+    }
+
+    Boolean Find(RecId id, Coord &x, Coord &y, Color &c) {
+        PointRec point;
+        if (_table.lookup(id, point) >= 0) {
+            x = point.x;
+            y = point.y;
+            c = point.c;
+#ifdef DEBUGPS
+            printf("Found <%ld,%.2f,%.2f,%ld> in point storage 0x%p\n",
+                   id, x, y, c, this);
+#endif
+            return true;
+        }
+        return false;
+    }
+
+    Boolean Remove(RecId id) {
+        if (_table.remove(id) >= 0) {
+#ifdef DEBUGPS
+            printf("Removed <%ld> from point storage 0x%p\n", id, this);
+#endif
+            return true;
+        }
+        printf("Warning: record %ld not found in point storage\n", id);
+        return false;
+    }
+
+  protected:
+    static int RecIdHash(RecId &id, int numBuckets) {
+        return id % numBuckets;
+    }
+
+    struct PointRec {
+        Coord x;
+        Coord y;
+        Color c;
+    };
+
+    HashTable<RecId, PointRec> _table;
+};
+
+#endif
