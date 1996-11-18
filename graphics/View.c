45e98026159dada43c4efc04c2e8caca8dfb3f8b
donjerko <>
1996-11-18 18:12:53 +0000

    Added DTE make stuff

--- graphics/View.c
+++ graphics/View.c
@@ -16,6 +16,16 @@
   $Id$
 
   $Log$
+  Revision 1.82  1996/11/13 16:56:13  wenger
+  Color working in direct PostScript output (which is now enabled);
+  improved ColorMgr so that it doesn't allocate duplicates of colors
+  it already has, also keeps RGB values of the colors it has allocated;
+  changed Color to GlobalColor, LocalColor to make the distinction
+  explicit between local and global colors (_not_ interchangeable);
+  fixed global vs. local color conflict in View class; changed 'dali'
+  references in command-line arguments to 'tasvir' (internally, the
+  code still mostly refers to Dali).
+
   Revision 1.81  1996/11/07 22:40:14  wenger
   More functions now working for PostScript output (FillPoly, for example);
   PostScript output also working for piled views; PSWindowRep member
@@ -332,7 +342,7 @@
 
 #include <time.h>
 
-//#define DEBUG
+// #define DEBUG
 
 #include "Util.h"
 #include "View.h"
