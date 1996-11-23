37444d4c7ce1e5523f6fa3dd181e70433f825d9f
jussi <>
1996-11-23 21:15:21 +0000

    Minor improvements.

--- graphics.new/TDataRangeList.c
+++ graphics.new/TDataRangeList.c
@@ -1,68 +1,68 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:15:59  jussi
+  Added CVS header.
+*/
 
 #include "TDataRangeList.h"
 #include "RangeList.h"
 
-TDataRangeListEntry *TDataRangeList::_freeList = NULL;
-
-/* Constructor */
-TDataRangeList::TDataRangeList (){
-	int i;
-	for (i=0; i < TDataRangeListHashSize; i++)
-		_hashTbl[i] = NULL;
+TDataRangeList::TDataRangeList()
+{
+    for(int i = 0; i < TDataRangeListHashSize; i++)
+        _hashTbl[i] = NULL;
 }
 
-TDataRangeListEntry *TDataRangeList::Find(TData *tdata){
-	int bucket = Hash(tdata);
-	TDataRangeListEntry *cur;
-	for (cur=_hashTbl[bucket]; cur != NULL; cur = cur->next){
-		if (cur->tdata == tdata)
-			break;
-	}
-	return cur;
+TDataRangeListEntry *TDataRangeList::Find(TData *tdata)
+{
+    int bucket = Hash(tdata);
+    TDataRangeListEntry *cur;
+    for(cur = _hashTbl[bucket]; cur != NULL; cur = cur->next) {
+        if (cur->tdata == tdata)
+            break;
+    }
+    return cur;
 }
 
-RangeList *TDataRangeList::Get(TData *tdata){
-	TDataRangeListEntry *cur = Find(tdata);
-	if (cur == NULL){
-		/* create a new one */
-		cur = AllocEntry();
-		cur->tdata = tdata;
-		cur->rangeList = new RangeList;
-		int bucket = Hash(tdata);
-		cur->next = _hashTbl[bucket];
-		_hashTbl[bucket] = cur;
-	}
-	return cur->rangeList;
+RangeList *TDataRangeList::Get(TData *tdata)
+{
+    TDataRangeListEntry *cur = Find(tdata);
+    if (cur == NULL) {
+        /* create a new one */
+        cur = new TDataRangeListEntry;
+        cur->tdata = tdata;
+        cur->rangeList = new RangeList;
+        int bucket = Hash(tdata);
+        cur->next = _hashTbl[bucket];
+        _hashTbl[bucket] = cur;
+    }
+    return cur->rangeList;
 }
 
-TDataRangeListEntry *TDataRangeList::AllocEntry(){
-	TDataRangeListEntry *entry;
-	if (_freeList == NULL)
-		entry =new TDataRangeListEntry();
-	else {
-		entry = _freeList;
-		_freeList = _freeList->next;
-	}
-	return entry;
-}
-
-void TDataRangeList::FreeEntry(TDataRangeListEntry *entry) {
-	entry->next = _freeList;
-	_freeList = entry;
-}
-
-void TDataRangeList::Clear(){
-	int i;
-	for (i=0; i < TDataRangeListHashSize; i++){
-		while(_hashTbl[i] != NULL){
-			TDataRangeListEntry *entry = _hashTbl[i];
-			_hashTbl[i] = _hashTbl[i]->next;
-			delete entry->rangeList;
-			FreeEntry(entry);
-		}
-	}
+void TDataRangeList::Clear()
+{
+    for(int i = 0; i < TDataRangeListHashSize; i++) {
+        while(_hashTbl[i] != NULL) {
+            TDataRangeListEntry *entry = _hashTbl[i];
+            _hashTbl[i] = _hashTbl[i]->next;
+            delete entry->rangeList;
+            delete entry;
+        }
+    }
 }
--- graphics.new/TDataRangeList.h
+++ graphics.new/TDataRangeList.h
@@ -1,44 +1,60 @@
 /*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1996
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
   $Id$
 
-  $Log$*/
+  $Log$
+  Revision 1.2  1995/09/05 22:16:00  jussi
+  Added CVS header.
+*/
 
 #ifndef TDataRangeList_h
 #define TDataRangeList_h
 
 class TData;
 class RangeList;
+
 struct TDataRangeListEntry {
-	TData *tdata;
-	RangeList *rangeList;
-	TDataRangeListEntry *next;
+    TData *tdata;
+    RangeList *rangeList;
+    TDataRangeListEntry *next;
 };
 
 const int TDataRangeListHashSize = 11;
+
 class TDataRangeList {
 public:
+    /* constructor */
+    TDataRangeList();
 
-	/* constructor */
-	TDataRangeList();
+    /* Get the RangeList associated with tdata. Create one if
+       there isn't one already */
+    RangeList *Get(TData *tdata);
 
-	/* Get the RangeList associated with tdata. Create one if
-	there isn't one already */
-	RangeList *Get(TData *tdata);
+    /* Clear all RangeLists, but don't free the RangeInfos */
+    void Clear();
 
-	/* Clear all RangeLists, but don't free the RangeInfos */
-	void Clear();
 private:
-	/* Find entry. Return NULL if not found */
-	TDataRangeListEntry *Find(TData *tdata);
+    /* Find entry. Return NULL if not found */
+    TDataRangeListEntry *Find(TData *tdata);
 
-	int Hash(TData *tdata){
-		return ((unsigned)tdata) % TDataRangeListHashSize;
-	}
+    int Hash(TData *tdata) {
+        return ((unsigned)tdata) % TDataRangeListHashSize;
+    }
 
-	TDataRangeListEntry *AllocEntry();
-	void FreeEntry(TDataRangeListEntry *entry);
-	
-	TDataRangeListEntry *_hashTbl[TDataRangeListHashSize];
-	static TDataRangeListEntry *_freeList;
+    TDataRangeListEntry *_hashTbl[TDataRangeListHashSize];
 };
+
 #endif
