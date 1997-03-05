67cb05c55f6189bc26ba38729571ed822a0c3f69
donjerko <>
1997-03-05 18:35:28 +0000

    Initial GUI sources for DTE.

---
+++ DTE/GUI/ParseAPIDTE.h
@@ -0,0 +1,8 @@
+#ifndef ParseAPIDTE_h
+#define ParseAPIDTE_h
+
+#include "Control.h"
+
+extern int ParseAPIDTE(int argc, char **argv, TkControlPanel *control);
+
+#endif
---
+++ DTE/GUI/ParseAPIParent.h
@@ -0,0 +1,9 @@
+#ifndef ParseAPIParent_h
+#define ParseAPIParent_h
+
+#include "ParseAPIDTE.h"
+#include "Control.h"
+
+extern int ParseAPIParent(int argc, char **argv, TkControlPanel *control);
+
+#endif
