b6cdbabcbd722a0d50910c34633256b0e5059822
arvind <>
1997-05-30 15:44:36 +0000

    External Sorting code using Qsort for in-memory sorting and priority queues for merging.

--- DTE/Core/Sort.c
+++ DTE/Core/Sort.c
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.6  1997/04/08 01:47:33  donjerko
+  Set up the basis for ORDER BY clause implementation.
+
   Revision 1.5  1997/04/04 23:10:25  donjerko
   Changed the getNext interface:
   	from: Tuple* getNext()
@@ -28,32 +31,232 @@
 
  */
 
+#include <assert.h>
 #include "Sort.h"
-#include "StandardRead.h"
-#include "Inserter.h"
-#include "types.h"
+  
 
-#define MAX_MEM		16000000
-#define RUNS		10
+void Sort::initialize()
+{
 
+   input->initialize();
+   tuple_size = numFlds * sizeof(Type *); 
+   Nruns = 0;
+   temp_filename = new char[20];
+   strcpy(temp_filename, "sort_temp_file");   
 
-void Sort::initialize(){
-	// creates all the runs
-	// Generate run using input->getNext
-	// when ready to output use inserter->insert(tuple) 
-	// To create temp files use Inserter(), open ...
-	// MemorySize is a constant (compile time)
+   // Creates the runs and sets up the Q for merging the runs
+   comparePtrs  = new (GeneralPtr *)[numFlds];
+   order = Ascending;
+ 
+   for (int i=0; i < numFlds; i++)
+    {
+       TypeID retVal;  // is a dummy	       
+       TRY(comparePtrs[i] = getOperatorPtr("comp",attrTypes[i], 
+                                            attrTypes[i],retVal),);
+    }
+  
+   generate_runs(); 
 
-	// use function pointers for sorting (types.h: getOperatorPtr).
+   // Create a priority queue in memory to merge runs
+   Q = new PQueue(numFlds,attrTypes,numSortFlds,sortFlds, order, Nruns);
+   Tuple *tuple_area = new Tuple[tuple_size*Nruns];
 
-	input->initialize();
+   // Open up all the temp files
+   input_buf = new StandardRead[Nruns];
+   temp_files = new ifstream[Nruns];
+
+   char *filename;
+   char run_num[5];
+   for (int i = 0; i < Nruns; i++)  // Enqueue first element of the Nruns
+    {
+      // open each temp file
+      filename = strdup(temp_filename); 
+      sprintf(run_num, "%d", i+1);
+      strcat(filename, run_num);
+      temp_files[i].open(filename);
+      input_buf[i].open(&temp_files[i], numFlds, attrTypes);
+      delete filename;
+
+      // Read the next tuple
+      Tuple *next_tuple = tuple_area + tuple_size*i;
+      if  (!input_buf[i].getNext(next_tuple))  // Empty run??
+        continue;
+      
+      // Create a new node to be inserted into Q
+      Node* node_ptr    = new Node;
+      node_ptr->tuple   = new Tuple[numFlds];
+      node_ptr->run_num = i;    // setup run number
+      
+      memcpy(node_ptr->tuple, next_tuple, tuple_size);
+      Q->enq(node_ptr);     
+   }
+
+   delete [] tuple_area;
+   return;
 }
 
-bool Sort::getNext(Tuple* next){
-	// open all the runs
-	// To read temp file use StandardRead ...
-	// use heap to get max/min
-	// copy the next tuple over "next"
+bool Sort::getNext(Tuple* tuple)
+{
+  // Return the next value from the prority queue
+  // Also insert into the queue the next value from the appropriate input 
+  // buffer - the one corresponding to the value just dequeued.
 
-	return input->getNext(next);
+  if (Q->num_elems() == 0) 
+     return false; 
+  
+  // Dequeue min/max from priority queue
+  Node* node_ptr = Q->deq();
+  memcpy(tuple, node_ptr->tuple, tuple_size);  
+
+  // Retrieve next value from appropriate input buffer 
+  Tuple* new_tuple = new Tuple[numFlds];
+
+  if (input_buf[node_ptr->run_num].getNext(new_tuple))        
+   {
+      // Enqueue the retrieved tuple 
+      Node *new_node = new Node;
+      new_node->tuple = new Tuple [numFlds];
+      memcpy(new_node->tuple, new_tuple, numFlds * sizeof(Type *));  
+
+      new_node->run_num = node_ptr->run_num;
+      Q->enq(new_node);
+   }
+
+  delete new_tuple;
+  return true;
 }
+
+
+void Sort::generate_runs()
+{
+
+   // Generate the runs for subsequent merging
+   // Use quick sort to perform the sorting
+
+   Tuple *table[MAX_MEM]; // array of pointers to Tuple
+   int count = 0;
+   int offset = 0;
+   output_buf = new Inserter();	
+   Tuple *tuple_area = new Tuple[numFlds*MAX_MEM]; 
+
+   // Read in tuples and set pointers in the table 
+   while(input->getNext(tuple_area+offset)) 
+   {
+     table[count] = tuple_area + offset;
+     offset += tuple_size;
+     count++;
+
+     if (count >= MAX_MEM/(int)sizeof(Tuple)) //Number of tuples exceeds memory
+       {
+         sort_and_write_run(table, count);
+         count = 0;
+         offset = 0;
+       }
+   }
+   
+   if (count) // there are tuples left to be written out
+     sort_and_write_run(table, count);
+
+   delete []table;
+   delete []tuple_area;
+   return;
+}
+
+void Sort::sort_and_write_run(Tuple **table, int length)
+{
+     Nruns++;       
+     qsort(table,0,length-1); 
+     insert_sort(table,length);
+		   
+     out_temp_file = new ofstream;   
+     char *filename = strdup(temp_filename);
+     char run_num[5];
+     sprintf(run_num, "%d", Nruns);
+     strcat(filename, run_num);
+     unlink(filename);
+     
+     out_temp_file->open(filename);
+     output_buf->open(out_temp_file, numFlds, attrTypes);
+
+     for (int i = 0; i < length; i++)
+       output_buf->insert(table[i]);                
+   
+     out_temp_file->close();
+     return;
+}
+
+void Sort::qsort(Tuple **A, int left, int right)
+{
+  // This sorts the array almost completely
+  // A call to this function must be succeeded by 
+  // a run of insertion sort to complete the sort
+ 
+  int cutoff =1;
+
+  if (left + cutoff < right)
+    {
+      Tuple *pivot = find_pivot(A, left, right);
+      int i = left;
+      int j = right-1;
+ 
+      while (true)
+	{
+	  while (tupleCompare(sortFlds, numSortFlds, comparePtrs,
+                              A[++i], pivot) < 0);  // A[++i] < pivot
+          while (tupleCompare(sortFlds, numSortFlds, comparePtrs, 
+                              A[--j], pivot) > 0);
+          if (i < j)     
+            swap (A[i], A[j]);
+          else
+	    break;
+	}
+
+      swap(A[i], A[right-1]);
+
+      qsort(A, left, i-1);
+      qsort(A, i+1, right);
+    }
+}
+
+Tuple* Sort::find_pivot(Tuple **A,int left, int right)
+{
+  // Pivot is median of left, right and center values
+
+  int center = (left + right)/2 ;  
+
+  if (tupleCompare(sortFlds, numSortFlds, comparePtrs, A[center],A[left]) < 0)
+    swap(A[left], A[center]);
+  if (tupleCompare(sortFlds, numSortFlds, comparePtrs, A[right], A[left]) < 0)
+    swap(A[left], A[right]);
+  if (tupleCompare(sortFlds, numSortFlds, comparePtrs, A[right], A[center])< 0)
+    swap(A[center], A[right]);
+
+  swap(A[center], A[right-1]);
+  return A[right-1];
+}
+
+inline void Sort::swap(Tuple *&left, Tuple *&right) 
+{
+  Tuple *temp = left;
+  left = right;
+  right = temp;
+  return;
+} 
+
+void Sort::insert_sort(Tuple **A, int length)
+{
+  Tuple *temp;
+  
+  for (int i = 1; i < length; i++)
+    {
+      temp = A[i];
+      int j;
+      for (j = i; j > 0 && tupleCompare(sortFlds, numSortFlds, 
+                               comparePtrs, temp, A[j-1]) < 0; j--)
+        A[j] = A[j-1];
+      A[j] = temp;
+  }
+
+  return;
+}
+
--- DTE/Core/Sort.h
+++ DTE/Core/Sort.h
@@ -16,6 +16,9 @@
   $Id$
 
   $Log$
+  Revision 1.5  1997/04/08 01:47:34  donjerko
+  Set up the basis for ORDER BY clause implementation.
+
   Revision 1.4  1997/04/04 23:10:26  donjerko
   Changed the getNext interface:
   	from: Tuple* getNext()
@@ -31,32 +34,105 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <fstream.h>
+
 #include "site.h"
+#include "Iterator.h"
+#include "Inserter.h"
+#include "StandardRead.h"
+#include "types.h"
+#include "PQueue.h"
+
+#define MAX_MEM         800000 
+#define NUM_RUNS       	1000
 
 class Sort : public Site {
-	List<BaseSelection*>* orderBy;
-	Site* input;
-	int* orderByNumbers;
+private:
+  
+  List<BaseSelection*>* orderBy;
+  Site* input;              // The iterator
+  PQueue *Q;                // Internal Priority Queue
+
+  StandardRead *input_buf;  // Used to read in the sorted runs while merging
+  ifstream  *temp_files;    // Temporary files that hold the runs
+  Inserter  *output_buf;    // Output buffer in which current run is placed
+  ofstream  *out_temp_file; // File connected with output_buf
+  char      *temp_filename; // Name of temporary file
+    
+  int       Nruns;          // Number of runs
+  TypeID    *attrTypes;     // Attribute types of the tuple fields
+  int       tuple_size;     // Size of a tuple
+   
+  int       numSortFlds;    // Number of fields on which tuple is to be sorted
+  int       *sortFlds;     // Index of fields to be sorted on 
+  SortOrder order;          // Ascending or Descending
+  GeneralPtr **comparePtrs; // Comparison operators
+    
+  void generate_runs();
+//  void PQ1_generate_runs();
+//  void PQ2_generate_runs();
+
+  Tuple *find_pivot(Tuple **, int, int);
+  inline void swap(Tuple *&, Tuple*&);
+  void qsort(Tuple **, int, int);
+  void insert_sort(Tuple **, int);
+  void sort_and_write_run(Tuple **, int);
+
 public:
-	Sort(String nm, List<BaseSelection*>* orderBy, Site* input) :
-		Site(nm), orderBy(orderBy), input(input) {
-		orderByNumbers = NULL;
-	}
-     virtual ~Sort() {}
-	/*
-     virtual int getNumFlds(){
-		return input->getNumFlds();
-	}
-     virtual String *getTypeIDs() = 0;
-     virtual String *getAttributeNames() = 0;
-     virtual WritePtr* getWritePtrs(){
-          assert(!"getWritePtrs not implemented");
-          return NULL;
+
+    Sort(String nm, List<BaseSelection*>* orderBy, Site* input): /*SortOrder*/
+       Site(nm), orderBy(orderBy), input(input) /*, order(order) */ 
+       {}
+
+    ~Sort()
+      { 
+         if (temp_files) delete [] temp_files; 
+         if (out_temp_file) delete out_temp_file; 
+         if (output_buf) delete output_buf; 
+         if (input_buf) delete [] input_buf;
+         if (Q) delete Q;
+         if (attrTypes) delete attrTypes;
+         if (comparePtrs) delete comparePtrs;
+
+         char *filename;
+         for (int i =0; i < Nruns; i++)
+	   {
+            filename= strdup(temp_filename);
+            char run_num[5];
+            sprintf(run_num, "%d", i+1);
+            strcat(filename, run_num);
+            unlink(filename);
+	  }
+     }    
+
+     virtual inline int getNumFlds(){
+		return numFlds;
      }
-     virtual String *getOrderingAttrib(){
+     virtual inline String *getTypeIDs(){
+                return attrTypes;
+     }  
+     virtual inline String *getAttributeNames(){
+                return input->getAttributeNames();
+     }
+     virtual inline String *getOrderingAttrib(){
           assert(0);
      }
-	*/
+
+     int getNumSortFlds() {
+      return numSortFlds;
+     }
+
+     int *getSortFlds() {
+      return sortFlds;
+     }
+  
+     GeneralPtr **getComparePtrs()   {
+      return comparePtrs;
+     }
+    
+     virtual void initialize();         // To be called once at the start
+     virtual bool getNext(Tuple* next); // returns false when no more tuples
+
 	virtual Stats* getStats(){
 		return input->getStats();
 	}
@@ -65,24 +141,25 @@ public:
 		tmpL->append(input);
 		TRY(typifyList(mySelect, tmpL), );
 		numFlds = mySelect->cardinality();
-		TypeID* types = getTypesFromList(mySelect);
-		TRY(orderByNumbers = findPositions(mySelect, orderBy), );
-		cout << "Order by not implemented yet, (index, type) = ";
-		for(int i = 0; i < orderBy->cardinality(); i++){
-			cout << "(" << orderByNumbers[i] << ", ";
-			cout << types[orderByNumbers[i]] << ") ";
+		attrTypes = getTypesFromList(mySelect);
+		TRY(sortFlds = findPositions(mySelect, orderBy), );
+                numSortFlds = orderBy->cardinality();
+		cout << "Sort fields are (index, type) = ";
+		for(int i = 0; i < numSortFlds; i++){
+			cout << "(" << sortFlds[i] << ", ";
+			cout << attrTypes[sortFlds[i]] << ") ";
 		}
 		cout << endl;
-		delete [] types;
 	}
-     virtual void enumerate(){
+
+       virtual void enumerate(){
           assert(input);
           List<BaseSelection*>* baseSchema = input->getSelectList();
           TRY(enumerateList(mySelect, name, baseSchema), );
           input->enumerate();
-     }
-	virtual void initialize();
-     virtual bool getNext(Tuple* next);
+      }
+
 };
 
 #endif
+ 
