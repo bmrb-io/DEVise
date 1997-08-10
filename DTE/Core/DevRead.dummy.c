3e95d8a975ed0ab02a8c84d617b1b4ab7a5e3764
donjerko <>
1997-08-10 18:38:20 +0000

    This file is used when unidata is not being linked in.

---
+++ DTE/Core/DevRead.dummy.c
@@ -0,0 +1,33 @@
+#include <strstream.h>
+
+#undef assert
+#include <assert.h>
+
+#include "types.h"
+#include "DevRead.h"
+
+
+TypeID translateUDType(Attr* at){
+   return "unknown";
+}
+
+void DevRead::Open(char* schemaFile, char* dataFile){ // throws
+}
+
+Iterator* DevRead::createExec(){
+	return NULL;
+}
+
+DevReadExec::~DevReadExec(){
+}
+
+const Tuple* DevReadExec::getNext(){
+	return NULL;
+}
+
+const Tuple* DevReadExec::getThis(Offset offset, RecId recId){
+	return NULL;
+}
+
+void DevRead::Close(){
+}
