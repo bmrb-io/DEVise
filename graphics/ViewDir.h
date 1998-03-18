d9a44cef2322d5f21a01efc93b630304f5e52271
wenger <>
1998-03-18 19:27:22 +0000

    Zhenhai forgot to commit this.

---
+++ graphics/ViewDir.h
@@ -0,0 +1,33 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  Declaration of ViewDir (view direction?).
+ */
+
+/*
+  $Id$
+
+  $Log$
+ */
+
+
+#ifndef ViewDir_h_
+#define ViewDir_h_
+
+// viewing direction
+// need modification later
+enum ViewDir {PosX, PosY, PosZ, NegX, NegY, NegZ};
+
+#endif // ViewDir_h_
