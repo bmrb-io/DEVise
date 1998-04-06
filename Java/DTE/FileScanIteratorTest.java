a81791bb579862b4c47c8fbc0ac670febc2d958e
gang <>
1998-04-06 03:20:36 +0000

    *** empty log message ***

--- Java/DTE/FileScanIteratorTest.java
+++ Java/DTE/FileScanIteratorTest.java
@@ -1,6 +1,12 @@
+package Iterators;
+
+
 import java.io.*;
 import Iterators.*;
 import Types.*;
+import Expressions.*;
+import Functions.*;
+
 
 /** This is a class whose only purpose is to test the FileScanIterator
     It takes a file name and a list of type names, for example:
@@ -15,12 +21,31 @@ public class FileScanIteratorTest {
 			typeDescs[i] = new TypeDesc(args[i + 1]);
 		}
 		FileScanIterator fs = new FileScanIterator(fileName, typeDescs);
+
+		FileWriter fw = new FileWriter("hg.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+		
 		Tuple t;
 		int cnt = 0;
 		while((t = fs.getNext()) != null){
-//			t.print(System.out);
+		        t.print(pw);
 			cnt++;
 		}
-		System.out.println("read " + cnt + " tuples");
+			       
+		pw.flush();
+		pw.close();
+
+	      	System.out.println("read " + cnt + " tuples");
+
+
+		ExecExpr[] m1 = new ExecExpr[10];
+		ExecExpr[] m2 = new ExecExpr[10];	
+		TypeDesc[] ty = new TypeDesc[10];
+
+		NLjoinIterator n = new NLjoinIterator(fs,fs,m1,m2,ty,10,"hh");
+		SelProj S = new SelProj(fs,m1,m2,ty);
+
+
 	}
 }
