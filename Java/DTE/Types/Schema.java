0f75082e505d1d8676952304906efb27a00d533d
nengwu <>
1998-04-18 20:11:29 +0000

    *** empty log message ***

--- Java/DTE/Types/Schema.java
+++ Java/DTE/Types/Schema.java
@@ -1,32 +1,127 @@
-package DataSources;
+package Types;
 
+import java.io.*;
 import java.util.*;
 
+
 public class Schema {
-  private TypeDesc[] typeIDs;
-  private String[] attributeNames;
 
-  public Schema(){
+  TypeDesc[] typeIDs;
+  String[] attributeNames;
+
+  public Schema( String str ) {
+    if( str != null ){
+
+      StringTokenizer st = new StringTokenizer( str );
+      int numFlds = (int) st.nextToken();
+
+      typeIDs = new TypeDesc[numFlds];
+      attributeNames = new String[ numFlds ];
+
+      while( st.hasMoreTokens() ){
+	for(int i=0; i<numFlds; i++){
+	  typeIDs[i] = new TypeDesc(st.nextToken());
+	  attributeNames[i] = st.nextToken();
+	}
+      }
+    }
   }
 
-  /*
-  public Schema( String str){
-    schemastr = new String(str);
+  public Schema(int numFlds, String types, String attrnms){
+    if( types != null && attrnms != null){
+      
+      StringTokenizer st1 = new StringTokenizer( types);
+      StringTokenizer st2 = new StringTokenizer( attrnms);
+
+      if( st1.countTokens() == numFlds && st2.countTokens() == numFlds){
+
+	typeIDs = new TypeDesc[ numFlds ];
+	attributeNames = new String[ numFlds ];
+
+	for(int i=0; i< numFlds; i++){
+	  typeIDs[i] = new TypeDesc(st1.nextToken());
+	  attributeNames[i] = st2.nextToken();
+	}
+      }
+    }
   }
-  */
-  
-  public boolean read(StreamTokenizer st) throws IOException {
+ 
+
+
+  public boolean read( StreamTokenizer st ) throws IOException {
+    
+    int count=0;
+    //StreamTokenizer st = new StreamToken(r);
+    
     if(st.nextToken() == StreamTokenizer.TT_EOF){
       return false;
     }
-    int numFlds = (int) st.nval;
+    
+    while( (st.ttype != StreamTokenizer.TT_NUMBER)&&
+	 (st.ttype != StreamTokenizer.TT_EOF) ){
+      st.nextToken();
+    }
+  
+    if( st.ttype == StreamTokenizer.TT_NUMBER ){
+      typeIDs = new TypeDesc[st.nval];
+      attributeNames = new String[st.nval];
+      
+      for(count=0; count<st.nval; count++){
+	if(st.nextToken() == StreamTokenizer.TT_WORD)
+	  typeIDs[count] = new TypeDesc(st.sval);
+	else throw new IOException("Misplaced, ATTRTYPE expected");
+	
+	if(st.nextToken() == StreamTokenizer.TT_WORD)
+	  attributeNames[count] = new String(st.sval);
+	else throw new IOException("Misplaced, ATTRNAME expeted");
+      }
+      return true;
+      
+    }
+    return false;
+  }
+  
 
-    // read type attrName pairs, such as "int a1 double a2"
-
-    return true;
+  public int getnumFlds() throws IOException {
+    if( typesIDs.length == attribueNames.length)
+      return typeIDs.length;
+    else throw IOException("Numbers of Types and ATTRNMs expected to equal");
   }
 
-  String toString(){
+  public String toString(){
+    String str = new String( );
+    str += typeIDs.length;
+  
+    for(int count=0; i< typeIDs.length; i++){
+      str += " ";
+      str += typeIDs[count].getString();
+      str += " ";
+      str += attributeNames[count];
+    }
+  
+    return str;  
+  }
+  
+
+  public void printTypes(){
+    String str = new String();
+    for(int i=0; i<typeIDs.length; i++){
+      str += " ";
+      str += typeIDs[i].getString();
+    }
+    System.out.println( str);
+  }
+
+  
+  public void printAttrnms(){
+    String str = new String();
+    for(int i=0; i<attributeNames.length; i++){
+      str += " ";
+      str += attributeNames[i];
+    }
+    System.out.println( str);
   }
 }
-    
+
+
+
