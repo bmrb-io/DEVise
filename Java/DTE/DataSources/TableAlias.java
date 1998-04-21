637f79c0a546d2f3b96e1ab46fa1dd756fda5424
donjerko <>
1998-04-21 22:53:56 +0000

    *** empty log message ***

---
+++ Java/DTE/DataSources/TableAlias.java
@@ -0,0 +1,11 @@
+
+class TableAlias{
+
+	// Jie, this class is the mirror of TableAlias in Core/myopt.h
+
+	RelationId relId;
+	String alias;
+	public TableAlias(relId, alias){
+	}
+	// implement toString
+}
---
+++ Java/DTE/Expressions/Query.java
@@ -0,0 +1,16 @@
+package Expressions;
+
+import java.io.*;
+
+/**  */
+
+public class Query {
+	Vector selectClause;
+	Vector fromClause;
+	Vector whereClause;
+	public Query(Vector selectClause, Vector fromClause, Vector whereClause){
+		
+	}
+	// Jie, add toString function that will return string like:
+	// "select t.x + 1 from 1.1 as t"
+}
