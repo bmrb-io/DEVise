6e5a07a3057aac4c36e00d782ac365dd6513d6cd
wenger <>
1998-05-04 17:47:46 +0000

    Added export files that Dongbin apparently forgot to commit.

---
+++ csgroup/export/CmdDescriptor.h
@@ -0,0 +1,28 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  Export file for CmdDescriptor.h -- exposes the 'real' CmdDescriptor.h to
+  code outside of csgroup.
+ */
+
+/*
+  $Id$
+
+  $Log$
+ */
+
+#include "../CmdDescriptor.h"
+
+/*============================================================================*/
---
+++ csgroup/export/Csprotocols.h
@@ -0,0 +1,28 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  Export file for Csprotocols.h -- exposes the 'real' Csprotocols.h to
+  code outside of csgroup.
+ */
+
+/*
+  $Id$
+
+  $Log$
+ */
+
+#include "../Csprotocols.h"
+
+/*============================================================================*/
