c61cad3f31eb95add76c86ce8a2b63a6295a68fd
wenger <>
1998-05-05 18:20:00 +0000

    Committed changes Zhenhai made, but his copy of this file wasn't in CVS somehow.

--- graphics/ViewDir.h
+++ graphics/ViewDir.h
@@ -20,14 +20,55 @@
   $Id$
 
   $Log$
+  Revision 1.1  1998/03/18 19:27:22  wenger
+  Zhenhai forgot to commit this.
+
  */
 
 
 #ifndef ViewDir_h_
 #define ViewDir_h_
 
+#include <string.h>
+
 // viewing direction
 // need modification later
 enum ViewDir {PosX, PosY, PosZ, NegX, NegY, NegZ};
+inline char * ViewDir2Char(ViewDir const dir) {
+  switch(dir) {
+	case PosX: return "PosX";
+	case PosY: return "PosY";
+	case PosZ: return "PosZ";
+	case NegX: return "NegX";
+	case NegY: return "NegY";
+	case NegZ: return "NegZ";
+  	default:
+		return 0;
+  }
+}
+
+inline int Char2ViewDir(ViewDir &dir, char const * const x) {
+  if (strcasecmp(x,"PosX")==0) {
+	dir=PosX;
+  }
+  else if (strcasecmp(x,"PosY")==0) {
+	dir=PosY;
+  }
+  else if (strcasecmp(x,"PosZ")==0) {
+	dir=PosZ;
+  }
+  else if (strcasecmp(x,"NegX")==0) {
+	dir=NegX;
+  }
+  else if (strcasecmp(x,"NegY")==0) {
+	dir=NegY;
+  }
+  else if (strcasecmp(x,"NegZ")==0) {
+	dir=NegZ;
+  }
+  else
+      return 0;
+  return 1;
+}
 
 #endif // ViewDir_h_
