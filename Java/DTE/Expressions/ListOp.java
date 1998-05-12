539a6d2de92d1c25ef34f193ceeedb85d2627331
gang <>
1998-05-12 16:01:19 +0000

    *** empty log message ***

--- Java/DTE/Expressions/ListOp.java
+++ Java/DTE/Expressions/ListOp.java
@@ -3,19 +3,25 @@ package Expressions;
 import Types.*;
 
 
+/** This class is used to check whether the retrieved tuples satisfy the join or selection projection condition. If so, it can produce the return tuple.*/ 
+
 public class ListOp
 {
+  /** This function is used to check whether the retrieved tuples satisfy the join or selection projection condition according to the condition in the ExecExpr[] list.*/
+  
   public static boolean evaluateList(ExecExpr[] list, Tuple left, Tuple right)
   {
     for(int i = 0; i < list.length; i++)
       {
-	if(!((DTE_Boolean) list[i].evaluate(left, right)).getValue())
+	if(!(((DTE_Boolean)(list[i].evaluate(left, right)))).getValue())
 	  return false;
       }
     return true;
   }
   
 
+  /** This function is used to produce the return tuple according to the condition in the ExecExpr[] list.*/
+ 
   public static void tupleFromList(Tuple retVal, ExecExpr[] list,
 			    Tuple left, Tuple right)
   {
