09effebdde09ce5a1a4afe772570421459213b9d
wenger <>
1998-06-16 16:30:55 +0000

    Added standard headers to DataReader sources.

--- DataReader/DataReaderParser.h
+++ DataReader/DataReaderParser.h
@@ -1,3 +1,23 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$
+ */
+
 #ifndef _DataReader_PARSER_H_
 #define _DataReader_PARSER_H_
 
--- DataReader/Schema.C
+++ DataReader/Schema.C
@@ -1,3 +1,23 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1992-1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  $Id$
+
+  $Log$
+ */
+
 #include "DRSchema.h"
 #include "sysdep.h"
 #include "DateTime.h"
