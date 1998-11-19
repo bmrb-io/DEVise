67e5abb531d5ad909ff3eacb4d32bc051713d1f9
wenger <>
1998-11-19 22:01:36 +0000

    Implemented non-DTE version of DEVise (new code handles data source catalog functions; Tables, SQLViews, etc., are not implemented).

---
+++ src/data/export/DataCatalog.h
@@ -0,0 +1,28 @@
+/*
+  ========================================================================
+  DEVise Data Visualization Software
+  (c) Copyright 1998
+  By the DEVise Development Group
+  Madison, Wisconsin
+  All Rights Reserved.
+  ========================================================================
+
+  Under no circumstances is this software to be copied, distributed,
+  or altered in any way without prior permission from the DEVise
+  Development Group.
+*/
+
+/*
+  Export file for DataCatalog.h -- exposes the "real" DataCatalog.h to code
+  outside of src/data.
+ */
+
+/*
+  $Id$
+
+  $Log$
+ */
+
+#include "../DataCatalog.h"
+
+/*============================================================================*/
