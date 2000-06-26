3488125520a54ce1c1a5f6c6463ef38baa519884
wenger <>
2000-06-26 16:47:36 +0000

    Added timestamp to jspop log.

--- Java/JavaScreen/YLogConsole.java
+++ Java/JavaScreen/YLogConsole.java
@@ -19,6 +19,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.4  2000/03/23 16:26:17  wenger
+// Cleaned up headers and added requests for comments.
+//
 // Revision 1.3  1999/06/23 20:59:20  wenger
 // Added standard DEVise header.
 //
@@ -26,6 +29,9 @@
 // ========================================================================
 
 // YLogConsole.java
+
+import  java.util.*;
+import  java.text.*;
 
 public class YLogConsole
 {
@@ -38,10 +44,14 @@ public class YLogConsole
 
     public void p(String msg, int level)
     {
-        if (level > loglevel)
+        if (level > loglevel) {
             return;
-        else
-            System.out.print(msg);
+        } else {
+	    Date date = new Date();
+	    DateFormat dtf = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, 
+	      DateFormat.MEDIUM);
+            System.out.print(msg + " [" + dtf.format(date) + "]");
+        }
     }
 
     public void p(String msg)
@@ -51,10 +61,14 @@ public class YLogConsole
 
     public void pn(String msg, int level)
     {
-        if (level > loglevel)
+        if (level > loglevel) {
             return;
-        else
-            System.out.println(msg);
+        } else {
+	    Date date = new Date();
+	    DateFormat dtf = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, 
+	      DateFormat.MEDIUM);
+            System.out.println(msg + " [" + dtf.format(date) + "]");
+        }
     }
 
     public void pn(String msg)
