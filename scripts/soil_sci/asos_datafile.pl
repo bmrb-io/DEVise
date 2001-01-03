1c940137b468756b04eeb37edaddd4a7f12a0468
wenger <>
2001-01-03 22:05:14 +0000

    Modified ASOS data grabbing script to work in the year 2001.

--- scripts/soil_sci/asos_datafile.pl
+++ scripts/soil_sci/asos_datafile.pl
@@ -9,42 +9,65 @@
 ############################################################
 
 $asosdir = "/p/devise/ftp/soil_data";
-$asosdat = "/local.biron/devise/dat/Asos";
-$homedir = "/local.biron/devise/dat/Asos/scripts";
+$asosdat = "/local.pumori/devise/dat/Asos";
+$homedir = "/local.pumori/devise/dat/Asos/scripts";
 
-$year = 2000000;
-$yr = $year;
 
-# Current DOY
-$day_of_year = (localtime(time()))[7];
 #
-open(ASOSLIST, "$homedir/asos.list") or die "Can't open $homedir/asos.list\n";
+# Get the info we need to figure out the filename we should open for the
+# most recent data.
+#
+
+# Seconds in a day.
+my $day = 60 * 60 * 24;
+
+# The file we get today has yesterday's data in it, so go back one day.
+@timeinfo = localtime(time() - $day);
+
+my $year = $timeinfo[5] + 1900;
+
+# Days in filename go from 1 - 366; localtime returns 0 - 365.
+my $day_of_year = $timeinfo[7] + 1;
+
+my $yr = $year * 1000;
+
+
+#
+# Get a list of all stations.
+#
+open(ASOSLIST, "$homedir/asos.list") or
+    die "Can't open $homedir/asos.list $!\n";
+
+$stnNum = 0;
 while ($line = <ASOSLIST>) {
   chop ($line);
   ($stn, $stnlat, $stnlon) = split (/\s+/, $line, 3);
-  $minstn[$i] = $stn;
-  $lat[$i] = $stnlat;
-  $lon[$i] = $stnlon;
-  $i++;
-  $nstn = $i - 1;
-  $n[$i] = 0;
+  $minstn[$stnNum] = $stn; #TEMP -- why "minstn"???
+  $lat[$stnNum] = $stnlat;
+  $lon[$stnNum] = $stnlon;
+  $stnNum++;
+  $recCt[$stnNum] = 0; # Number of data records for this station.
 }
+$stnCt = $stnNum - 1; # Number of stations.
+
 #
-for ($i=0; $i<=$nstn; $i++) {
-#     print("$i  $lat[$i]  $lon[$i] $minstn[$i]\n");
+for ($stnNum=0; $stnNum<=$stnCt; $stnNum++) {
+#     print("$stnNum  $lat[$stnNum]  $lon[$stnNum] $minstn[$stnNum]\n");
 }
+
 
 chdir ($asosdir);
 #for ($doy=$day_of_year; $doy<=$day_of_year; $doy++) {
 for ($doy=$day_of_year) {
-  $yyddd = $yr + $doy;
-  $file = "WI_HR.$yyddd";
-  open (INFILE, "$file") or die "Can't open $file\n";
+  my $datestr = $yr + $doy;
+  $file = "WI_HR.$datestr";
+
+  open (INFILE, "$file") or die "Can't open $file: $!\n";
   $line = <INFILE>;
   $line = <INFILE>;
   while ($line = <INFILE>) {
     $yearddd  = substr($line,0,9);
-    $hour   = substr($line,10,9);
+    # Not used. $hour   = substr($line,10,9);
     $hhmmss = substr($line,20,9);
     $xstn   = substr($line,30,4);
     $xlat   = substr($line,35,9);
@@ -55,40 +78,41 @@ for ($doy=$day_of_year) {
     $pres   = substr($line,85,9);
     $prec   = substr($line,95,9);
     $wdir   = substr($line,105,9);
-    for ($i=0; $i<=$nstn; $i++) {
-      if ($xstn eq $minstn[$i]) {
-        $n[$i]++;
-        $timloc = $hhmmss/10000.;
-        $doy = $yearddd - $year;
+    for ($stnNum=0; $stnNum<=$stnCt; $stnNum++) {
+      if ($xstn eq $minstn[$stnNum]) {
+        $recCt[$stnNum]++;
+	#TEMP -- we shouldn't be using yr here, because that comes from
+	# the current date, not the date in the input file
+        $doy = $yearddd - $yr;
         &AWSTime;
 #
-#        $dectime{$i,$n[$i]} = $yearddd + $hhmmss/240000.;
+#        $dectime{$stnNum,$recCt[$stnNum]} = $yearddd + $hhmmss/240000.;
         if ($ta!=0 && $td!=0) {
           $ea   = 6.108*10.0**(7.5*($td-273.15)/(237.15+$td-273.15));
           $esat = 6.108*10.0**(7.5*($ta-273.15)/(237.15+$ta-273.15));
-          $rh{$i,$n[$i]} = $ea/$esat*100;
+          $rh{$stnNum,$recCt[$stnNum]} = $ea/$esat*100;
         } else {
-          $rh{$i,$n[$i]} = -9999;
+          $rh{$stnNum,$recCt[$stnNum]} = -9999;
         }
-	$eao{$i,$n[$i]} = $ea;
-	$preco{$i,$n[$i]} = $prec;
-        $tao{$i,$n[$i]} = $ta - 273.15;
-        $tdo{$i,$n[$i]} = $td - 273.15;
-        $windo{$i,$n[$i]} = $wind;
-        $wdiro{$i,$n[$i]} = $wdir;
+	$eao{$stnNum,$recCt[$stnNum]} = $ea;
+	$preco{$stnNum,$recCt[$stnNum]} = $prec;
+        $tao{$stnNum,$recCt[$stnNum]} = $ta - 273.15;
+        $tdo{$stnNum,$recCt[$stnNum]} = $td - 273.15;
+        $windo{$stnNum,$recCt[$stnNum]} = $wind;
+        $wdiro{$stnNum,$recCt[$stnNum]} = $wdir;
       }
     }
   }
   close (INFILE);
 }
 
-for ($i=0; $i<=$nstn; $i++) {
- open (STNFILE, ">>$asosdat/$minstn[$i]") or
-     die "Can't open $asosdat/$minstn[$i]";
-  for ($j=1; $j<=$n[$i]; $j++) {  
+for ($stnNum=0; $stnNum<=$stnCt; $stnNum++) {
+  open (STNFILE, ">>$asosdat/$minstn[$stnNum]") or
+     die "Can't open $asosdat/$minstn[$stnNum]: $!";
+  for ($j=1; $j<=$recCt[$stnNum]; $j++) {  
     printf STNFILE ("%9s %9.0f %9.3f  %9.3f  %9.3f  %9.3f  %9.3f  %9.3f  %9.3f\n",
-              $yyddd{$i,$j},$hhmm{$i,$j},$rh{$i,$j},$eao{$i,$j},$tao{$i,$j},
-	      $tdo{$i,$j},$preco{$i,$j},$windo{$i,$j},$wdiro{$i,$j});
+              $yyddd{$stnNum,$j},$hhmm{$stnNum,$j},$rh{$stnNum,$j},$eao{$stnNum,$j},$tao{$stnNum,$j},
+	      $tdo{$stnNum,$j},$preco{$stnNum,$j},$windo{$stnNum,$j},$wdiro{$stnNum,$j});
   }
   close (STNFILE)
 }
@@ -97,48 +121,47 @@ for ($i=0; $i<=$nstn; $i++) {
 #							            AWS_TIME
 sub AWSTime {
 
-  $mm = $timloc % 1.0;
-  $hh = $timloc - $mm;
-  $hhmm{$i,$n[$i]} = $hh*100. + $mm*60.;
-  $hhmm{$i,$n[$i]} = $hhmm{$i,$n[$i]} % 2400;
+  $hhmm{$stnNum,$recCt[$stnNum]} = $hhmmss / 100;
+  $hhmm{$stnNum,$recCt[$stnNum]} = $hhmm{$stnNum,$recCt[$stnNum]} % 2400;
   $ddd = $doy;
-  if ($hhmm{$i,$n[$i]} == 0.0) {
-    $hhmm{$i,$n[$i]} = 2400;
+  if ($hhmm{$stnNum,$recCt[$stnNum]} == 0.0) {
+    $hhmm{$stnNum,$recCt[$stnNum]} = 2400;
     $ddd--;
   }
-  $yyddd{$i,$n[$i]} = $yr + $ddd;
+  $yyddd{$stnNum,$recCt[$stnNum]} = $yr + $ddd;
 
-  if ($hhmm{$i,$n[$i]} > 600.0) {
-    if($hhmm{$i,$n[$i]} >= 2340.0) {
-      if($hhmm{$i,$n[$i]} < 2400.0) {
-        if($yyddd{$i,$n[$i]} > $yyddd{$i-1,$n[$i-1]}) {
-          $yyddd{$i,$n[$i]} = $yyddd{$i,$n[$i]} - 1.0;
-	  if($yyddd{$i,$n[$i]} == 2000000) {
-	    $yyddd{$i,$n[$i]} = 1999365;
-          }
+  #TEMP -- what the hell is this?  some kind of attempt to convert from 
+  # GMT to local time???
+  if ($hhmm{$stnNum,$recCt[$stnNum]} > 600.0) {
+    if($hhmm{$stnNum,$recCt[$stnNum]} >= 2340.0) {
+      if($hhmm{$stnNum,$recCt[$stnNum]} < 2400.0) {
+        if($yyddd{$stnNum,$recCt[$stnNum]} > $yyddd{$stnNum-1,$recCt[$stnNum-1]}) {
+          $yyddd{$stnNum,$recCt[$stnNum]} = $yyddd{$stnNum,$recCt[$stnNum]} - 1.0;
+	  #TEMP -- this is not general!!!
+	  if($yyddd{$stnNum,$recCt[$stnNum]} == 2000000) {
+	    $yyddd{$stnNum,$recCt[$stnNum]} = 1999365;
+          } elsif($yyddd{$stnNum,$recCt[$stnNum]} == 2001000) {
+	    $yyddd{$stnNum,$recCt[$stnNum]} = 2000366;
+	  }
         }
       }
     }
-    $hhmm{$i,$n[$i]} = $hhmm{$i,$n[$i]} - 600.0;
+    $hhmm{$stnNum,$recCt[$stnNum]} = $hhmm{$stnNum,$recCt[$stnNum]} - 600.0;
   } else { 
-    $hhmm{$i,$n[$i]} = $hhmm{$i,$n[$i]} + 600.0 + 1200.0;
-    $yyddd{$i,$n[$i]} = $yyddd{$i,$n[$i]} - 1.0;
-    if($yyddd{$i,$n[$i]} == 2000000) {
-       $yyddd{$i,$n[$i]} = 1999365;
+    $hhmm{$stnNum,$recCt[$stnNum]} = $hhmm{$stnNum,$recCt[$stnNum]} + 600.0 + 1200.0;
+    $yyddd{$stnNum,$recCt[$stnNum]} = $yyddd{$stnNum,$recCt[$stnNum]} - 1.0;
+    #TEMP -- this is not general!!!
+    if($yyddd{$stnNum,$recCt[$stnNum]} == 2000000) {
+       $yyddd{$stnNum,$recCt[$stnNum]} = 1999365;
+    } elsif($yyddd{$stnNum,$recCt[$stnNum]} == 2001000) {
+       $yyddd{$stnNum,$recCt[$stnNum]} = 2000366;
     }
   }
-  $dy = $yyddd{$i,$n[$i]} - $yr;
-  if($yyddd{$i,$n[$i]} >= $yr) {
-    $yy = "00";
-  }
-#
-  if($dy < 10) {
-    $dy = "00$dy";
-  } elsif($dy >= 10 && $dy < 100) {
-    $dy = "0$dy";
-  } elsif($dy >= 100) {
-    $dy = $dy;
-  }
-  $yyddd{$i,$n[$i]} = $yy.$dy;
-#  print("$yyddd{$i,$n[$i]}  $hhmm{$i,$n[$i]}\n");
+
+  # Change $yyddd{$stnNum,$recCt[$stnNum] from int to string, cut off
+  # the 1st. 2 chars (e.g., 2001002 becomes "01002").
+  my $tmpstr = sprintf("%7d", $yyddd{$stnNum,$recCt[$stnNum]});
+  $yyddd{$stnNum,$recCt[$stnNum]} = substr($tmpstr, 2);
+
+#  print("$yyddd{$stnNum,$recCt[$stnNum]}  $hhmm{$stnNum,$recCt[$stnNum]}\n");
 }
