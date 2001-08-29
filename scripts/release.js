3057b03f1ff26a18a56451f65443ff954e680455
wenger <>
2001-08-29 17:40:00 +0000

    Major changes to DEVise and JavaScreen release scripts.

--- scripts/release.js
+++ scripts/release.js
@@ -2,7 +2,7 @@
 
 #  ========================================================================
 #  DEVise Data Visualization Software
-#  (c) Copyright 1992-1998
+#  (c) Copyright 2001
 #  By the DEVise Development Group
 #  Madison, Wisconsin
 #  All Rights Reserved.
@@ -19,32 +19,42 @@
 #  $Id$
 
 #  $Log$
-#  Revision 1.1  1998/08/04 21:03:43  wenger
-#  First version -- may not be fully functional.
-#
 
 ############################################################
 
-set files = "Java/JavaScreen/js.class"
+#-----------------------------------------------------------
+# Get and check command-line arguments.
 
-set target = /p/devise/public
-
-if ($#argv > 0) then
-  set target = $1
+if ($#argv < 1) then
+  echo "Usage: release.js <destination>"
+  exit 1
 endif
 
+set src = Java/JavaScreen
+
+set desttop = $1/release
+set dest = $desttop/JavaScreen
+
+#-----------------------------------------------------------
+# Make sure we have the files we need to do the release.
+
+set files = ($src/jsa1.jar $src/JavaScreen/jspop.class \
+    $src/JavaScreen/jss.class)
 foreach file ($files)
-  if ($file == "") then
-    continue
-  endif
   if (! -f $file) then
-    echo File $file missing. Cannot proceed with release procedure.
+    echo "File $file missing."
+    echo "Cannot proceed with release procedure."
     exit 1
   endif
 end
 
-echo You are about to release YOUR copy of the JavaScreen to the
-echo general public. The target directory is $target.
+#-----------------------------------------------------------
+# Confirm that we want to go ahead.
+
+echo ""
+echo "You are about to release YOUR copy of DEVise JavaScreen to the"
+echo "general public. The destination directory is:"
+echo "  $dest"
 echo ""
 echo -n "Are you absolutely sure you want to do this? [N] "
 
@@ -55,28 +65,83 @@ if ($answer != y && $answer != Y) then
 endif
 
 echo ""
-echo Executing release procedures...
-sleep 1
 
-if (! -d $target) then
-  mkdir $target
-endif
-if (! -d $target/Java) then
-  mkdir $target/Java
-endif
-if (! -d $target/Java/JavaScreen) then
-  mkdir $target/Java/JavaScreen
+#-----------------------------------------------------------
+# Create the top-level directories we need.
+
+echo "Creating top-level directories"
+
+if (! -d $desttop) then
+  mkdir $desttop
 endif
 
-cp -p Java/JavaScreen/*.class $target/Java/JavaScreen
-cp -p Java/JavaScreen/*.gif $target/Java/JavaScreen
-cp -p Java/JavaScreen/runjs $target/Java/JavaScreen
-cp -p Java/JavaScreen/devised.jspop $target/Java/JavaScreen
-cp -p Java/JavaScreen/js $target/Java/JavaScreen
-cp -p Java/JavaScreen/jspop $target/Java/JavaScreen
-cp -p Java/JavaScreen/users.cfg $target/Java/JavaScreen
+if (! -d $dest) then
+  mkdir $dest
+endif
 
-cp -p run_public/runjs $target/../run
+#-----------------------------------------------------------
+# Release the class files.
 
 echo ""
-echo Done!
+echo "Releasing class files"
+set classdir = $dest/JavaScreen
+if (! -d $classdir) then
+  mkdir $classdir
+endif
+cp $src/JavaScreen/*.class $classdir
+
+#-----------------------------------------------------------
+# Release the jar files.
+
+echo ""
+echo "Releasing jar files"
+cp $src/*.jar $dest
+
+#-----------------------------------------------------------
+# Set up the bin directory.
+
+echo ""
+echo "Setting up bin directory"
+pushd $dest >& /dev/null
+if (! -e bin) then
+  ln -s ../bin .
+endif
+popd >& /dev/null
+
+#-----------------------------------------------------------
+# Set up the lib directory.
+
+echo ""
+echo "Setting up lib directory"
+pushd $dest >& /dev/null
+if (! -e lib) then
+  ln -s ../lib .
+endif
+popd >& /dev/null
+
+#-----------------------------------------------------------
+# Release various script files, etc.
+
+echo ""
+echo "Releasing misc. files"
+
+pushd $src > /dev/null
+set files = (check_jss restart_jss jss DEVise.kill jss.kill ports+files \
+    DEVise.jspop DEVise.jspop_soil get_timestamp jss_savepid \
+    DEVise_savepid js.cgi check_connect check_jsall check_jspop jspop \
+    js js_cgi restart_jspop jspop.kill kill_jsall ports+files get_stats \
+    get_timestamp run_top run_check jspop_savepid users.cfg install_js \
+    Tasvir)
+foreach file ($files)
+  cp -p $file $dest
+  chmod 755 $dest/$file
+end
+popd >& /dev/null
+
+#-----------------------------------------------------------
+# Release the GIFs.
+
+echo ""
+echo "Releasing GIFs"
+cp -p $src/*.gif $dest
+chmod 644 $dest/*.gif
