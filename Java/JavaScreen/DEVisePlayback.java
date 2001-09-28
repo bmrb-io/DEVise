e93034cfe68e1e6152cd750412f11c74cf33c7fb
xuk <>
2001-09-28 18:34:51 +0000

    JavaScreen command log playback.  This class starts a thread that sends playback commands.

---
+++ Java/JavaScreen/DEVisePlayback.java
@@ -0,0 +1,113 @@
+// ========================================================================
+// DEVise Data Visualization Software
+// (c) Copyright 2000-2001
+// By the DEVise Development Group
+// Madison, Wisconsin
+// All Rights Reserved.
+// ========================================================================
+
+// Under no circumstances is this software to be copied, distributed,
+// or altered in any way without prior permission from the DEVise
+// Development Group.
+
+// ------------------------------------------------------------------------
+
+// JavaScreen command log playback.  This class starts a thread that sends 
+// playback request.
+
+// ------------------------------------------------------------------------
+
+// ========================================================================
+
+//TEMP package edu.wisc.cs.devise.js.jsc;
+package JavaScreen;
+
+import java.io.*;
+import java.net.*;
+
+public class DEVisePlayback implements Runnable
+{
+    private DEViseCmdDispatcher _dispatcher = null;
+    private Thread _thread = null;
+    private String _filename = null;
+    private jsdevisec _jsc = null;
+
+    public DEVisePlayback(jsdevisec jsc, DEViseCmdDispatcher dispatcher, String filename) {
+        _dispatcher = dispatcher;
+	_filename = filename;
+	_jsc = jsc;
+
+	_thread = new Thread(this);
+	_thread.start();
+    }
+
+    public void run()
+    {
+	long pretime = 0, time = 0;
+
+	try {
+	    //BufferedReader file = new BufferedReader(new FileReader(_filename));
+	    URL myURL = new URL(_filename);
+	    BufferedReader file = new BufferedReader(new InputStreamReader(myURL.openStream()));
+
+	    String line = null;
+	     
+	    if (_jsc.isDisplay && _jsc.isOriginal) {
+		while ((line = file.readLine()) != null) {
+		    _jsc.pn("read line: " + line);
+		    time = Long.valueOf(line).longValue();
+		    
+		    if (pretime != 0) {
+			long gap = time-pretime;
+			_jsc.pn("we are going to sleep " + gap + "ms.");
+			Thread.sleep(gap);
+		    }
+		    pretime = time;		
+		    
+		    line = file.readLine();
+		    _jsc.pn("read line: " + line);
+		    if ( line.startsWith(DEViseCommands.CONNECT) || 
+			 line.startsWith(DEViseCommands.GET_SESSION_LIST) ||
+			 line.startsWith(DEViseCommands.EXIT) ) {
+			continue;
+		    } else {
+			_dispatcher.start(line);
+		    }   
+		}
+	    } else { // not original playback
+		String cmd = new String();
+		while ((line = file.readLine()) != null) {
+		    _jsc.pn("read line: " + line);
+
+		    line = file.readLine();
+		    _jsc.pn("read line: " + line);
+
+		    if ( line.startsWith(DEViseCommands.CONNECT) || 
+			 line.startsWith(DEViseCommands.GET_SESSION_LIST) ||
+			 line.startsWith(DEViseCommands.EXIT) ) {
+			continue;
+		    } else {
+			cmd = cmd + line + "\n"; 
+		    } 
+		}
+		_dispatcher.start(cmd);
+	    }
+
+	    file.close();
+	} catch (InterruptedException e) {
+	} catch (NumberFormatException e) {
+	    _jsc.showMsg("Wrong long number format.");
+	} catch (FileNotFoundException e) {
+	    _jsc.showMsg("File not found: " + _filename);
+	} catch (IOException e) {
+	    _jsc.showMsg("File read error: " + _filename);
+	}
+    }
+
+    public void stop()
+    {
+        _thread.stop();
+    }
+}
+
+
