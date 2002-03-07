4866c694b10824f5153eb1205479417d76d77383
xuk <>
2002-03-07 17:20:03 +0000

    Added DEViseJSTimer.java file to implement a timer to destroy applet when it's invisible longer than one hour.

---
+++ Java/JavaScreen/DEViseJSTimer.java
@@ -0,0 +1,87 @@
+// ========================================================================
+// DEVise Data Visualization Software
+// (c) Copyright 2000-2001
+// By the DEVise Development Group
+// Madison, Wisconsin
+// All Rights Reserved.
+// ========================================================================
+
+// Under no circumstances is this software to be copied, distributed,
+// or altered in any way without prior permission from the DEVise
+// Development Group.
+
+// ------------------------------------------------------------------------
+
+// JavaScreen timer.  This class starts a thread that destroy 
+// an invisible applet after 60mins.
+
+// ------------------------------------------------------------------------
+
+
+
+// ========================================================================
+
+package JavaScreen;
+
+public class DEViseJSTimer implements Runnable
+{
+    private static final int DEBUG = 0;
+    private static final int INTERVAL = 60 * 60 * 1000; //one hour 
+    private DEViseJSApplet applet = null;
+    private Thread thread = null;
+    public static boolean stopped = false;
+
+    public DEViseJSTimer(DEViseJSApplet a) {
+        applet = a;
+
+	thread = new Thread(this);
+    }
+
+    public void start() 
+    {
+	if (DEBUG >= 1) {
+	    System.out.println("DEViseJSTimer.start()");
+	}
+
+	thread.start();
+    }
+
+    public void run()
+    {
+	if (DEBUG >= 1) {
+	    System.out.println("DEViseJSTimer.run()");
+	}
+
+	try {
+	    Thread.sleep(INTERVAL);
+
+	    if (stopped) {
+		stopped = false;
+		System.out.println("stop timer.");
+	    } else {
+		applet.destroy();
+		System.out.println("destroy applet.");
+	    }
+
+	    stop();
+	} catch (InterruptedException e) {
+	}
+    }
+
+    public void stop()
+    {
+	if (DEBUG >= 1) {
+	    System.out.println("DEViseJSTimer.stop()");
+	}
+        thread.stop();
+    }
+}
+
+
+
+
+
+
+
+
+
