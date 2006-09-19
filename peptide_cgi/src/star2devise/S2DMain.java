4062d0ebae1277849eb2cb6b81e0192aa57030f3
wenger <>
2006-09-19 22:05:07 +0000

    Fixed a bug in the chem shift reference 1 visualization -- some of the views had 'shape attr 3'/'X Offset' set to a non-zero value (I think because of an old GAttr link) -- this caused the average and tolerance bars to be drawn at the wrong location when run with DEVise 1.9.x.  (I think in previous DEVise versions 'shape attr 3' had no effect on drawing Bars.)

--- peptide_cgi/src/star2devise/S2DMain.java
+++ peptide_cgi/src/star2devise/S2DMain.java
@@ -21,6 +21,11 @@
 // $Id$
 
 // $Log$
+// Revision 1.17  2006/09/06 16:55:00  wenger
+// Increased JVM memory to 512m because that's what it took to get 6011/
+// 1RJJ coordinate vis processing to work at BMRB (marlin); added test37,
+// which tests 6011.
+//
 // Revision 1.16  2006/08/21 21:21:16  wenger
 // Changed version to 11.0.1.
 //
@@ -1189,7 +1194,7 @@ public class S2DMain {
 
     private static final int DEBUG = 0;
 
-    public static final String PEP_CGI_VERSION = "11.0.2x1"/*TEMP*/;
+    public static final String PEP_CGI_VERSION = "11.0.2x2"/*TEMP*/;
     public static final String DEVISE_MIN_VERSION = "1.9.0";
 
     private String _masterBmrbId = ""; // accession number the user requested
