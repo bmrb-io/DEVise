34b3e6018b51e52ae0ec42bcffacef5689b6f735
wenger <>
2009-09-25 21:30:32 +0000

    Changed version to 11.6.3x1, added 11.6.3 section to version history.

--- peptide_cgi/src/star2devise/S2DMain.java
+++ peptide_cgi/src/star2devise/S2DMain.java
@@ -21,6 +21,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.152  2009/09/25 20:05:39  wenger
+// Changed version to 11.6.2 for release.
+//
 // Revision 1.151  2009/09/25 14:48:44  wenger
 // Fixed a bug I introduced in the DNA/RNA deltashift template when
 // I fixed the colors; cleaned up a few commente in other session
@@ -702,7 +705,7 @@ public class S2DMain {
     private static boolean _extraGC = false;
 
     // Change version to 11.3.1 when S2 order stuff is implemented.
-    public static final String PEP_CGI_VERSION = "11.6.2";
+    public static final String PEP_CGI_VERSION = "11.6.3x1"/*TEMP*/;
     public static final String DEVISE_MIN_VERSION = "1.9.0";
     public static final String JS_CLIENT_MIN_VERSION = "5.9.4";
 
