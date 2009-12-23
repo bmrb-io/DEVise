e37268889a54aa6099283193fd75494b56f11bd3
wenger <>
2009-12-23 15:37:10 +0000

    Changed version to 11.7.1x2, added new version history section.

--- peptide_cgi/src/star2devise/S2DMain.java
+++ peptide_cgi/src/star2devise/S2DMain.java
@@ -21,6 +21,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.170  2009/12/22 19:40:30  wenger
+// Changed version to 11.7.1 for release.
+//
 // Revision 1.169  2009/12/22 16:16:41  wenger
 // Updated SPARTA test files to the latest version.
 //
@@ -904,7 +907,7 @@ public class S2DMain {
     private static boolean _extraGC = false;
 
     // Change version to 11.3.1 when S2 order stuff is implemented.
-    public static final String PEP_CGI_VERSION = "11.7.1";
+    public static final String PEP_CGI_VERSION = "11.7.2x1"/*TEMP*/;
     public static final String DEVISE_MIN_VERSION = "1.9.1";
     public static final String JS_CLIENT_MIN_VERSION = "5.10.0";
 
