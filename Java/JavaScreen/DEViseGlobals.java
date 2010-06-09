110323c6c3ca7b0c99c6a9a28473de345ef88004
wenger <>
2010-06-09 19:22:05 +0000

    Changed version to 5.13.2x1 and added 5.13.2 version history section.

--- Java/JavaScreen/DEViseGlobals.java
+++ Java/JavaScreen/DEViseGlobals.java
@@ -20,6 +20,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.132  2010/06/04 17:12:02  wenger
+// Changed version to 5.13.1 for release.
+//
 // Revision 1.131  2010/06/04 16:29:13  wenger
 // Enabled auto resizing in drill-down dialog JTable to improve appearance.
 //
@@ -824,7 +827,7 @@ public final class DEViseGlobals
     public static final int DEFAULTCMDPORT = 6666, DEFAULTIMGPORT = 6644,
       JSSPORT = 1688, JSPOPPORT = 1689;
     public static final String JSPOPHOST = new String("localhost");
-    public static final String VERSION = new String("5.13.1");
+    public static final String VERSION = new String("5.13.2x1"/*TEMP*/);
 
     public static final String PROTOCOL_VERSION = new String("16.0");
 
