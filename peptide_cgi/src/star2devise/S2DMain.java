1e8a13c191adf12578210e174ffd2f5cbd339067
wenger <>
2010-06-16 17:17:34 +0000

    Changed version to 11.8.3 for release.

--- peptide_cgi/src/star2devise/S2DMain.java
+++ peptide_cgi/src/star2devise/S2DMain.java
@@ -21,6 +21,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.212  2010/06/16 14:36:33  wenger
+// Reduced applet size to match the latest changes in the JavaScreen client.
+//
 // Revision 1.211  2010/06/09 19:39:52  wenger
 // Changed version to 11.8.3x1, and updated DEVISE_MIN_VERSION and
 // JS_CLIENT_MIN_VERSION, which I've forgotten about for a while;
@@ -1328,7 +1331,7 @@ public class S2DMain {
     private static boolean _extraGC = false;
 
     // Change version to 11.3.1 when S2 order stuff is implemented.
-    public static final String PEP_CGI_VERSION = "11.8.3x2"/*TEMP*/;
+    public static final String PEP_CGI_VERSION = "11.8.3";
     public static final String DEVISE_MIN_VERSION = "1.11.0";
     public static final String JS_CLIENT_MIN_VERSION = "5.13.2";
 
