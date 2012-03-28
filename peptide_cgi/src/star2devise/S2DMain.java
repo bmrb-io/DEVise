36aed48943917a5c7c48dc6f8ac2a4cbdae8b834
wenger <>
2012-03-28 22:40:54 +0000

    Changed version to 12.2.2 for release.

--- peptide_cgi/src/star2devise/S2DMain.java
+++ peptide_cgi/src/star2devise/S2DMain.java
@@ -21,6 +21,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.299  2012/03/19 20:28:37  wenger
+// Changed version to 12.2.2x1; added 12.2.2 version history section.
+//
 // Revision 1.298  2012/03/16 18:10:40  wenger
 // Changed version to 12.2.1 for release.
 //
@@ -707,7 +710,7 @@ public class S2DMain {
     	// Whether to do "extra" calls to System.gc().
     private static boolean _extraGC = false;
 
-    public static final String PEP_CGI_VERSION = "12.2.2x1"/*TEMP*/;
+    public static final String PEP_CGI_VERSION = "12.2.2";
     public static final String DEVISE_MIN_VERSION = "1.11.1";
     public static final String JS_CLIENT_MIN_VERSION = "5.14.1";
 
