868d1048a60ea50820d09e977962b07b2e08dd52
wenger <>
2015-01-16 17:24:52 +0000

    Changed SockTest to SockTestClient (in preparation for making SockTestServer); changed SockTestClient to match the latest communication protocol version (17.1).

--- Java/JavaScreen/socket_test/SockTestClient.java
+++ Java/JavaScreen/socket_test/SockTestClient.java
@@ -1,11 +1,12 @@
+//TEMPTEMP -- need to also have this as an applet
 import java.net.Socket;
 import java.net.InetAddress;
 import java.io.*;
 
-class SockTest {
+class SockTestClient {
 
 	public static void main(String args[]) {
-		DoTest("devise.cs.wisc.edu", 6666);//TEMPTEMP?
+		DoTest("devise.cs.wisc.edu", 6667);//TEMPTEMP?
 	}
 
 	public static void DoTest(String hostname, int port) {
@@ -28,22 +29,27 @@ class SockTest {
 //  cmd: JAVAC_Connect
 //  cmd: {guest}
 //  cmd: {guest}
-//  cmd: {17.0}
+//  cmd: {17.1}
+//  cmd: {client_host}
+
+			System.out.println("\nSending command");
 
             sockOut.writeShort(5); // msgType
 			sockOut.writeInt(0); // ID
             sockOut.writeShort(0); // not CGI
 
-			int nelem = 4;
+			int nelem = 5;
 			String cmd[] = new String[nelem];
 			cmd[0] = "JAVAC_Connect" + "\u0000";
 			cmd[1] = "{guest}" + "\u0000";
 			cmd[2] = "{guest}" + "\u0000";
-			cmd[3] = "{17.0}" + "\u0000";
+			cmd[3] = "{17.1}" + "\u0000";
+			cmd[4] = "{client_host}" + "\u0000";
 
 			int size = 0;
 			for (int i = 0; i < nelem; ++i) {
 				size += cmd[i].length() + 2; // +2 copied from real code
+				System.out.println("  " + cmd[i]);
 			}
 
             sockOut.writeShort(nelem);
@@ -53,6 +59,8 @@ class SockTest {
 			    sockOut.writeShort(cmd[i].length());
 				sockOut.writeBytes(cmd[i]);
 			}
+
+			System.out.println("\nReading reply");
 
 			// Read command back...
 			// TEMP -- this isn't fully correct yet, but it's good
