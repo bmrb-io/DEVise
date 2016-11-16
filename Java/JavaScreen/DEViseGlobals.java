44bf3ebf998bfb0137aeaac0560e99db546e274a
Jon Wedell <wedell@bmrb.wisc.edu>
2016-11-16 12:16:31 -0600

    Bump version of javascreen.

--- Java/JavaScreen/DEViseGlobals.java
+++ Java/JavaScreen/DEViseGlobals.java
@@ -40,7 +40,7 @@ public final class DEViseGlobals
     public static final int DEFAULTCMDPORT = 6666,
                             JSSPORT = 7770, JSPOPPORT = 7670;
     public static final String JSPOPHOST = new String("localhost");
-    public static final String VERSION = new String("5.15.2x1"/*TEMP*/);
+    public static final String VERSION = new String("5.15.3");
 
     public static final String PROTOCOL_VERSION = new String("17.2");
 
